2c973494b5228bd6e27df2ff3c12ce28f5627549
sam bassett <samson.bassett@gmail.com>
2015-12-16 22:04:40 -0800

    rewrite chest sack spell so more is inside the item. now it gets block by id, not always chest, couldbe any IInvo

--- src/main/java/com/lothrazar/cyclicmagic/spell/SpellChestSack.java
+++ src/main/java/com/lothrazar/cyclicmagic/spell/SpellChestSack.java
@@ -1,14 +1,9 @@
 package com.lothrazar.cyclicmagic.spell;
 
-import com.lothrazar.cyclicmagic.util.UtilMoveBlock;
-import com.lothrazar.cyclicmagic.ItemRegistry;
+import com.lothrazar.cyclicmagic.item.ItemChestSack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
@@ -24,82 +19,22 @@ public class SpellChestSack extends BaseSpellExp {
 
 		BlockPos posChest = pos.offset(side);
 		//imported from my old mod https://github.com/PrinceOfAmber/SamsPowerups/blob/b02f6b4243993eb301f4aa2b39984838adf482c1/src/main/java/com/lothrazar/samscontent/item/ItemChestSackEmpty.java
-	
-		if(world.getTileEntity(posChest) == null || world.getTileEntity(posChest) instanceof IInventory == false){return;}
-			
-		TileEntity tile = world.getTileEntity(posChest);
-		IInventory invo = (IInventory)tile;
-		
-		ItemStack chestItem;   
-		int ROWS = 3;
-		int COLS = 9;
-		int START_CHEST = 0; 
-		int END_CHEST =  START_CHEST + ROWS * COLS; 
 
-		ItemStack drop = new ItemStack(ItemRegistry.chest_sack ,1,0); 
+		ItemStack drop = ItemChestSack.createStackFromInventory( world,player, posChest);
 		
-		if(drop.getTagCompound() == null)  drop.setTagCompound(new NBTTagCompound());
- 
-		int stacks = 0;
-		int count = 0;
-		
-		int[] itemids = new int[END_CHEST - START_CHEST];
-		int[] itemqty = new int[END_CHEST - START_CHEST];		
-		int[] itemdmg = new int[END_CHEST - START_CHEST];
-		
-		//inventory and chest has 9 rows by 3 columns, never changes. same as 64 max stack size
-		for(int islotChest = 0; islotChest < invo.getSizeInventory(); islotChest++)
-		{
-			//zeroes to avoid nulls, and signify nothing goes there
-			itemids[islotChest] = 0;
-			itemqty[islotChest] = 0;
-			itemids[islotChest] = 0;
-			chestItem = invo.getStackInSlot(islotChest);
-		
-			if(chestItem == null){continue;}//not an error; empty chest slot
-			if(chestItem.getTagCompound() != null)
-			{
-				//probably has an enchantment
-				player.dropPlayerItemWithRandomChoice(chestItem, false); 
+		if(drop != null){
+
+			world.setBlockToAir(posChest); 
+			
+			if(world.isRemote == false  ){
+				player.entityDropItem(drop, 1); 
 			}
-			else
-			{
-				stacks++; 
-				count += chestItem.stackSize;
-				
-				itemids[islotChest] = Item.getIdFromItem(chestItem.getItem());
-				itemdmg[islotChest] = chestItem.getItemDamage(); 
-				itemqty[islotChest] = chestItem.stackSize;
-				
-			}
-			//its either in the bag, or dropped on the player
-			invo.setInventorySlotContents(islotChest, null);	
 		}
-		 
-		if(drop.getTagCompound() == null) drop.setTagCompound(new NBTTagCompound());
-		
-		drop.getTagCompound().setIntArray("itemids", itemids);
-		drop.getTagCompound().setIntArray("itemdmg", itemdmg);
-		drop.getTagCompound().setIntArray("itemqty", itemqty);
-		 
-		drop.getTagCompound().setString("count",""+count);
-		drop.getTagCompound().setString("stacks",""+stacks);
-	 	 
-		if(world.isRemote == false){
-			player.entityDropItem(drop, 1); 
-		}
-		 //the 2 here is just a magic flag it passes to the world to propogate the event
-	
-		player.worldObj.setBlockToAir(posChest); 
 
 		//playerIn.swingItem();
-	
 		//Util.spawnParticle(entityPlayer.worldObj, EnumParticleTypes.CRIT, pos);
-		
 		//Util.playSoundAt(entityPlayer, "random.wood_click");
 		
- 
 		super.onCastSuccess(world, player, posChest);
-    	//return super.onItemUse(stack, player, worldIn, pos, side, hitX, hitY, hitZ); 
 	}
 }
