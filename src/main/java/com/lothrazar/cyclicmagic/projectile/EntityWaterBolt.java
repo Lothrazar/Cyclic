641a0dd8e1dea8cd092098507a1be35b0952c148
sam bassett <samson.bassett@gmail.com>
2015-12-19 08:42:17 -0800

    water shot: do not place water in the nether

--- src/main/java/com/lothrazar/cyclicmagic/projectile/EntityWaterBolt.java
+++ src/main/java/com/lothrazar/cyclicmagic/projectile/EntityWaterBolt.java
@@ -1,23 +1,24 @@
 package com.lothrazar.cyclicmagic.projectile; 
 
 import java.util.ArrayList;
+import com.lothrazar.cyclicmagic.Const;
+import com.lothrazar.cyclicmagic.util.UtilParticle;
+import com.lothrazar.cyclicmagic.util.UtilSound;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.monster.EntityBlaze;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World; 
 
 public class EntityWaterBolt extends EntityThrowable
 { 
 	public static int secondsFrozenOnHit;
-	public static int damageToNormal = 0;//TODO CONFIG
-	public static int damageToBlaze = 2;//TODO CONFIG
 	
     public EntityWaterBolt(World worldIn)
     {
@@ -41,27 +42,20 @@ public class EntityWaterBolt extends EntityThrowable
     {
         if (mop.entityHit != null)
         {
-            float damage = damageToNormal;
-
-            if (mop.entityHit instanceof EntityBlaze)
-            {
-                damage = damageToBlaze;//TODO: config file blaze damage
-            }
-            
             //do the snowball damage, which should be none. put out the fire
-            mop.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), damage);
-            
             
             if(mop.entityHit instanceof EntityLivingBase)
             {
+                mop.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 0);
+
+            	UtilParticle.spawnParticle(this.worldObj, EnumParticleTypes.WATER_SPLASH,  mop.entityHit.getPosition());
+            	
             	EntityLivingBase e = (EntityLivingBase)mop.entityHit;
 
                 if(e.isBurning())
                 {
                 	e.extinguish();
                 }
-                
-            	//e.addPotionEffect(new PotionEffect(PotionRegistry.frozen.id, secondsFrozenOnHit * Reference.TICKS_PER_SEC,0));
             } 
         }
         
@@ -70,41 +64,44 @@ public class EntityWaterBolt extends EntityThrowable
         
         if(pos == null){return;}//hasn't happened yet, but..
 
-		ArrayList<Block> waterBoth = new ArrayList<Block>();
-		waterBoth.add(Blocks.flowing_water);
-		waterBoth.add(Blocks.water);
-    	//Util.spawnParticle(this.worldObj, EnumParticleTypes.SNOWBALL, pos);
-    	//Util.spawnParticle(this.worldObj, EnumParticleTypes.SNOW_SHOVEL, pos);
- 
-        if( mop.sideHit != null && this.getThrower() instanceof EntityPlayer)
-        {
-        	this.worldObj.extinguishFire((EntityPlayer)this.getThrower(), pos, mop.sideHit);
-        	
-        	offset = mop.getBlockPos().offset(mop.sideHit);
+    	UtilParticle.spawnParticle(this.worldObj, EnumParticleTypes.WATER_SPLASH, pos);
+    	
+        if(this.dimension == Const.Dimension.nether){
+        	UtilSound.playSoundAt(this, UtilSound.fizz);
         }
-        
-        Block hitBlock = this.worldObj.getBlockState(pos).getBlock();
-        
-		if(waterBoth.contains(hitBlock) )
-		{
-			//turn flowing water into solid
-    		this.worldObj.setBlockState(pos, Blocks.water.getDefaultState()); 
-		}
-        
-    	if(this.isInWater() == false )
-        {  
-    		if(this.worldObj.isAirBlock(pos)  ) 
-    		{
-        		this.worldObj.setBlockState(pos, Blocks.water.getDefaultState()); 
-    		}
-    		else if(offset != null && this.worldObj.isAirBlock(mop.getBlockPos().offset(mop.sideHit))  ) 
-    		{
-        		this.worldObj.setBlockState(mop.getBlockPos().offset(mop.sideHit), Blocks.water.getDefaultState()); 
-        	}
+        else{
+			ArrayList<Block> waterBoth = new ArrayList<Block>();
+			waterBoth.add(Blocks.flowing_water);
+			waterBoth.add(Blocks.water);
+	 
+	        if( mop.sideHit != null && this.getThrower() instanceof EntityPlayer)
+	        {
+	        	this.worldObj.extinguishFire((EntityPlayer)this.getThrower(), pos, mop.sideHit);
+	        	
+	        	offset = mop.getBlockPos().offset(mop.sideHit);
+	        }
+	        
+	        Block hitBlock = this.worldObj.getBlockState(pos).getBlock();
+	        
+			if(waterBoth.contains(hitBlock) )
+			{
+				//turn flowing water into solid
+	    		this.worldObj.setBlockState(pos, Blocks.water.getDefaultState()); 
+			}
+	        
+	    	if(this.isInWater() == false )
+	        {  
+	    		if(this.worldObj.isAirBlock(pos)  ) 
+	    		{
+	        		this.worldObj.setBlockState(pos, Blocks.water.getDefaultState()); 
+	    		}
+	    		else if(offset != null && this.worldObj.isAirBlock(mop.getBlockPos().offset(mop.sideHit))  ) 
+	    		{
+	        		this.worldObj.setBlockState(mop.getBlockPos().offset(mop.sideHit), Blocks.water.getDefaultState()); 
+	        	}
+	        }
         }
-    	 
-        	 
+
         this.setDead();
- 
     }  
 }
