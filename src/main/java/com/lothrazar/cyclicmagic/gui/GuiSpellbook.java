937e0874caf63b44596c5fcca32c3533c109ff01
sam bassett <samson.bassett@gmail.com>
2015-12-20 12:40:57 -0800

    spell GUI: cleanup the circle and add buttons behind the circle

--- src/main/java/com/lothrazar/cyclicmagic/gui/GuiSpellbook.java
+++ src/main/java/com/lothrazar/cyclicmagic/gui/GuiSpellbook.java
@@ -6,9 +6,6 @@ import com.lothrazar.cyclicmagic.spell.ISpell;
 import com.lothrazar.cyclicmagic.util.UtilTextureRender;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.StatCollector;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -16,20 +13,55 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class GuiSpellbook extends GuiScreen {
 
 	private final EntityPlayer entityPlayer;
-
-	// public final ResourceLocation texture = new
-	// ResourceLocation(ModSamsContent.MODID,
-	// "textures/enderbook/textures/gui/book_ender.png" );
-
-	public GuiSpellbook(EntityPlayer entityPlayer) {
-		this.entityPlayer = entityPlayer;
+//https://github.com/LothrazarMinecraftMods/EnderBook/blob/66363b544fe103d6abf9bcf73f7a4051745ee982/src/main/java/com/lothrazar/enderbook/GuiEnderBook.java
+	private int xCenter;
+	private int yCenter;
+	private int radius;
+	private double arc;
+	PlayerPowerups props;
+	
+	public GuiSpellbook(EntityPlayer p) {
+		this.entityPlayer = p;
+		this.props = PlayerPowerups.get(entityPlayer);
 	}
 
 	@Override
 	public void initGui() {
 
-		// TODO: buttons to add/remove each spell from player rotation
 		super.initGui();
+		xCenter = this.width/2;
+		yCenter = this.height/2;
+		radius = xCenter/3 + 26;
+		// TODO: buttons to add/remove each spell from player rotation
+		
+		arc = (2*Math.PI)/SpellRegistry.spellbook.size();
+		
+		double ang = 0;
+		double cx,cy; 
+ 
+ 
+		ang = 0;
+		
+		//int spellSize = 16;
+		for (ISpell s : SpellRegistry.spellbook) {
+			
+			cx = xCenter + radius * Math.cos(ang) - 2;
+			cy = yCenter + radius * Math.sin(ang) - 2;
+			
+			//int buttonId, int x, int y, int widthIn, int heightIn, String buttonText)
+			this.buttonList.add(new GuiButtonSpell(s.getID(), (int)cx, (int)cy, ""));
+			
+			/*
+			UtilTextureRender.drawTextureSquare(s.getIconDisplay(), (int)cx, (int)cy, spellSize);
+	
+			if (s.getID() == props.getSpellCurrent()) {
+				//TODO: mark current spell with a highlight or some circle texture or something
+				drawCenteredString(fontRendererObj, "current", (int)cx, (int)cy, FONT);
+			}
+			*/
+			ang += arc;
+		}
+		
 	}
 
 	@Override
@@ -38,28 +70,14 @@ public class GuiSpellbook extends GuiScreen {
 
 		super.drawScreen(mouseX, mouseY, partialTicks);
 
-		/*
-		ItemStack wand = entityPlayer.getHeldItem();
-		if (wand.hasTagCompound() == false) {
-			wand.setTagCompound(new NBTTagCompound());
-		}*/
-
-		
-		int xCenter = this.width/2;
-		int yCenter = this.height/2;
+	
 		int FONT = 16777215;
 		
 		drawCenteredString(fontRendererObj, "test", xCenter,yCenter, FONT);
 		
-		int radius = xCenter/3;
-		
-		
-		double arc = (2*Math.PI)/SpellRegistry.spellbook.size();
-		
 		double ang = 0;
 		double cx,cy;
 	
-		PlayerPowerups props = PlayerPowerups.get(entityPlayer);
  
 		ang = 0;
 		
