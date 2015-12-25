f6efef0b7961acc4ed88cc8ee1d0b13ffade6ae0
sam bassett <samson.bassett@gmail.com>
2015-12-25 14:01:53 -0800

    spell renderer: move all over to the right, make distances relative and add one more layer down in the cycle

--- src/main/java/com/lothrazar/cyclicmagic/SpellScreenRender.java
+++ src/main/java/com/lothrazar/cyclicmagic/SpellScreenRender.java
@@ -10,20 +10,17 @@ import com.lothrazar.cyclicmagic.util.UtilTextureRender;
 public class SpellScreenRender {
 
 	private static final int ymain = 14;
-	private static final int xmain = 10;
+	private static final int xmain = 20;
 	private static final int spellSize = 16;
 
 	private static void drawSpellHeader(PlayerPowerups props, ISpell spellCurrent) {
-		int dim = 12;
-
-		int x = 12, y = 2;
-
-		// draw header
+		int dim = spellSize - 4, x = xmain+1, y = ymain-12;
+  
 		if (SpellCaster.isBlockedBySpellTImer(props) == false) {
-			UtilTextureRender.drawTextureSquare(spellCurrent.getIconDisplayHeaderEnabled(), x, y, dim);
+			UtilTextureRender.drawTextureSquare(spellCurrent.getIconDisplayHeaderEnabled(), x,y, dim);
 		}
 		else {
-			UtilTextureRender.drawTextureSquare(spellCurrent.getIconDisplayHeaderDisabled(), x, y, dim);
+			UtilTextureRender.drawTextureSquare(spellCurrent.getIconDisplayHeaderDisabled(), x,y, dim);
 		}
 	}
 
@@ -37,36 +34,38 @@ public class SpellScreenRender {
 
 	private static void drawPrevSpells(PlayerPowerups props, ISpell spellCurrent) {
 
-		// PlayerPowerups props = PlayerPowerups.get(player);
-
-		ISpell spellPrev = SpellRegistry.getSpellFromID(props.prevId(spellCurrent.getID()));
-		// SpellRegistry.right(spellCurrent);
-		if (spellPrev == null) {
-			System.out.println("spellPrev is null, prevId from " + spellCurrent.getID() + " -> " + props.prevId(spellCurrent.getID()));
-		}
-		if (spellPrev != null) {
+		ISpell prev = SpellRegistry.getSpellFromID(props.prevId(spellCurrent.getID()));
+  
+		if (prev != null) {
 			int x = xmain + 6;
 			int y = ymain + spellSize;
 			int dim = spellSize / 2;
-			UtilTextureRender.drawTextureSquare(spellPrev.getIconDisplay(), x, y, dim);
+			UtilTextureRender.drawTextureSquare(prev.getIconDisplay(), x, y, dim);
 
-			ISpell sRightRight = SpellRegistry.getSpellFromID(props.prevId(spellPrev.getID()));
-			// SpellRegistry.right(spellPrev);//
-			// SpellRegistry.getSpellFromType(spellPrev.getSpellID().prev());
+			prev = SpellRegistry.getSpellFromID(props.prevId(prev.getID()));
+		
+			if (prev != null ) {
+				x += 5;//11
+				y += 14;//14
+				dim -= 2;
+				UtilTextureRender.drawTextureSquare(prev.getIconDisplay(), x, y, dim);
 
-			if (sRightRight != null && sRightRight.getIconDisplay() != null) {
-				x = xmain + 6 + 4;
-				y = ymain + spellSize + 14;
-				dim = spellSize / 2 - 2;
-				UtilTextureRender.drawTextureSquare(sRightRight.getIconDisplay(), x, y, dim);
-
-				ISpell another = SpellRegistry.getSpellFromID(props.prevId(sRightRight.getID()));
-				// SpellRegistry.right(sRightRight);
-				if (another != null) {
-					x = xmain + 6 + 7;
-					y = ymain + spellSize + 14 + 10;
-					dim = spellSize / 2 - 4;
-					UtilTextureRender.drawTextureSquare(another.getIconDisplay(), x, y, dim);
+				prev = SpellRegistry.getSpellFromID(props.prevId(prev.getID()));
+	
+				if (prev != null) {
+					x += 3;
+					y += 10;
+					dim -= 2;
+					UtilTextureRender.drawTextureSquare(prev.getIconDisplay(), x, y, dim);
+					
+					prev = SpellRegistry.getSpellFromID(props.prevId(prev.getID()));
+					
+					if (prev != null) {
+						x += 2;
+						y += 10;
+						dim -= 1;
+						UtilTextureRender.drawTextureSquare(prev.getIconDisplay(), x, y, dim);
+					}
 				}
 			}
 		}
@@ -74,30 +73,36 @@ public class SpellScreenRender {
 
 	private static void drawNextSpells(PlayerPowerups props, ISpell spellCurrent) {
 
-		// PlayerPowerups props = PlayerPowerups.get(player);
-		ISpell spellNext = SpellRegistry.getSpellFromID(props.nextId(spellCurrent.getID()));
-		// SpellRegistry.left(spellCurrent);
+		ISpell next = SpellRegistry.getSpellFromID(props.nextId(spellCurrent.getID()));
 
-		if (spellNext != null) {
+		if (next != null) {
 			int x = xmain - 3;
 			int y = ymain + spellSize;
 			int dim = spellSize / 2;
-			UtilTextureRender.drawTextureSquare(spellNext.getIconDisplay(), x, y, dim);
+			UtilTextureRender.drawTextureSquare(next.getIconDisplay(), x, y, dim);
 
-			ISpell sLeftLeft = SpellRegistry.getSpellFromID(props.nextId(spellNext.getID()));// SpellRegistry.left(spellNext);
+			next = SpellRegistry.getSpellFromID(props.nextId(next.getID())); 
 
-			if (sLeftLeft != null && sLeftLeft.getIconDisplay() != null) {
-				x = xmain - 3 - 1;
-				y = ymain + spellSize + 14;
-				dim = 16 / 2 - 2;
-				UtilTextureRender.drawTextureSquare(sLeftLeft.getIconDisplay(), x, y, dim);
+			if (next != null ) {
+				x -= 2;
+				y += 14;
+				dim -= 2;
+				UtilTextureRender.drawTextureSquare(next.getIconDisplay(), x, y, dim);
 
-				ISpell another = SpellRegistry.getSpellFromID(props.nextId(sLeftLeft.getID()));// SpellRegistry.left(sLeftLeft);
-				if (another != null) {
-					x = xmain - 3 - 3;
-					y = ymain + spellSize + 14 + 10;
-					dim = spellSize / 2 - 4;
-					UtilTextureRender.drawTextureSquare(another.getIconDisplay(), x, y, dim);
+				next = SpellRegistry.getSpellFromID(props.nextId(next.getID())); 
+				if (next != null) {
+					x -= 2;
+					y += 10;
+					dim -= 2;
+					UtilTextureRender.drawTextureSquare(next.getIconDisplay(), x, y, dim);
+					
+					next = SpellRegistry.getSpellFromID(props.nextId(next.getID())); 
+					if (next != null) {
+						x -= 2;
+						y += 10;
+						dim -= 1;
+						UtilTextureRender.drawTextureSquare(next.getIconDisplay(), x, y, dim);
+					}
 				}
 			}
 		}
