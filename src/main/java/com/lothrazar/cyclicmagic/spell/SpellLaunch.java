959db0a34c86b6192315a5434b9bcd0498ab10ac
sam bassett <samson.bassett@gmail.com>
2015-12-26 09:26:50 -0800

    decrease launch power

--- src/main/java/com/lothrazar/cyclicmagic/spell/SpellLaunch.java
+++ src/main/java/com/lothrazar/cyclicmagic/spell/SpellLaunch.java
@@ -7,10 +7,13 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public class SpellLaunch extends BaseSpell implements ISpell {
+	
+	private static final float power = 1.1F;
+	
 	public SpellLaunch(int id,String name){
 		super(id,name);
 	}
-	private static final float power = 1.4F;
+	
 	@Override
 	public boolean cast(World world, EntityPlayer player, BlockPos pos, EnumFacing side ) {
 
@@ -20,34 +23,30 @@ public class SpellLaunch extends BaseSpell implements ISpell {
 		double velX;
 		double velY = (double) (-MathHelper.sin((player.rotationPitch) / 180.0F * (float) Math.PI) * power);
 		double velZ;
+
+		if (velY < 0) {
+			velY *= -1;// make it always up never down
+		}
 		
 		if(player.isSneaking()){
 			//then do vertical only
 			velX = 0;
 			velZ = 0;
-			if (velY < 0) {
-				velY *= -1;// make it always up never down
-			}
-			//power = 1.8F;
-			velY *= (power / 2);
+			
+			velY *= 2 ;//and pump up the power 
 		}
 		else{
 			velX = (double) (-MathHelper.sin(player.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * power);
-			velZ = (double) (MathHelper.cos(player.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * power);
+			velZ = (double) ( MathHelper.cos(player.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * power);
 			
 			// launch the player up and forward at minimum 30 degrees
 			// regardless of look vector
-			if (velY < 0) {
-				velY *= -1;// first invert direction
-			}
-			if (velY < 0.4) {
-				// if you are looking straight ahead, this is zero
 
+			if (velY < 0.4) {
 				velY = 0.4 + player.jumpMovementFactor;// do a bit of a
 			}
 		}
 
-		System.out.println(velX  +":"+ velY +":"+ velZ);
 		player.addVelocity(velX, velY, velZ);
 		
 		return true;
