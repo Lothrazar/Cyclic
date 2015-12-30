6fbd6cedd92d81670b41876b2d4bff82a50b116a
sam bassett <samson.bassett@gmail.com>
2015-12-30 13:37:45 -0800

    spell launch: remove holding shift variant, decrease power a tad

--- src/main/java/com/lothrazar/cyclicmagic/spell/SpellLaunch.java
+++ src/main/java/com/lothrazar/cyclicmagic/spell/SpellLaunch.java
@@ -8,7 +8,7 @@ import net.minecraft.world.World;
 
 public class SpellLaunch extends BaseSpell implements ISpell {
 	
-	private static final float power = 1.1F;
+	private static final float power = 1.005F;
 	
 	public SpellLaunch(int id,String name){
 		super(id,name);
@@ -21,33 +21,21 @@ public class SpellLaunch extends BaseSpell implements ISpell {
 		player.motionY = 0;
 		player.fallDistance = 0;
 		
-		double velX;
 		double velY = (double) (-MathHelper.sin((player.rotationPitch) / 180.0F * (float) Math.PI) * power);
-		double velZ;
 
 		if (velY < 0) {
 			velY *= -1;// make it always up never down
 		}
+		// launch the player up and forward at minimum angle
+		// regardless of look vector
 		
-		if(player.isSneaking()){
-			//then do vertical only
-			velX = 0;
-			velZ = 0;
-			
-			velY *= 2 ;//and pump up the power 
+		if (velY < 0.4) {
+			velY = 0.4 + player.jumpMovementFactor;
 		}
-		else{
-			velX = (double) (-MathHelper.sin(player.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * power);
-			velZ = (double) ( MathHelper.cos(player.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * power);
-			
-			// launch the player up and forward at minimum 30 degrees
-			// regardless of look vector
-
-			if (velY < 0.4) {
-				velY = 0.4 + player.jumpMovementFactor;// do a bit of a
-			}
-		}
-
+	
+		double velX = (double) (-MathHelper.sin(player.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * power);
+		double velZ = (double) ( MathHelper.cos(player.rotationYaw / 180.0F * (float) Math.PI) * MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * power);
+		
 		player.addVelocity(velX, velY, velZ);
 		
 		return true;
