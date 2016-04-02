8000c75cf3e27227f0b61bc13f48090b48f3fd91
sam bassett <samson.bassett@gmail.com>
2016-04-01 22:23:05 -0700

    instead, player heads obtainable by using one of each normal head

--- src/main/java/com/lothrazar/cyclicmagic/registry/RecipeRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/RecipeRegistry.java
@@ -1,5 +1,10 @@
 package com.lothrazar.cyclicmagic.registry;
 
+import com.lothrazar.cyclicmagic.util.Const;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 
 public class RecipeRegistry{
 
@@ -7,6 +12,15 @@ public class RecipeRegistry{
 		//todo:
 		// IHasRecipe
 		//loop on blocks/items, if they implement this then call
+		
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.skull,4,Const.skull_player),
+				new ItemStack(Items.skull,1,Const.skull_wither),
+				new ItemStack(Items.skull,1,Const.skull_skeleton),
+				new ItemStack(Items.skull,1,Const.skull_zombie),
+				new ItemStack(Items.skull,1,Const.skull_creeper)
+				);
+
 		
 		
 		//top logs recipe
