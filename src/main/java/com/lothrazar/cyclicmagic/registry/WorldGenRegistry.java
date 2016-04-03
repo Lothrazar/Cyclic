39e6a1ec9f0beee268e5af80c3e31178265b233c
sam bassett <samson.bassett@gmail.com>
2016-04-03 11:49:56 -0700

    add config and lang entries for nether gold

--- src/main/java/com/lothrazar/cyclicmagic/registry/WorldGenRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/WorldGenRegistry.java
@@ -1,21 +1,35 @@
 package com.lothrazar.cyclicmagic.registry;
 
+import com.lothrazar.cyclicmagic.util.Const;
 import com.lothrazar.cyclicmagic.world.gen.*;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 
 public class WorldGenRegistry{
+	
+	private static boolean oceanEnabled;
+	private static boolean netherGoldEnabled;
 
 	public static void syncConfig(Configuration config){
-
 		
+		String category = Const.MODCONF + "world_generation";
+		
+		config.setCategoryComment(category, "Tons of new recipes for existing blocks and items.  Bonemeal to undye wool; repeater and dispenser tweaks;  making player skulls out of the four mob heads...");
+
+		oceanEnabled  = config.getBoolean( "oceanEnabled", category,true,"Generate clay, sand, and dirt in the ocean instead of only gravel (like the old days)");
+		netherGoldEnabled  = config.getBoolean( "netherGoldEnabled",category, true,"Generate gold ore in netherrack");
 	}
 	
 	final static int weight = 0;
 	public static void register(){
 
-		GameRegistry.registerWorldGenerator(new WorldGenOcean(), weight);
-		GameRegistry.registerWorldGenerator(new WorldGenNetherGold(), weight);
+		if(oceanEnabled){
+			GameRegistry.registerWorldGenerator(new WorldGenOcean(), weight);
+		}
+		
+		if(netherGoldEnabled){
+			GameRegistry.registerWorldGenerator(new WorldGenNetherGold(), weight);
+		}
 	}
 }
