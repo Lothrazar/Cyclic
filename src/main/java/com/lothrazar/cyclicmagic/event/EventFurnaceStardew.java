33c4a7644c94427c48c6a84e3e1586c5fc2a791e
sam bassett <samson.bassett@gmail.com>
2016-04-03 23:24:45 -0700

    fix furnace event using wrong slot number for player

--- src/main/java/com/lothrazar/cyclicmagic/event/EventFurnaceStardew.java
+++ src/main/java/com/lothrazar/cyclicmagic/event/EventFurnaceStardew.java
@@ -14,114 +14,108 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.wrapper.InvWrapper;
 
-
 public class EventFurnaceStardew{
-	//inspired by stardew valley
 
-	//http://minecraft.gamepedia.com/Furnace
+	// inspired by stardew valley
+
+	// http://minecraft.gamepedia.com/Furnace
 	final static int SLOT_INPUT = 0;
 	final static int SLOT_FUEL = 1;
 	final static int SLOT_OUTPUT = 2;
 
 	@SubscribeEvent
 	public void onPlayerFurnace(PlayerInteractEvent event){
-		
-		
 
 		EntityPlayer entityPlayer = event.getEntityPlayer();
-		//ignore in creative// left clicking just breaks it anyway
+		// ignore in creative// left clicking just breaks it anyway
 		if(entityPlayer.capabilities.isCreativeMode){
 			return;
 		}
-		
+
 		BlockPos pos = event.getPos();
 		World worldObj = event.getWorld();
 		if(pos == null){
 			return;
 		}
 		ItemStack held = entityPlayer.getHeldItemMainhand();
-		int slot = entityPlayer.inventory.currentItem;
-		
+		int playerSlot = entityPlayer.inventory.currentItem;
+
 		if(held == null){
 			held = entityPlayer.getHeldItemOffhand();
-			//offhand slot is the highest number
-			slot = entityPlayer.inventory.getSizeInventory() - 1;
+			// offhand slot is the highest number
+			playerSlot = entityPlayer.inventory.getSizeInventory() - 1;
 		}
-	
+
 		TileEntity tile = worldObj.getTileEntity(pos);
-		 
+
 		if(tile instanceof TileEntityFurnace){
-			System.out.println("furnace");
-			TileEntityFurnace furnace = (TileEntityFurnace)tile;
- 
-			
+
+			TileEntityFurnace furnace = (TileEntityFurnace) tile;
+
 			if(held == null){
-				System.out.println("try empty output slot");
 				extractFurnaceOutput(furnace);
 			}
 			else if(isFuel(held)){
-				System.out.println("is valid for FUEL");
-				tryMergeStackIntoSlot(furnace,entityPlayer,held,SLOT_FUEL);
+				tryMergeStackIntoSlot(furnace, entityPlayer, playerSlot, SLOT_FUEL);
 			}
 			else if(canBeSmelted(held)){
-				System.out.println("is valid for top");
 
-				tryMergeStackIntoSlot(furnace,entityPlayer,held,SLOT_INPUT);
+				tryMergeStackIntoSlot(furnace, entityPlayer, playerSlot, SLOT_INPUT);
 			}
-			
-			//nope does not satisfy this
-			//boolean inf = furnace instanceof IItemHandlerModifiable;
+
+			// nope does not satisfy this
+			// boolean inf = furnace instanceof IItemHandlerModifiable;
 		}
 	}
-	
 
-	private void tryMergeStackIntoSlot(TileEntityFurnace furnace, EntityPlayer entityPlayer,ItemStack held, int slot){
+	private void tryMergeStackIntoSlot(TileEntityFurnace furnace, EntityPlayer entityPlayer, int playerSlot, int furnaceSlot){
 
-		System.out.println("tryMergeStackIntoSlot:"+slot);
-		ItemStack current = furnace.getStackInSlot(slot);
-	
+		ItemStack current = furnace.getStackInSlot(furnaceSlot);
+		ItemStack held = entityPlayer.inventory.removeStackFromSlot(playerSlot);
+
 		if(current == null){
-			//just done
-			furnace.setInventorySlotContents(slot, held.copy());
-			
-			System.out.println("dump all");
+			// just done
+			furnace.setInventorySlotContents(furnaceSlot, held.copy());
+
 			held = null;
-			entityPlayer.inventory.setInventorySlotContents(slot, null);
+			entityPlayer.inventory.setInventorySlotContents(playerSlot, null);
 		}
 		else{
-			
+
+			// merging updates the stack size numbers in both furnace and in players invo
 			UtilInventory.mergeItemsBetweenStacks(held, current);
 
-			System.out.println("mergeItemsBetweenStacks");
-			if(held == null || held.stackSize == 0){
-				System.out.println("set null after merge");
-				entityPlayer.inventory.setInventorySlotContents(slot, null);
+			// so now we just fix if something is size zero
+			if(held.stackSize == 0){
+				held = null;
 			}
+
+			entityPlayer.inventory.setInventorySlotContents(playerSlot, held);
+
 		}
-		
 
 		entityPlayer.inventory.markDirty();
-
 	}
-	
+
 	private void extractFurnaceOutput(TileEntityFurnace furnace){
 
-		ItemStack current = furnace.removeStackFromSlot(SLOT_OUTPUT); 
+		ItemStack current = furnace.removeStackFromSlot(SLOT_OUTPUT);
 		if(current != null){
 			UtilEntity.dropItemStackInWorld(furnace.getWorld(), furnace.getPos(), current);
 		}
 	}
 
-	
 	private boolean canBeSmelted(ItemStack input){
-		//we literally get the smelt recipe instance to test if it has one
+
+		// we literally get the smelt recipe instance to test if it has one
 		ItemStack itemstack = FurnaceRecipes.instance().getSmeltingResult(input);
-        return (itemstack != null);
+		return (itemstack != null);
 	}
 
 	private boolean isFuel(ItemStack input){
-		//how long does it burn for? zero means it isnt fuel
-        return TileEntityFurnace.getItemBurnTime(input) > 0;
+
+		// how long does it burn for? zero means it isnt fuel
+		return TileEntityFurnace.getItemBurnTime(input) > 0;
 	}
 
 }
