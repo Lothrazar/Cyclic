54e07167f77f365ce1838c72df86035d09ebc963
sam bassett <samson.bassett@gmail.com>
2016-04-04 20:41:41 -0700

    get plant crop works by reflection

--- src/main/java/com/lothrazar/cyclicmagic/BehaviorDispenserPlaceBlock.java
+++ src/main/java/com/lothrazar/cyclicmagic/BehaviorDispenserPlaceBlock.java
@@ -1,17 +1,16 @@
 package com.lothrazar.cyclicmagic;
 
+import com.lothrazar.cyclicmagic.util.UtilReflection;
 import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
 import net.minecraft.dispenser.IBlockSource;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemSeeds;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDispenser;
+import net.minecraft.block.state.IBlockState;
 
 
 public class BehaviorDispenserPlaceBlock extends BehaviorDefaultDispenseItem{
@@ -19,41 +18,43 @@ public class BehaviorDispenserPlaceBlock extends BehaviorDefaultDispenseItem{
 	@Override
 	public ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
 		World world = source.getWorld();
-		BlockPos pos = source.getBlockPos().offset(BlockDispenser.getFacing(source.getBlockMetadata()));
-		
-		Block plantedBlock = null;
-		//todo bet block from seed
-		//Items.wheat_seeds ;
-		
-		if(stack != null && stack.getItem() instanceof ItemSeeds){
-		/*dammit crops is private
-		 * 
-		 * public class ItemSeeds extends Item implements net.minecraftforge.common.IPlantable
-{
-    private Block crops;
-    */	
-			
-			stack.getItem().onItemUse(stack, null, world, pos, null, BlockDispenser.getFacing(source.getBlockMetadata()), 0, 0, 0);
-			/*net.minecraft.block.state.IBlockState state = worldIn.getBlockState(pos);
-        if (facing == EnumFacing.UP && playerIn.canPlayerEdit(pos.offset(facing), facing, stack) && state.getBlock().canSustainPlant(state, worldIn, pos, EnumFacing.UP, this) && worldIn.isAirBlock(pos.up()))
-        {
-            worldIn.setBlockState(pos.up(), this.crops.getDefaultState());
-            --stack.stackSize;
-            return EnumActionResult.SUCCESS;
-        }
-        else
-        {
-            return EnumActionResult.FAIL;
-        }*/
-		}
-		
-		
-		if(plantedBlock != null && plantedBlock.canPlaceBlockAt(world, pos)) {
-			world.setBlockState(pos, plantedBlock.getDefaultState());
-			stack.stackSize--;
-			return stack;
-		}
+		//we want to place in front of the dispenser - which is based on where its facing
+		BlockPos posForPlant = source.getBlockPos().offset(BlockDispenser.getFacing(source.getBlockMetadata()));
+		BlockPos posSoil = posForPlant.down();
 
+		if(stack != null && stack.getItem() instanceof ItemSeeds){
+			
+			ItemSeeds seed = (ItemSeeds)stack.getItem();
+			
+			/*dammit crops is private
+			 * 
+			 * public class ItemSeeds extends Item implements net.minecraftforge.common.IPlantable{
+	    private Block crops;
+	    //if that was public OR had a getter, we would just get and use that
+	    */	
+			
+
+			Block crop = UtilReflection.getFirstPrivateBlock(seed);
+			
+			if(crop != null){
+	    
+				//mimic exactly what onItemUse.onItemUse is doing
+				IBlockState state = world.getBlockState(posSoil);
+
+
+		        boolean canSustainPlant = state.getBlock().canSustainPlant(state, world, posSoil, EnumFacing.UP, seed);
+		  
+		        if (canSustainPlant && world.isAirBlock(posForPlant))
+		        {
+		            world.setBlockState(posForPlant, crop.getDefaultState());
+		            stack.stackSize--;
+
+
+		            return stack;
+		        }
+			}
+		}
+		
 		return super.dispenseStack(source, stack);
 	}
 }
