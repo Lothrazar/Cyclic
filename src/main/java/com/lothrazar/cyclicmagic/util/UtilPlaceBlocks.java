30c580a423838e3c99963e24e5a7d6c3860825ba
sam bassett <samson.bassett@gmail.com>
2016-04-05 20:47:24 -0700

    remove unused assets from deleted spells

--- src/main/java/com/lothrazar/cyclicmagic/util/UtilPlaceBlocks.java
+++ src/main/java/com/lothrazar/cyclicmagic/util/UtilPlaceBlocks.java
@@ -5,7 +5,6 @@ import java.util.ConcurrentModificationException;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.EnumFacing;
