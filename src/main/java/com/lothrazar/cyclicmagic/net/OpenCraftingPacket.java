ea70ba355c89fa0826b1039d3d3ef8dc7306de06
Sam <samson.bassett@gmail.com>
2016-04-08 00:34:07 -0700

    added a fake workbench. but somehow it has 9 horizontal slots so its the wrong type

--- src/main/java/com/lothrazar/cyclicmagic/net/OpenCraftingPacket.java
+++ src/main/java/com/lothrazar/cyclicmagic/net/OpenCraftingPacket.java
@@ -1,5 +1,6 @@
 package com.lothrazar.cyclicmagic.net;
 
+import com.lothrazar.cyclicmagic.gui.ContainerFakeWorkbench;
 import com.lothrazar.cyclicmagic.util.UtilInventorySort;
 
 import net.minecraft.inventory.ContainerWorkbench;
@@ -55,9 +56,11 @@ public class OpenCraftingPacket  implements IMessage , IMessageHandler<OpenCraft
          * Displays a GUI by ID. In order starting from id 0: Chest, Workbench, Furnace, Dispenser, Enchanting table,
          * Brewing stand, Villager merchant, Beacon, Anvil, Hopper, Dropper, Horse
          */
+
+		System.out.println("SPacketOpenWindow " + player.currentWindowId);
         // (int windowIdIn, String inventoryTypeIn, ITextComponent windowTitleIn, int slotCountIn, int entityIdIn)
-        player.playerNetServerHandler.sendPacket(new SPacketOpenWindow(player.currentWindowId, "Crafting", p.getDisplayName()));
-        player.openContainer = new ContainerWorkbench(player.inventory, player.worldObj, p.getPosition());
+        player.playerNetServerHandler.sendPacket(new SPacketOpenWindow(player.currentWindowId, "Crafting", p.getDisplayName(),9,player.getEntityId()));
+        player.openContainer = new ContainerFakeWorkbench(player.inventory, player.worldObj);
         player.openContainer.windowId = player.currentWindowId;
         player.openContainer.onCraftGuiOpened(player);
        // player.openContainer.addCraftingToCrafters(player);
