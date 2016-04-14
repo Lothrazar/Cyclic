b7a61e67467ee84825b30fb3e13e52bd8a217978
Sam <samson.bassett@gmail.com>
2016-04-13 20:33:52 -0700

    testing out new feature: reaching through signs and item frames to reach the item behind it. works for signs and banners but not yet item frames.. they are entities

--- src/main/java/com/lothrazar/cyclicmagic/event/EventEditSign.java
+++ src/main/java/com/lothrazar/cyclicmagic/event/EventEditSign.java
@@ -1,6 +1,11 @@
 package com.lothrazar.cyclicmagic.event;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntitySign;
@@ -9,6 +14,8 @@ import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 
 public class EventEditSign {
 
@@ -20,8 +27,36 @@ public class EventEditSign {
 		World worldObj = event.getWorld();
 		if (pos == null) { return; }
 		//if (entityPlayer.isSneaking()) { return; }
-
 		ItemStack held = event.getItemStack();//entityPlayer.getHeldItem(event.getHand());
+		
+		
+		 IBlockState state = event.getWorld().getBlockState(pos);
+		 if(state != null && entityPlayer.isSneaking() == false && 
+				 (state.getBlock() == Blocks.wall_sign 
+					 || state.getBlock() == Blocks.wall_banner
+				   || Item.getItemFromBlock(state.getBlock()) == Items.item_frame )){
+			 //but NOT standing sign or standing banner
+			 //?? paintings maybe?
+			 
+			 BlockPos posBehind = pos.offset(EnumFacing.getFront(state.getBlock().getMetaFromState(state)).getOpposite());
+			 
+			 IBlockState stuffBehind = worldObj.getBlockState(posBehind);
+			 
+			 if(stuffBehind != null && stuffBehind.getBlock() != null && worldObj.getTileEntity(posBehind) != null){
+				 //then perform the action on that thing (chest/furnace/etc)      
+				 
+				 //a function in base class of block
+				 // public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
+				 stuffBehind.getBlock().onBlockActivated(event.getWorld(), posBehind, stuffBehind, event.getEntityPlayer(), event.getHand(), held, event.getFace(), 0, 0, 0);
+
+				 //stop the normal item thing happening
+         event.setUseItem(net.minecraftforge.fml.common.eventhandler.Event.Result.DENY);
+         return;
+			 }
+		 }
+		//move above event to own class if it works
+		
+
 
 		TileEntity tile = worldObj.getTileEntity(pos);
 
