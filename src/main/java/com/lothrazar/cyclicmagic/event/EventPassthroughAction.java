5ac4b64663312bc4fb876666bed56ced61bbb19f
Sam <samson.bassett@gmail.com>
2016-04-13 21:09:54 -0700

    remove debugging and add formatting

--- src/main/java/com/lothrazar/cyclicmagic/event/EventPassthroughAction.java
+++ src/main/java/com/lothrazar/cyclicmagic/event/EventPassthroughAction.java
@@ -5,10 +5,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItemFrame;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -18,6 +15,9 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventPassthroughAction {
 	
+
+	//TODO: why does the chest not animate? or only sometimes?
+
 	@SubscribeEvent
 	public void onEntityInteractEvent(EntityInteract event) {
 
@@ -26,45 +26,24 @@ public class EventPassthroughAction {
 		Entity target = event.getTarget();
 		World worldObj = event.getWorld();
 
-		
-		if(target instanceof EntityItemFrame){
-			 System.out.println("EntityItemFrame");
+		if (target instanceof EntityItemFrame) {
+
 			BlockPos pos = target.getPosition();
-			//TileEntity tile = worldObj.getTileEntity(target.getPosition());
-			// IBlockState state = worldObj.getBlockState(pos);
-			
-		//	if(tile != null){
-				
-				
-			 EntityItemFrame frame = (EntityItemFrame)target;
-			 
-			
-				//EnumFacing.getFront(state.getBlock().getMetaFromState(state)
-			 EnumFacing face = frame.getAdjustedHorizontalFacing();
-				 BlockPos posBehind = pos.offset(  face.getOpposite() ).down();//; // 
 
-				 System.out.println("posBehind = "+posBehind);
-				 
-				 IBlockState stuffBehind = worldObj.getBlockState(posBehind);
-				 
-				 
-				 if(stuffBehind != null && stuffBehind.getBlock() != null){//  && worldObj.getTileEntity(posBehind) != null
-					 
-					 System.out.println("stuff is not null"+stuffBehind.getBlock().getUnlocalizedName());
-					 
-					 //then perform the action on that thing (chest/furnace/etc)      
-					 
-					 //a function in base class of block
-					 // public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
-					 // event.getFace()
-					 stuffBehind.getBlock().onBlockActivated(worldObj, posBehind, stuffBehind, entityPlayer, event.getHand(), held, face , 0, 0, 0);
+			EntityItemFrame frame = (EntityItemFrame) target;
 
-					 //stop the normal item thing happening
-	         //event.setCanceled(true);
-	         //.setUseItem(net.minecraftforge.fml.common.eventhandler.Event.Result.DENY);
-	     
-				 }
-			//}
+			EnumFacing face = frame.getAdjustedHorizontalFacing();
+			// why does it need to go down. not sure exactly. but it works
+			BlockPos posBehind = pos.offset(face.getOpposite()).down();
+
+			IBlockState stuffBehind = worldObj.getBlockState(posBehind);
+
+			if (stuffBehind != null && stuffBehind.getBlock() != null && worldObj.getTileEntity(posBehind) != null) {
+
+				stuffBehind.getBlock().onBlockActivated(worldObj, posBehind, stuffBehind, entityPlayer, event.getHand(), held, face, 0, 0, 0);
+
+				event.setCanceled(true);
+			}
 		}
 	}
 
@@ -75,35 +54,32 @@ public class EventPassthroughAction {
 		BlockPos pos = event.getPos();
 		World worldObj = event.getWorld();
 		if (pos == null) { return; }
-		//if (entityPlayer.isSneaking()) { return; }
-		ItemStack held = event.getItemStack();//entityPlayer.getHeldItem(event.getHand());
-		
-		
-		 IBlockState state = event.getWorld().getBlockState(pos);
-		 if(state != null && entityPlayer.isSneaking() == false && 
-				 (state.getBlock() == Blocks.wall_sign 
-					 || state.getBlock() == Blocks.wall_banner
-				   || Item.getItemFromBlock(state.getBlock()) == Items.item_frame )){
-			 //but NOT standing sign or standing banner
-			 //?? paintings maybe?
-			 
-			 BlockPos posBehind = pos.offset(EnumFacing.getFront(state.getBlock().getMetaFromState(state)).getOpposite());
-			 
-			 IBlockState stuffBehind = worldObj.getBlockState(posBehind);
-			 
-			 if(stuffBehind != null && stuffBehind.getBlock() != null && worldObj.getTileEntity(posBehind) != null){
-				 //then perform the action on that thing (chest/furnace/etc)      
-				 
-				 //a function in base class of block
-				 // public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
-				 stuffBehind.getBlock().onBlockActivated(worldObj, posBehind, stuffBehind, entityPlayer, event.getHand(), held, event.getFace(), 0, 0, 0);
+		// if (entityPlayer.isSneaking()) { return; }
+		ItemStack held = event.getItemStack();// entityPlayer.getHeldItem(event.getHand());
 
-				 //stop the normal item thing happening
-         event.setUseItem(net.minecraftforge.fml.common.eventhandler.Event.Result.DENY);
-     
-			 }
-		 }
-		//move above event to own class if it works
-		
+		IBlockState state = event.getWorld().getBlockState(pos);
+		if (state != null && entityPlayer.isSneaking() == false && (state.getBlock() == Blocks.wall_sign || state.getBlock() == Blocks.wall_banner)) {
+			// but NOT standing sign or standing banner
+	
+			EnumFacing face = EnumFacing.getFront(state.getBlock().getMetaFromState(state));
+			BlockPos posBehind = pos.offset(face.getOpposite());
+
+			IBlockState stuffBehind = worldObj.getBlockState(posBehind);
+
+			if (stuffBehind != null && stuffBehind.getBlock() != null && worldObj.getTileEntity(posBehind) != null) {
+				// then perform the action on that thing (chest/furnace/etc)
+
+				// a function in base class of block
+				// public boolean onBlockActivated(World worldIn, BlockPos pos,
+				// IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack
+				// heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
+				stuffBehind.getBlock().onBlockActivated(worldObj, posBehind, stuffBehind, entityPlayer, event.getHand(), held, event.getFace(), 0, 0, 0);
+
+				
+				// stop the normal item thing happening
+				event.setUseItem(net.minecraftforge.fml.common.eventhandler.Event.Result.DENY);
+
+			}
+		}
 	}
 }
