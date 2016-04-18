b4a61c65cea3e828130c72228bf1c37e336b4fca
Sam <samson.bassett@gmail.com>
2016-04-17 18:37:40 -0700

    divide up the trades: profession is elder, with two careers: sage and druid

--- src/main/java/com/lothrazar/cyclicmagic/registry/VillageTradeRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/VillageTradeRegistry.java
@@ -17,9 +17,17 @@ import net.minecraftforge.fml.common.registry.VillagerRegistry.VillagerProfessio
 
 public class VillageTradeRegistry {
 
-	public static void register(){
+	final static String sage = Const.MODRES+"textures/entity/villager/elder.png";
+	final static ResourceLocation sageTexture = new ResourceLocation(sage);
+	private static VillagerProfession  elderProfession;
 
-		//TOO TEST: /summon Villager ~ ~ ~ {Profession:5}
+	public static void register(){
+		elderProfession = new VillagerProfession(Const.MODRES+"elder", sage){
+				@Override
+		    public ResourceLocation getSkin() {
+					return sageTexture;
+				}
+			};
 		//kill: /kill @e[type=Villager]
 		//CONFIRMED: IT does spawn randomly when using default villager eggs
 		
@@ -33,90 +41,80 @@ public class VillageTradeRegistry {
     }
     //that %5 needs to be removed, it forces max of 4 profs
     */
-		String sage = Const.MODRES+"textures/entity/villager/sage.png";
-		final ResourceLocation sageTexture = new ResourceLocation(sage);
-		
-		VillagerProfession prof = new VillagerProfession(Const.MODRES+"sage", sage){
-			@Override
-      public ResourceLocation getSkin() {
 
-			//	System.out.println("getSkin"+this.getSkin().getResourceDomain()+"_"+this.getSkin().getResourcePath());
-				return sageTexture;//super.getSkin();
-			}
-		}
-		;
+		//	System.out.println("getSkin"+this.getSkin().getResourceDomain()+"_"+this.getSkin().getResourcePath());
+
      
-		
-		VillagerRegistry.instance().register(prof);
-		
-	//	VillagerProfession test = prof = net.minecraftforge.fml.common.registry.VillagerRegistry.instance().getRegistry().getValue(new ResourceLocation(Const.MODRES+"sage"));
+		VillagerRegistry.instance().register(elderProfession);
+
+		//TOO TEST: /summon Villager ~ ~ ~ {Profession:5,Career:0}
+		addVillagerSage();
+
+		//TOO TEST: /summon Villager ~ ~ ~ {Profession:5,Career:1}
+		addVillagerDruid();
 		 
-		//System.out.println("test isNull :"+ (test == null));
-		
-		 
-		 final EntityVillager.ITradeList[][] sageTrades = {
-				 
-				 {new EmeraldForItems(Items.ender_pearl, new PriceInfo(16, 16)) 	 },// New to mod
-			
-				 {new EmeraldForItems(Items.beetroot, new PriceInfo(8, 12)) 	 },//New to mod
-				 
-				 {new EmeraldForItems(Items.wheat_seeds, new PriceInfo(64, 64))  },//New to mod
-				
-				 {new EmeraldForItems(Items.bone, new PriceInfo(32, 32)) }, //New to mod
-				 
-				 {new EmeraldForItems(Items.gunpowder, new PriceInfo(5, 5))  },//New to mod
-						
-				 {new EmeraldForItems(Items.feather, new PriceInfo(16, 16))  },//New to mod		
-				 {new EmeraldForItems(Items.nether_wart, new PriceInfo(16, 16))  },//New to mod			
-				 {new EmeraldForItems(Items.mutton, new PriceInfo(16, 16))  },//New to mod			
-				 
-				 {new EmeraldForItems(Items.blaze_rod, new PriceInfo(16, 16))  },//New to mod
-				
-				 {new EmeraldForItems(Item.getItemFromBlock(Blocks.brown_mushroom), new PriceInfo(16, 16)) },//New to mod
-				 
-				 {new EmeraldForItems(Item.getItemFromBlock(Blocks.red_mushroom), new PriceInfo(16, 16))  },	//New to mod
-									 
-				 {new EmeraldForItems(Items.blaze_rod, new PriceInfo(16, 16))  },//New to mod
-								 
-				 {new EmeraldForItems(Items.slime_ball, new PriceInfo(16,16)) }, //New to mod				
-				 
-				 {new EmeraldForItems(Items.poisonous_potato, new PriceInfo(3, 3))  },//New to mod				
-				 
-				 {new EmeraldForItems(Items.spider_eye, new PriceInfo(9, 9))  },//New to mod				
-				 
-				 {new EmeraldForItems(Items.ghast_tear, new PriceInfo(2, 2)) },//New to mod				 
-				 
-				 {new EmeraldForItems(Items.diamond, new PriceInfo(9, 9)) },//New to mod, 9 emeralds = 1 diamond
-				 				 
-				 {new EmeraldForItems(Items.cooked_fish, new PriceInfo(9, 12)) },//removed from Farmer, not in 1.9
-								 
-				 {new ListItemForEmeralds(Items.apple, new PriceInfo(5,5)) },//Refund at most				
-				 
-				 {new ListItemForEmeralds(Items.chicken, new PriceInfo(14,17))	 },//is no longer in farmer
-			
-				 {new EmeraldForItems(Items.beef, new PriceInfo(14, 17)) },// removed from Butcher, not in 1.9
-				 {new EmeraldForItems(Items.rabbit, new PriceInfo(14, 17)) },// NEW to mod
-				
-				 {new EmeraldForItems(Items.experience_bottle, new PriceInfo(2, 4)) },// removed from Priest, not in 1.9 (better deal V has 3-11  emeralds for 1 bottle)
-				
-				 {new EmeraldForItems(Items.redstone, new PriceInfo(4,4)) },// REFUND at most
-				
-				 {new EmeraldForItems(Items.glowstone_dust, new PriceInfo(12,12))}// REFUND at most, since we can buy 1emerald = 3blocks = 12 dust
-			
-		 };
-		 
-		// VillagerProfession libr =	VillagerRegistry.instance().getRegistry().getValue(new ResourceLocation("minecraft:librarian"));
-			
-		  new VillagerCareer(prof, "sage_career"){
+	}
+
+	private static void addVillagerSage() {
+
+		  new VillagerCareer(elderProfession, "sage_career"){
 			 @Override
-       public ITradeList[][] getTrades()
-       {
+       public ITradeList[][] getTrades(){
            return sageTrades;
        }
 		 };
-		 
-		 
 	}
+	private static void addVillagerDruid() {
+
+	  new VillagerCareer(elderProfession, "druid_career"){
+		 @Override
+     public ITradeList[][] getTrades(){
+         return druidTrades;
+     }
+	 };
+		
+	}
+
+  final static EntityVillager.ITradeList[][] druidTrades = {
+
+ 		 {	new EmeraldForItems(Items.cooked_fish, new PriceInfo(9, 12))
+ 			 ,new EmeraldForItems(Items.apple, new PriceInfo(3,6)) 
+ 			 ,new EmeraldForItems(Items.beetroot, new PriceInfo(8, 12)) },
+ 		 
+		 {	new EmeraldForItems(Items.feather, new PriceInfo(12, 13)) 
+			 ,new EmeraldForItems(Items.wheat_seeds, new PriceInfo(50, 64))  },
+		 
+		 {	new EmeraldForItems(Item.getItemFromBlock(Blocks.brown_mushroom), new PriceInfo(8, 12))
+			 ,new EmeraldForItems(Item.getItemFromBlock(Blocks.red_mushroom), new PriceInfo(8, 12)) },
+		 
+		 {	new EmeraldForItems(Items.beef, new PriceInfo(14, 17))
+			 ,new EmeraldForItems(Items.rabbit, new PriceInfo(14, 17))
+			 ,new EmeraldForItems(Items.chicken, new PriceInfo(14,17))	 },
+		 
+			 {	new EmeraldForItems(Items.poisonous_potato, new PriceInfo(1, 3))
+				 ,new EmeraldForItems(Items.spider_eye, new PriceInfo(3, 6))}
+	 };
+	
+  final static EntityVillager.ITradeList[][] sageTrades = {
+		 
+		 {	new EmeraldForItems(Items.gunpowder, new PriceInfo(5, 8))
+			 ,new EmeraldForItems(Items.nether_wart, new PriceInfo(12, 16))},
+		 
+		 {	new EmeraldForItems(Items.bone, new PriceInfo(26, 32))
+			 ,new EmeraldForItems(Items.mutton, new PriceInfo(12, 16))},
+		 
+		 {	new EmeraldForItems(Items.blaze_rod, new PriceInfo(12, 16))
+			 ,new EmeraldForItems(Items.slime_ball, new PriceInfo(12,16))},
+		 
+		 {	new EmeraldForItems(Items.ghast_tear, new PriceInfo(2, 3))
+			 ,new EmeraldForItems(Items.redstone, new PriceInfo(4,6)) 
+			 },	
+		 {	new EmeraldForItems(Items.glowstone_dust, new PriceInfo(12,16))
+			 ,new ListItemForEmeralds(Items.experience_bottle, new PriceInfo(2, 4))
+			 ,new EmeraldForItems(Items.diamond, new PriceInfo(1, 1))
+			 ,new EmeraldForItems(Items.ender_pearl, new PriceInfo(12, 16))}
+	 };
+	
 	public static void syncConfig(Configuration c){
 		
 	}
