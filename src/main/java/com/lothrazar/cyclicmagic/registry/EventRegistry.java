74c7cf1adec2694a47f64a91e76652e3d32fab29
Sam <samson.bassett@gmail.com>
2016-05-07 19:49:12 -0700

    sort event constructors by name so i know none are missing

--- src/main/java/com/lothrazar/cyclicmagic/registry/EventRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/EventRegistry.java
@@ -14,38 +14,33 @@ public class EventRegistry {
 		featureEvents.add(new EventAppleUse());
 		featureEvents.add(new EventBucketBlocksBreak());
 		featureEvents.add(new EventConfigChanged());
-		featureEvents.add(new EventSaplingBlockGrowth());
-		featureEvents.add(new EventPotions());
-		featureEvents.add(new EventSpells());
-		featureEvents.add(new EventKeyInput());
-		featureEvents.add(new EventHorseFood());
-		featureEvents.add(new EventGuiTerrariaButtons());
-		featureEvents.add(new EventExtendedInventory());
-		featureEvents.add(new EventOreMined());
-		featureEvents.add(new EventMounted());
-		featureEvents.add(new EventSpawnChunks());
-		featureEvents.add(new EventPassthroughAction());
-		featureEvents.add(new EventEnderChest());
-		featureEvents.add(new EventEntityItemExpire());
-
-		featureEvents.add(new EventLadderClimb());
-		
-		featureEvents.add(new EventFoodDetails());
-		
-		featureEvents.add(new EventFragileTorches());
-		
-		featureEvents.add(new EventFurnaceStardew());
-		 
-		featureEvents.add(new EventMountedPearl());
-		featureEvents.add(new EventMobDropsReduced());
-
-		featureEvents.add(new EventNametagDeath());
 		featureEvents.add(new EventEditSign());
-		featureEvents.add(new EventNameVillager());
+		featureEvents.add(new EventEnderChest());
 		featureEvents.add(new EventEndermanDropBlock());
+		featureEvents.add(new EventEntityItemExpire());
+		featureEvents.add(new EventExtendedInventory());
+		featureEvents.add(new EventFoodDetails());	
+		featureEvents.add(new EventFragileTorches());		
+		featureEvents.add(new EventFurnaceStardew());	
+		//EventGuiInventory is done in client proxy
+		featureEvents.add(new EventGuiTerrariaButtons());	
+		featureEvents.add(new EventHorseFood());
+		featureEvents.add(new EventKeyInput());
+		featureEvents.add(new EventLadderClimb());	
+		featureEvents.add(new EventMobDropsReduced());
+		featureEvents.add(new EventMounted());	
+		featureEvents.add(new EventMountedPearl());
+		featureEvents.add(new EventNametagDeath());
+		featureEvents.add(new EventNameVillager());
+		featureEvents.add(new EventOreMined());
+		featureEvents.add(new EventPassthroughAction()); 
 		featureEvents.add(new EventPlayerDeathCoords());
 		featureEvents.add(new EventPlayerWakeup());
+		featureEvents.add(new EventPotions());
+		featureEvents.add(new EventSaplingBlockGrowth());
 		featureEvents.add(new EventSignSkullName());
+		featureEvents.add(new EventSpawnChunks());
+		featureEvents.add(new EventSpells());
 	}
  
 	public void syncConfig(Configuration config) {
