57900502d2ab814bb9f6c55a8126fb3a5e717049
Sam Bassett <samson.bassett@gmail.com>
2016-05-26 00:19:50 -0700

    only sleep at night, with message

--- src/main/java/com/lothrazar/cyclicmagic/item/ItemSleepingBag.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/ItemSleepingBag.java
@@ -6,7 +6,9 @@ import com.lothrazar.cyclicmagic.ModMain;
 import com.lothrazar.cyclicmagic.registry.CapabilityRegistry;
 import com.lothrazar.cyclicmagic.registry.CapabilityRegistry.IPlayerExtendedProperties;
 import com.lothrazar.cyclicmagic.util.Const;
+import com.lothrazar.cyclicmagic.util.UtilChat;
 import com.lothrazar.cyclicmagic.util.UtilInventory;
+import com.lothrazar.cyclicmagic.util.UtilWorld;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -19,30 +21,25 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.common.config.Configuration;
 
-public class ItemSleepingBag extends ItemFood  implements IHasRecipe,IHasConfig{
+public class ItemSleepingBag extends Item  implements IHasRecipe,IHasConfig{
 	// thank you for the examples forge. player data storage based on API source
 	// code example:
 	// !!
 	// https://github.com/MinecraftForge/MinecraftForge/blob/1.9/src/test/java/net/minecraftforge/test/NoBedSleepingTest.java
 
-	private static final int numFood = 1;
-	
 	//public static final ItemSleepingBag instance = new ItemSleepingBag();
 	//public static final String name = "sleeping_bag";
 
 	public ItemSleepingBag() {
-		super(numFood, false);  
-		this.setAlwaysEdible();
+		super();  
+		this.setMaxStackSize(1);
 		setCreativeTab(ModMain.TAB);
 		//setUnlocalizedName(Const.MODID + ":" + name);
 		//setRegistryName(new ResourceLocation(Const.MODID, name));
 	}
 
-	//@Override
-	//public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer player, EnumHand hand) {
-
 	@Override
-	protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player) {
+	public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer player, EnumHand hand) {	
 		if (!world.isRemote) {
 			final EntityPlayer.SleepResult result = player.trySleep(player.getPosition());
 			
@@ -51,21 +48,26 @@ public class ItemSleepingBag extends ItemFood  implements IHasRecipe,IHasConfig{
 				//final IPlayerExtendedProperties sleep = player.getCapability(ModMain.CAPABILITYSTORAGE, null);
 				final IPlayerExtendedProperties sleep = CapabilityRegistry.getPlayerProperties(player);
 				if (sleep != null) {
-					sleep.setSleeping(true);
-
-					stack.stackSize--;
-					
-					if(stack.stackSize == 0){
-						stack = null;
+					if(UtilWorld.isNight(world) == false){
+						UtilChat.addChatMessage(player, "tile.bed.noSleep");
+					}
+					else{
+						sleep.setSleeping(true);
+	
+						stack.stackSize--;
+						
+						if(stack.stackSize == 0){
+							stack = null;
+						}
 					}
 				}
 				else{
 					ModMain.logger.error("NULL IPlayerExtendedProperties found");
 				}
-				return;// ActionResult.newResult(EnumActionResult.SUCCESS, stack);
+				return ActionResult.newResult(EnumActionResult.SUCCESS, stack);
 			}
 		}
-		//return; ActionResult.newResult(EnumActionResult.PASS, stack);
+		return ActionResult.newResult(EnumActionResult.PASS, stack);
 	}
 
 	@Override
---
+++ src/main/java/com/lothrazar/cyclicmagic/util/UtilWorld.java
@@ -0,0 +1,16 @@
+package com.lothrazar.cyclicmagic.util;
+
+import net.minecraft.world.World;
+
+public class UtilWorld {
+
+	public static boolean isNight(World world){
+		long t = world.getWorldTime();
+		
+		int timeOfDay = (int)t%24000;
+		
+		System.out.println(timeOfDay);
+		
+		return timeOfDay > 12000;
+	}
+}
