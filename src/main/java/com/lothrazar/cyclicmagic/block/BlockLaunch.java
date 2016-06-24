d9a5e99c5f8a14fe7b280569dba16f98f173fe64
Sam Bassett <samson.bassett@gmail.com>
2016-06-23 19:14:45 -0700

    update slime pad sounds and mapcolor to match block

--- src/main/java/com/lothrazar/cyclicmagic/block/BlockLaunch.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/BlockLaunch.java
@@ -1,8 +1,8 @@
 package com.lothrazar.cyclicmagic.block;
-
 import com.lothrazar.cyclicmagic.util.UtilEntity;
-
 import net.minecraft.block.BlockBasePressurePlate;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
@@ -14,55 +14,39 @@ import net.minecraft.world.World;
 import net.minecraft.util.SoundEvent;
 
 public class BlockLaunch extends BlockBasePressurePlate {
-
-	private final static float angle = 90;
+	private final static float ANGLE = 90;
 	private float power;
 	private SoundEvent sound;
 	public BlockLaunch(float p, SoundEvent s) {
-		super(Material.WOOD);
+		super(Material.CLAY, MapColor.GRASS);//same as BlockSlime
+		this.setSoundType(SoundType.SLIME);
 		power = p;
 		sound = s;
 	}
-
 	@Override
 	protected void playClickOnSound(World worldIn, BlockPos pos) {
-
-		//SoundEvents.BLOCK_WOOD_PRESSPLATE_CLICK_ON
-		worldIn.playSound((EntityPlayer) null, pos, this.sound, SoundCategory.BLOCKS,0.3F, 0.5F);
-
+		worldIn.playSound((EntityPlayer) null, pos, this.sound, SoundCategory.BLOCKS, 0.3F, 0.5F);
 	}
-
 	@Override
 	protected void playClickOffSound(World worldIn, BlockPos pos) {
-
-//		worldIn.playSound((EntityPlayer) null, pos, SoundEvents.BLOCK_STONE_PRESSPLATE_CLICK_OFF, SoundCategory.BLOCKS,0.3F, 0.5F);
-
 	}
-
 	@Override
 	protected int computeRedstoneStrength(World worldIn, BlockPos pos) {
 		return 0;
 	}
-
 	@Override
 	protected int getRedstoneStrength(IBlockState state) {
 		return 0;
 	}
-
 	@Override
 	protected IBlockState setRedstoneStrength(IBlockState state, int strength) {
 		return null;
 	}
-
-
 	@Override
 	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entity) {
-
-		if (entity instanceof EntityLivingBase){
-			UtilEntity.launch((EntityLivingBase) entity, angle, power);
-			
+		if (entity instanceof EntityLivingBase) {
+			UtilEntity.launch((EntityLivingBase) entity, ANGLE, power);
 			this.playClickOnSound(worldIn, pos);
 		}
 	}
-
 }
