e5fa69645bc2a61bcd9c865b2517083c745bc944
Sam Bassett <samson.bassett@gmail.com>
2016-06-25 14:55:29 -0700

    code changes in 1.10: dispenser use IPosition instead of BlockPos. enum GameType has changed namespace location

--- src/main/java/com/lothrazar/cyclicmagic/util/UtilWorld.java
+++ src/main/java/com/lothrazar/cyclicmagic/util/UtilWorld.java
@@ -1,4 +1,6 @@
 package com.lothrazar.cyclicmagic.util;
+import net.minecraft.dispenser.IPosition;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class UtilWorld {
@@ -7,5 +9,9 @@ public class UtilWorld {
     int timeOfDay = (int) t % 24000;
     System.out.println(timeOfDay);
     return timeOfDay > 12000;
+  }
+
+  public static BlockPos convertIposToBlockpos(IPosition here) {
+    return new BlockPos(here.getX(),here.getY(),here.getZ());
   }
 }
