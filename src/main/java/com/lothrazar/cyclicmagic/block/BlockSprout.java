d8faaa57d638eb7140f22761217ba21219c2f79d
Sam Bassett <samson.bassett@gmail.com>
2016-06-29 19:17:30 -0700

    block seeds from dropping if sprout is full grown

--- src/main/java/com/lothrazar/cyclicmagic/block/BlockSprout.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/BlockSprout.java
@@ -1,4 +1,6 @@
 package com.lothrazar.cyclicmagic.block;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import com.lothrazar.cyclicmagic.registry.ItemRegistry;
 import net.minecraft.block.BlockCrops;
@@ -6,15 +8,17 @@ import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockSprout extends BlockCrops {
   public static final PropertyInteger AGE = PropertyInteger.create("age", 0, 7);
   private static final AxisAlignedBB[] AABB = new AxisAlignedBB[] { new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.1875D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.25D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.3125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.375D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.4375D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5625D, 1.0D) };
   private Random rand = new Random();
-  private static final Item[] drops = new Item[] { Items.REDSTONE, Items.GUNPOWDER,Items.ENDER_PEARL,Items.BLAZE_POWDER,Items.ROTTEN_FLESH,Items.BONE,Items.STRING,Items.SPIDER_EYE,Items.SUGAR,Items.WHEAT,Items.FLINT,Items.BRICK,Items.CLAY_BALL,Items.COAL,Items.GOLD_NUGGET,Items.APPLE,Items.EMERALD,Items.STICK };
+  private static final Item[] drops = new Item[] { Items.REDSTONE, Items.GUNPOWDER, Items.ENDER_PEARL, Items.BLAZE_POWDER, Items.ROTTEN_FLESH, Items.BONE, Items.STRING, Items.SPIDER_EYE, Items.SUGAR, Items.WHEAT, Items.FLINT, Items.BRICK, Items.CLAY_BALL, Items.COAL, Items.GOLD_NUGGET, Items.APPLE, Items.EMERALD, Items.STICK };
   protected Item getSeed() {
     return ItemRegistry.sprout_seed;
   }
@@ -23,5 +27,20 @@ public class BlockSprout extends BlockCrops {
   }
   public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
     return AABB[((Integer) state.getValue(this.getAgeProperty())).intValue()];
+  }
+  @Override
+  public java.util.List<ItemStack> getDrops(net.minecraft.world.IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+    //override getdrops so it never drops seeds IF its fully grown.
+    //should be seed if its young
+    java.util.List<ItemStack> ret = new ArrayList<ItemStack>();//super.getDrops(world, pos, state, fortune);
+    Random rand = world instanceof World ? ((World) world).rand : new Random();
+    int count = quantityDropped(state, fortune, rand);
+    for (int i = 0; i < count; i++) {
+      Item item = this.getItemDropped(state, rand, fortune);
+      if (item != null) {
+        ret.add(new ItemStack(item, 1, 0));//this.damageDropped(state)
+      }
+    }
+    return ret;
   }
 }
