29ecbfe694da66dfce70ee2dd2308b9feeeaff06
Sam Bassett <samson.bassett@gmail.com>
2016-07-02 21:12:25 -0700

    add ender pearl  to stacksize

--- src/main/java/com/lothrazar/cyclicmagic/registry/StackSizeRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/StackSizeRegistry.java
@@ -5,13 +5,10 @@ import com.lothrazar.cyclicmagic.util.Const;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.common.config.Property;
 
 public class StackSizeRegistry {
- // private static boolean enabled = true;
   private static Map<Item, Integer> stackMap = new HashMap<Item, Integer>();
   private static Map<Item, Integer> enabledMap = new HashMap<Item, Integer>();
-
   public static void construct() {
     int boat = 16;
     int MAX = 64;
@@ -32,31 +29,22 @@ public class StackSizeRegistry {
     stackMap.put(Items.ARMOR_STAND, MAX);
     stackMap.put(Items.SIGN, MAX);
     stackMap.put(Items.BED, MAX);
+    stackMap.put(Items.ENDER_PEARL, MAX);
     stackMap.put(Items.BUCKET, MAX);
-//    for (Map.Entry<Item, Integer> entry : stackMap.entrySet()) {
-//      all += entry.getKey().getUnlocalizedName() + ",";
-//    }
-//    all.replace(all.substring(all.length() - 1), "");
   }
   public static void register() {
-
-
     for (Map.Entry<Item, Integer> entry : stackMap.entrySet()) {
       boolean enabled = (enabledMap.get(entry.getKey()) == 1);
-      if(enabled){
+      if (enabled) {
         entry.getKey().setMaxStackSize(entry.getValue());
       }
     }
   }
   public static void syncConfig(Configuration config) {
     String category = Const.ConfigCategory.itemsTack;
-    // config.setCategoryComment(category, "Tons of new recipes for existing
-    // blocks and items. Bonemeal to undye wool; repeater and dispenser tweaks;
-    // making player skulls out of the four mob heads...");
-   
     for (Map.Entry<Item, Integer> entry : stackMap.entrySet()) {
       String name = entry.getKey().getUnlocalizedName();
-      int enabled = config.getBoolean(name, category, true, "Increase stack size to "+entry.getValue()) ? 1 : 0;
+      int enabled = config.getBoolean(name, category, true, "Increase stack size to " + entry.getValue()) ? 1 : 0;
       enabledMap.put(entry.getKey(), enabled);
     }
   }
