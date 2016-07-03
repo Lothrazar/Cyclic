04247b0061d92453964fb060795152a1e3899cab
Sam Bassett <samson.bassett@gmail.com>
2016-07-02 22:05:06 -0700

    update todolist

--- src/main/java/com/lothrazar/cyclicmagic/ModMain.java
+++ src/main/java/com/lothrazar/cyclicmagic/ModMain.java
@@ -146,14 +146,8 @@ public class ModMain {
    * 
    * TODO:
    * 
-   * heart containers do not persist on death..?? do we need a sethealth clientside?
-   * 
    * enderpearl hotkey feature-just like old overpoweredinventory mod
    * here : https://raw.githubusercontent.com/LothrazarMinecraftMods/OverpoweredInventory/8a7459161837b930c5417f774676504bce970e66/src/main/java/com/lothrazar/powerinventory/net/EnderPearlPacket.java
-   * 
-   * remove 'falling' restriction on doublejump chant.
-   * 
-   * hearts persist death?
    * 
    *  is there a bug in searchrecipe? or a bug in new repeater rec?
    * 
@@ -174,8 +168,6 @@ public class ModMain {
    * ButtonBuildToggle-store ptr to wand not player??
    * 
    * lifeleech enchant - like diablo 2 ? maybe only on kill?
-   * 
-   * conveyor belt type item: copy slime pad but make the entity go in a given direction
    * 
    * [ Trading Tool // gui]
    * Upgrade villager gui: either make my own or add buttons/some way to view all trades at once
