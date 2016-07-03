e1d55a3e1a7baa33dbf229c0a3ff252749aeda48
Sam Bassett <samson.bassett@gmail.com>
2016-07-02 23:37:11 -0700

    barebones lifeleeche nchant. currently only on death

--- src/main/java/com/lothrazar/cyclicmagic/registry/EnchantRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/EnchantRegistry.java
@@ -1,5 +1,6 @@
 package com.lothrazar.cyclicmagic.registry;
 import com.lothrazar.cyclicmagic.enchantment.EnchantLaunch;
+import com.lothrazar.cyclicmagic.enchantment.EnchantLifeLeech;
 import com.lothrazar.cyclicmagic.enchantment.EnchantMagnet;
 import com.lothrazar.cyclicmagic.enchantment.EnchantVenom;
 import com.lothrazar.cyclicmagic.util.Const;
@@ -11,9 +12,11 @@ public class EnchantRegistry {
   public static EnchantLaunch launch;
   public static EnchantMagnet magnet;
   public static EnchantVenom venom;
+  public static EnchantLifeLeech lifeleech;
   public static int launchid;
   public static int magnetid;
   public static int venomid;
+  public static int lifeleechid=89;
   public static void register() {
     launch = new EnchantLaunch();
     magnet = new EnchantMagnet();
@@ -21,7 +24,8 @@ public class EnchantRegistry {
     Enchantment.REGISTRY.register(launchid, new ResourceLocation(launch.getName()), launch);
     Enchantment.REGISTRY.register(magnetid, new ResourceLocation(magnet.getName()), magnet);
     Enchantment.REGISTRY.register(venomid, new ResourceLocation(venom.getName()), venom);
-  }
+    Enchantment.REGISTRY.register(lifeleechid, new ResourceLocation(lifeleech.getName()), lifeleech);
+ }
   public static void syncConfig(Configuration c) {
     launchid = c.getInt("enchant.launch.id", Const.ConfigCategory.modpackMisc,
         86, 71, 999, "Id of the launch enchantment (double jump on boots).  Change this if you get id conflicts with other mods.");
