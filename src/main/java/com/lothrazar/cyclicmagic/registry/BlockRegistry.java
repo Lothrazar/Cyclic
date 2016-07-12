2b7b5aef1073d51aaa6beee637b3f85ace205f01
Sam Bassett <samson.bassett@gmail.com>
2016-07-11 19:06:37 -0700

    nerf sprout recipe output again

--- src/main/java/com/lothrazar/cyclicmagic/registry/BlockRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/BlockRegistry.java
@@ -155,7 +155,7 @@ public class BlockRegistry {
     ItemRegistry.sprout_seed.setUnlocalizedName("sprout_seed");
     ItemRegistry.registerItem(ItemRegistry.sprout_seed, "sprout_seed");
     ItemRegistry.itemMap.put("sprout_seed", ItemRegistry.sprout_seed);
-    GameRegistry.addRecipe(new ItemStack(ItemRegistry.sprout_seed, 2,0),
+    GameRegistry.addRecipe(new ItemStack(ItemRegistry.sprout_seed),
         "waw",
         "bEc",
         "wdw",
