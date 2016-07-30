90ea957b97001e576c90c3b565831276f620ae22
Sam Bassett <samson.bassett@gmail.com>
2016-07-30 15:20:21 -0700

    alphebetize module constructors

--- src/main/java/com/lothrazar/cyclicmagic/ModMain.java
+++ src/main/java/com/lothrazar/cyclicmagic/ModMain.java
@@ -82,51 +82,52 @@ public class ModMain {
     this.events.registerAll();
   }
   private void createFeatureModules() {
-    modules.add(new BuilderBlockModule());
+    // :) http://alphabetizer.flap.tv/
+    modules.add(new AchievementExpModule());
     modules.add(new BucketBlockModule());
+    modules.add(new BuilderBlockModule());
     modules.add(new CarbonPaperModule());
     modules.add(new ChestSackModule());
     modules.add(new ConsumeablesModule());
     modules.add(new ConveyorPlateModule());
-    modules.add(new EnderBookModule());
+    modules.add(new DispenserBehaviorModule());
+    modules.add(new DropNametagDeathModule());
+    modules.add(new EditSignBarehandModule());
     modules.add(new EmeraldArmorModule());
+    modules.add(new EnchantModule());
     modules.add(new EnderBombModule());
+    modules.add(new EnderBookModule());
+    modules.add(new EnderChestClickopenModule());
+    modules.add(new EndermanDropCarryModule());
+    modules.add(new F3InfoModule());
     modules.add(new FragileBlockModule());
+    modules.add(new FragileTorchesModule());
+    modules.add(new FurnaceStardewModule());
+    modules.add(new GuiTerrariaButtonsModule());
     modules.add(new HorseFoodModule());
+    modules.add(new ItemstackInfoModule());
+    modules.add(new LadderClimbSpeedModule());
+    modules.add(new LightningTransformModule());
+    modules.add(new LootTableModule());
     modules.add(new MagicBeanModule());
+    modules.add(new MobDropChangesModule());
     modules.add(new MobSpawnModule());
+    modules.add(new MountedTweaksModule());
+    modules.add(new PassthroughActionModule());
     modules.add(new PotionModule());
     modules.add(new ProjectileModule());
+    modules.add(new RecipeChangerModule());
+    modules.add(new SaplingMutationModule());
+    modules.add(new SkullNameFromSignModule());
     modules.add(new SlimepadModule());
     modules.add(new StackSizeModule());
     modules.add(new StorageBagModule());
     modules.add(new ToolsModule());
     modules.add(new UnbreakableSpawnerModule());
     modules.add(new UncrafterModule());
-    modules.add(new EnchantModule());
-    modules.add(new AchievementExpModule());
-    modules.add(new EditSignBarehandModule());
-    modules.add(new EnderChestClickopenModule());
-    modules.add(new EndermanDropCarryModule());
-    modules.add(new ItemstackInfoModule());
-    modules.add(new FragileTorchesModule());
-    modules.add(new FurnaceStardewModule());
-    modules.add(new GuiTerrariaButtonsModule());
-    modules.add(new LadderClimbSpeedModule());
-    modules.add(new LightningTransformModule());
-    modules.add(new LootTableModule());
-    modules.add(new MobDropChangesModule());
-    modules.add(new MountedTweaksModule());
-    modules.add(new DropNametagDeathModule());
-    modules.add(new VillagerNametagModule());
-    modules.add(new PassthroughActionModule());
-    modules.add(new SaplingMutationModule());
-    modules.add(new SkullNameFromSignModule());
-    modules.add(new F3InfoModule());
-    modules.add(new WorldGenModule());
-    modules.add(new RecipeChangerModule());
     modules.add(new VillagerCreateModule());
-    modules.add(new DispenserBehaviorModule());
+    modules.add(new VillagerNametagModule());
+    modules.add(new WorldGenModule());
     //event modules TODO: make actual modules.?? maybe
     ModMain.instance.events.addEvent(new EventSpells());//so far only used by cyclic wand...
   }
