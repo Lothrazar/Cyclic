267c576fed9cf13c935ac643c330987e9136e98d
Sam Bassett <samson.bassett@gmail.com>
2016-08-03 22:03:38 -0700

    add assets and recipe for magnet

--- src/main/java/com/lothrazar/cyclicmagic/block/BlockMagnet.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/BlockMagnet.java
@@ -1,18 +1,26 @@
 package com.lothrazar.cyclicmagic.block;
+import javax.annotation.Nullable;
 import com.lothrazar.cyclicmagic.IHasRecipe;
 import com.lothrazar.cyclicmagic.block.tileentity.TileEntityMagnet;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockMagnet extends Block implements IHasRecipe {
+  protected static final AxisAlignedBB BOUNDS = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.03125D, 0.9375D);
   public BlockMagnet() {
     super(Material.IRON);
     this.setHardness(3.0F).setResistance(5.0F);
@@ -32,12 +40,47 @@ public class BlockMagnet extends Block implements IHasRecipe {
     return hasTileEntity();
   }
   @Override
+  public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    return BOUNDS;
+  }
+  @Nullable
+  public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, World worldIn, BlockPos pos) {
+    return NULL_AABB;
+  }
+  /**
+   * Used to determine ambient occlusion and culling when rebuilding chunks for
+   * render
+   */
+  public boolean isOpaqueCube(IBlockState state) {
+    return false;
+  }
+  public boolean isFullCube(IBlockState state) {
+    return false;
+  }
+  public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+    return true;
+  }
+  //for transparency
+  @SideOnly(Side.CLIENT)
+  @Override
+  public BlockRenderLayer getBlockLayer() {
+    return BlockRenderLayer.TRANSLUCENT;
+  }
+  /**
+   * Return true if an entity can be spawned inside the block (used to get the
+   * player's bed spawn location)
+   */
+  public boolean canSpawnInBlock() {
+    return true;
+  }
+  @Override
   public void addRecipe() {
-    GameRegistry.addRecipe(new ItemStack(this), "rsr", "gbg", "ooo", 
-        'o', Blocks.OBSIDIAN, 
-        'g', Items.GHAST_TEAR, 
-        's', Blocks.DISPENSER, 
-        'r', Items.IRON_INGOT, 
-        'b', Blocks.BEACON);
+    GameRegistry.addRecipe(new ItemStack(this, 4),
+        "sbs",
+        "bxb",
+        "sbs",
+        's', new ItemStack(Items.IRON_INGOT),
+        'b', new ItemStack(Items.COAL),
+        'x', new ItemStack(Items.DYE, 1, EnumDyeColor.PURPLE.getDyeDamage()));
   }
 }
