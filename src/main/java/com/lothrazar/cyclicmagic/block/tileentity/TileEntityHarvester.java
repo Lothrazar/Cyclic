65efdd5e136759b4de4885681ba6cc0e7dce8a0d
Sam Bassett <samson.bassett@gmail.com>
2016-08-04 23:30:37 -0700

    tile entities new are not inventories. tweak speed and vector of magnet pull so they can get up over the farmland-grass ledge

--- src/main/java/com/lothrazar/cyclicmagic/block/tileentity/TileEntityHarvester.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/tileentity/TileEntityHarvester.java
@@ -4,11 +4,6 @@ import com.lothrazar.cyclicmagic.util.UtilHarvestCrops;
 import com.lothrazar.cyclicmagic.util.UtilParticle;
 import com.lothrazar.cyclicmagic.util.UtilWorld;
 import com.lothrazar.cyclicmagic.util.UtilHarvestCrops.HarestCropsConfig;
-import net.minecraft.block.Block;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.ISidedInventory;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;// net.minecraft.network.play.server.S35PacketUpdateTileEntity;
@@ -19,16 +14,13 @@ import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 
-public class TileEntityHarvester extends TileEntity implements IInventory, ITickable, ISidedInventory {
+public class TileEntityHarvester extends TileEntity implements ITickable {
   private int timer;
   public static final int TIMER_FULL = 80;
-  private int[] hopperInput = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };// all slots
   private HarestCropsConfig conf;
   private static final String NBT_TIMER = "Timer";
   private static final int HARVEST_RADIUS = 8;
-  public static enum Fields {
-    TIMER
-  }
+
   public TileEntityHarvester() {
     this.timer = TIMER_FULL;
     conf = new HarestCropsConfig();
@@ -38,95 +30,14 @@ public class TileEntityHarvester extends TileEntity implements IInventory, ITick
     conf.doesMelonBlocks = true;
   }
   @Override
-  public boolean hasCustomName() {
-    return false;
-  }
-  @Override
   public ITextComponent getDisplayName() {
     return null;
-  }
-  @Override
-  public int getSizeInventory() {
-    return 0;
-  }
-  @Override
-  public ItemStack getStackInSlot(int index) {
-    return null;
-  }
-  @Override
-  public ItemStack decrStackSize(int index, int count) {
-    ItemStack stack = getStackInSlot(index);
-    if (stack != null) {
-      if (stack.stackSize <= count) {
-        setInventorySlotContents(index, null);
-      }
-      else {
-        stack = stack.splitStack(count);
-        if (stack.stackSize == 0) {
-          setInventorySlotContents(index, null);
-        }
-      }
-    }
-    return stack;
-  }
-  @Override
-  public void setInventorySlotContents(int index, ItemStack stack) {
-  }
-  @Override
-  public int getInventoryStackLimit() {
-    return 64;
-  }
-  @Override
-  public boolean isUseableByPlayer(EntityPlayer player) {
-    return true;
-  }
-  @Override
-  public void openInventory(EntityPlayer player) {
-  }
-  @Override
-  public void closeInventory(EntityPlayer player) {
-  }
-  @Override
-  public boolean isItemValidForSlot(int index, ItemStack stack) {
-    return Block.getBlockFromItem(stack.getItem()) != null;
-  }
-  @Override
-  public int getField(int id) {
-    if (id >= 0 && id < this.getFieldCount())
-      switch (Fields.values()[id]) {
-      case TIMER:
-        return timer;
-      default:
-        break;
-      }
-    return -1;
-  }
-  @Override
-  public void setField(int id, int value) {
-    if (id >= 0 && id < this.getFieldCount())
-      switch (Fields.values()[id]) {
-      case TIMER:
-        this.timer = value;
-        break;
-      default:
-        break;
-      }
-  }
-  public int getTimer() {
-    return this.getField(Fields.TIMER.ordinal());
   }
   public void setHarvestConf(HarestCropsConfig c) {
     conf = c;
   }
   public HarestCropsConfig getHarvestConf() {
     return conf;
-  }
-  @Override
-  public int getFieldCount() {
-    return Fields.values().length;
-  }
-  @Override
-  public void clear() {
   }
   @Override
   public void readFromNBT(NBTTagCompound tagCompound) {
@@ -216,39 +127,5 @@ public class TileEntityHarvester extends TileEntity implements IInventory, ITick
     else
       facing = b.getFacingFromState(this.worldObj.getBlockState(this.pos)).getOpposite();
     return facing;
-  }
-  @Override
-  public int[] getSlotsForFace(EnumFacing side) {
-    return hopperInput;
-  }
-  @Override
-  public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction) {
-    return this.isItemValidForSlot(index, itemStackIn);
-  }
-  @Override
-  public boolean canExtractItem(int index, ItemStack stack, EnumFacing direction) {
-    // do not let hoppers pull out of here for any reason
-    return false;// direction == EnumFacing.DOWN;
-  }
-  @Override
-  public ItemStack removeStackFromSlot(int index) {
-    ItemStack stack = getStackInSlot(index);
-    if (stack != null) {
-      setInventorySlotContents(index, null);
-    }
-    return stack;
-  }
-  @Override
-  public String getName() {
-    return null;
-  }
-  @Override
-  public boolean receiveClientEvent(int id, int value) {
-    if (id >= 0 && id < this.getFieldCount()) {
-      this.setField(id, value);
-      return true;
-    }
-    else
-      return super.receiveClientEvent(id, value);
   }
 }
--- src/main/java/com/lothrazar/cyclicmagic/block/tileentity/TileEntityMagnet.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/tileentity/TileEntityMagnet.java
@@ -1,116 +1,26 @@
 package com.lothrazar.cyclicmagic.block.tileentity;
 import com.lothrazar.cyclicmagic.util.UtilEntity;
-import net.minecraft.block.Block;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.ISidedInventory;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;// net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.text.ITextComponent;
 
-public class TileEntityMagnet extends TileEntity implements IInventory, ITickable, ISidedInventory {
+public class TileEntityMagnet extends TileEntity implements ITickable{
   private int timer;
   public static final int TIMER_FULL = 36;
-  private int[] hopperInput = {};
   private static final String NBT_TIMER = "Timer";
-  private final static float ITEMSPEED = 0.4F;
+  private final static float ITEMSPEED = 0.33F;
   private static final int ITEM_VRADIUS = 1;
   private static final int ITEM_HRADIUS = 16;
-  public static enum Fields {
-    TIMER
-  }
+
   public TileEntityMagnet() {
     this.timer = TIMER_FULL;
   }
   @Override
-  public boolean hasCustomName() {
-    return false;
-  }
-  @Override
   public ITextComponent getDisplayName() {
     return null;
-  }
-  @Override
-  public int getSizeInventory() {
-    return 0;
-  }
-  @Override
-  public ItemStack getStackInSlot(int index) {
-    return null;
-  }
-  @Override
-  public ItemStack decrStackSize(int index, int count) {
-    ItemStack stack = getStackInSlot(index);
-    if (stack != null) {
-      if (stack.stackSize <= count) {
-        setInventorySlotContents(index, null);
-      }
-      else {
-        stack = stack.splitStack(count);
-        if (stack.stackSize == 0) {
-          setInventorySlotContents(index, null);
-        }
-      }
-    }
-    return stack;
-  }
-  @Override
-  public void setInventorySlotContents(int index, ItemStack stack) {
-  }
-  @Override
-  public int getInventoryStackLimit() {
-    return 64;
-  }
-  @Override
-  public boolean isUseableByPlayer(EntityPlayer player) {
-    return true;
-  }
-  @Override
-  public void openInventory(EntityPlayer player) {
-  }
-  @Override
-  public void closeInventory(EntityPlayer player) {
-  }
-  @Override
-  public boolean isItemValidForSlot(int index, ItemStack stack) {
-    return Block.getBlockFromItem(stack.getItem()) != null;
-  }
-  @Override
-  public int getField(int id) {
-    if (id >= 0 && id < this.getFieldCount())
-      switch (Fields.values()[id]) {
-      case TIMER:
-        return timer;
-      default:
-        break;
-      }
-    return -1;
-  }
-  @Override
-  public void setField(int id, int value) {
-    if (id >= 0 && id < this.getFieldCount())
-      switch (Fields.values()[id]) {
-      case TIMER:
-        this.timer = value;
-        break;
-      default:
-        break;
-      }
-  }
-  public int getTimer() {
-    return this.getField(Fields.TIMER.ordinal());
-  }
-  @Override
-  public int getFieldCount() {
-    return Fields.values().length;
-  }
-  @Override
-  public void clear() {
   }
   @Override
   public void readFromNBT(NBTTagCompound tagCompound) {
@@ -151,11 +61,12 @@ public class TileEntityMagnet extends TileEntity implements IInventory, ITickabl
     }
     // center of the block
     double x = this.getPos().getX() + 0.5;
-    double y = this.getPos().getY() + 0.5;
+    double y = this.getPos().getY() + 0.9;
     double z = this.getPos().getZ() + 0.5;
     if (trigger) {
-      //int moved = 
+      int moved = 
       UtilEntity.pullEntityItemsTowards(this.getWorld(), x, y, z, ITEMSPEED, ITEM_HRADIUS, ITEM_VRADIUS);
+      System.out.println("MOVED:"+moved);
       timer = TIMER_FULL;//harvest worked!
     }
 //    else {
@@ -168,39 +79,5 @@ public class TileEntityMagnet extends TileEntity implements IInventory, ITickabl
   }
   private int getSpeed() {
     return 1;
-  }
-  @Override
-  public int[] getSlotsForFace(EnumFacing side) {
-    return hopperInput;
-  }
-  @Override
-  public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction) {
-    return this.isItemValidForSlot(index, itemStackIn);
-  }
-  @Override
-  public boolean canExtractItem(int index, ItemStack stack, EnumFacing direction) {
-    // do not let hoppers pull out of here for any reason
-    return false;// direction == EnumFacing.DOWN;
-  }
-  @Override
-  public ItemStack removeStackFromSlot(int index) {
-    ItemStack stack = getStackInSlot(index);
-    if (stack != null) {
-      setInventorySlotContents(index, null);
-    }
-    return stack;
-  }
-  @Override
-  public String getName() {
-    return null;
-  }
-  @Override
-  public boolean receiveClientEvent(int id, int value) {
-    if (id >= 0 && id < this.getFieldCount()) {
-      this.setField(id, value);
-      return true;
-    }
-    else
-      return super.receiveClientEvent(id, value);
   }
 }
