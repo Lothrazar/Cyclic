c4afb45750f645d3c2f9ae17dad1c8360b4a814c
Sam <samson.bassett@gmail.com>
2016-08-24 13:09:39 -0700

    prospector range in config

--- src/main/java/com/lothrazar/cyclicmagic/item/ItemToolProspector.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/ItemToolProspector.java
@@ -1,11 +1,10 @@
 package com.lothrazar.cyclicmagic.item;
+import com.lothrazar.cyclicmagic.IHasConfig;
 import com.lothrazar.cyclicmagic.IHasRecipe;
 import com.lothrazar.cyclicmagic.util.Const;
 import com.lothrazar.cyclicmagic.util.UtilChat;
 import net.minecraft.block.BlockOre;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.entity.monster.EntityBlaze;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -15,17 +14,16 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class ItemToolProspector extends BaseTool implements IHasRecipe {
-  private static final int durability = 2000;
-  private static final int cooldown = 12;
-  private static final int range = 16;
+public class ItemToolProspector extends BaseTool implements IHasRecipe, IHasConfig {
+  private static final int DURABILITY = 2000;
+  private static final int COOLDOWN = 12;
+  private static int range = 16;
   public ItemToolProspector() {
-    super(durability);
+    super(DURABILITY);
   }
   @Override
   public EnumActionResult onItemUse(ItemStack stack, EntityPlayer player, World worldObj, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
@@ -37,7 +35,7 @@ public class ItemToolProspector extends BaseTool implements IHasRecipe {
       BlockPos current = pos;
       IBlockState at = worldObj.getBlockState(current);
       for (int i = 0; i <= range; i++) {
-        if (at != null && at.getBlock() instanceof BlockOre) {
+        if (at != null && at.getBlock() != null && at.getBlock() instanceof BlockOre) {
           UtilChat.addChatMessage(player, UtilChat.lang("tool_prospector.found") + at.getBlock().getLocalizedName());
           found = true;
           break;
@@ -49,7 +47,7 @@ public class ItemToolProspector extends BaseTool implements IHasRecipe {
         UtilChat.addChatMessage(player, UtilChat.lang("tool_prospector.none") + range);
       }
     }
-    player.getCooldownTracker().setCooldown(this, cooldown);
+    player.getCooldownTracker().setCooldown(this, COOLDOWN);
     super.onUse(stack, player, worldObj, hand);
     return super.onItemUse(stack, player, worldObj, pos, hand, side, hitX, hitY, hitZ);
   }
@@ -63,45 +61,8 @@ public class ItemToolProspector extends BaseTool implements IHasRecipe {
         's', new ItemStack(Items.DIAMOND),
         'g', new ItemStack(Blocks.STAINED_GLASS, 1, EnumDyeColor.LIGHT_BLUE.getMetadata()));
   }
-  public static class SpawnDetail {
-    private int itemWeight;
-    private String displayName;
-    private String creatureTypeName;
-    private boolean lightEnabled = false;
-    public SpawnDetail(Biome.SpawnListEntry entry, EnumCreatureType creatureType, int currentLightLevel) {
-      itemWeight = entry.itemWeight;
-      creatureTypeName = creatureType.name();
-      displayName = entry.entityClass.getSimpleName().replace("Entity", "");
-      //one caveat: the above canSpawn ignores light level
-      if (creatureType != EnumCreatureType.MONSTER) {
-        //ambient/water/passive, all ignore light
-        lightEnabled = true;
-      }
-      else {
-        int reqLight = entry.entityClass == EntityBlaze.class ? Const.LIGHT_MOBSPAWN_BLAZE : Const.LIGHT_MOBSPAWN;
-        if (currentLightLevel <= reqLight) {
-          lightEnabled = true;
-        }
-      }
-    }
-    public SpawnDetail(String n, EnumCreatureType creatureType, int currentLightLevel, int odds) {
-      //special case of JUST witherywither
-      itemWeight = odds;
-      displayName = n;
-      creatureTypeName = creatureType.name();
-      if (currentLightLevel <= Const.LIGHT_MOBSPAWN) {
-        lightEnabled = true;
-      }
-    }
-    public String getSortBy() {
-      return displayName;
-    }
-    public String toString(boolean showOdds) {
-      TextFormatting color = (lightEnabled) ? TextFormatting.WHITE : TextFormatting.DARK_GRAY;
-      if (showOdds) // todo; super.tostring here?
-        return color + "[" + creatureTypeName + ", " + String.format("%03d", itemWeight) + "] " + displayName;
-      else
-        return color + displayName;
-    }
+  @Override
+  public void syncConfig(Configuration config) {
+    ItemToolProspector.range = config.getInt("ProspectorRange", Const.ConfigCategory.modpackMisc, 16, 2, 256, "Block Range it will search onclick");
   }
 }
