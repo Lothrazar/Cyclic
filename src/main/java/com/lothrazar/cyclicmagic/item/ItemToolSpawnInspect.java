6c811b1c361c691bbab4dbe43ece460fa1783ef6
Sam <samson.bassett@gmail.com>
2016-08-27 15:27:26 -0700

    spawn detector: empty message, line up in csv. fix light levels

--- src/main/java/com/lothrazar/cyclicmagic/item/ItemToolSpawnInspect.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/ItemToolSpawnInspect.java
@@ -8,7 +8,6 @@ import com.lothrazar.cyclicmagic.util.Const;
 import com.lothrazar.cyclicmagic.util.UtilChat;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.entity.monster.EntityBlaze;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -19,10 +18,11 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldEntitySpawner;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.ChunkProviderServer;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -34,38 +34,49 @@ public class ItemToolSpawnInspect extends BaseTool implements IHasRecipe {
   }
   @Override
   public EnumActionResult onItemUse(ItemStack stack, EntityPlayer player, World worldObj, BlockPos posIn, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-    if (side == null || posIn == null) { return super.onItemUse(stack, player, worldObj, posIn, hand, side, hitX, hitY, hitZ); }
+    if (side == null || posIn == null || side != EnumFacing.UP) {
+      if (!worldObj.isRemote) {
+        UtilChat.addChatMessage(player, "item.tool_spawn_inspect.up");
+      }
+      return super.onItemUse(stack, player, worldObj, posIn, hand, side, hitX, hitY, hitZ);
+    }
     boolean showOdds = player.isSneaking();
     if (!worldObj.isRemote) {
+      ChunkProviderServer s = (ChunkProviderServer) worldObj.getChunkProvider();
+      BlockPos pos = posIn.offset(side);
+      Chunk chunk = worldObj.getChunkFromBlockCoords(pos);
       if (worldObj.getChunkProvider() instanceof ChunkProviderServer) {
-        ChunkProviderServer s = (ChunkProviderServer) worldObj.getChunkProvider();
-        BlockPos pos = posIn.offset(side);
-        int light = worldObj.getLight(pos);
+
         List<SpawnDetail> names = new ArrayList<SpawnDetail>();
         for (EnumCreatureType creatureType : EnumCreatureType.values()) {
           List<Biome.SpawnListEntry> list = s.getPossibleCreatures(creatureType, pos);
           for (Biome.SpawnListEntry entry : list) {
             if (WorldEntitySpawner.canCreatureTypeSpawnAtLocation(EntitySpawnPlacementRegistry.getPlacementForEntity(entry.entityClass), worldObj, pos)) {
-              names.add(new SpawnDetail(entry, creatureType, light));
+              names.add(new SpawnDetail(entry, creatureType));
               //hack since witherskeleton is not its own class/entry, just a mob modifier like zombietypes or villagertypes
               if (entry.entityClass.equals(EntitySkeleton.class) && player.dimension == Const.Dimension.nether) {
-                names.add(new SpawnDetail("WitherSkeleton", creatureType, light, entry.itemWeight));
+                names.add(new SpawnDetail("WitherSkeleton", creatureType, entry.itemWeight));
               }
             }
           }
         }
         if (names.size() > 0) {
+          String strLight = "Light: " + chunk.getLightSubtracted(pos, 0) + " (" + chunk.getLightFor(EnumSkyBlock.SKY, pos) + " sky, " + chunk.getLightFor(EnumSkyBlock.BLOCK, pos) + " block)";
+          
+          UtilChat.addChatMessage(player, strLight);
           Collections.sort(names, new Comparator<SpawnDetail>() {
             @Override
             public int compare(SpawnDetail o1, SpawnDetail o2) {
               return o1.getSortBy().compareTo(o2.getSortBy());
             }
           });
+          List<String> csv = new ArrayList<String>();
           for (SpawnDetail detail : names) {
-            UtilChat.addChatMessage(player, detail.toString(showOdds));
+            csv.add(detail.toString(showOdds));
           }
+          UtilChat.addChatMessage(player, String.join(", ", csv));
         }
-        else{
+        else {
           UtilChat.addChatMessage(player, "item.tool_spawn_inspect.empty");
         }
       }
@@ -88,41 +99,41 @@ public class ItemToolSpawnInspect extends BaseTool implements IHasRecipe {
     private int itemWeight;
     private String displayName;
     private String creatureTypeName;
-    private boolean lightEnabled = false;
-    public SpawnDetail(Biome.SpawnListEntry entry, EnumCreatureType creatureType, int currentLightLevel) {
+//    private boolean lightEnabled = true;
+    public SpawnDetail(Biome.SpawnListEntry entry, EnumCreatureType creatureType) {
       itemWeight = entry.itemWeight;
       creatureTypeName = creatureType.name();
       displayName = entry.entityClass.getSimpleName().replace("Entity", "");
       //one caveat: the above canSpawn ignores light level
-      if (creatureType != EnumCreatureType.MONSTER) {
-        //ambient/water/passive, all ignore light
-        lightEnabled = true;
-      }
-      else {
-        int reqLight = entry.entityClass == EntityBlaze.class ? Const.LIGHT_MOBSPAWN_BLAZE : Const.LIGHT_MOBSPAWN;
-        if (currentLightLevel <= reqLight) {
-          lightEnabled = true;
-        }
-      }
+//      if (creatureType != EnumCreatureType.MONSTER) {
+//        //ambient/water/passive, all ignore light
+//        lightEnabled = true;
+//      }
+//      else {
+//        int reqLight = entry.entityClass == EntityBlaze.class ? Const.LIGHT_MOBSPAWN_BLAZE : Const.LIGHT_MOBSPAWN;
+//        if (currentLightLevel <= reqLight) {
+//          lightEnabled = true;
+//        }
+//      }
     }
-    public SpawnDetail(String n, EnumCreatureType creatureType, int currentLightLevel, int odds) {
+    public SpawnDetail(String n, EnumCreatureType creatureType, int odds) {
       //special case of JUST witherywither
       itemWeight = odds;
       displayName = n;
       creatureTypeName = creatureType.name();
-      if (currentLightLevel <= Const.LIGHT_MOBSPAWN) {
-        lightEnabled = true;
-      }
+//      if (currentLightLevel <= Const.LIGHT_MOBSPAWN) {
+//        lightEnabled = true;
+//      }
     }
     public String getSortBy() {
       return displayName;
     }
     public String toString(boolean showOdds) {
-      TextFormatting color = (lightEnabled) ? TextFormatting.WHITE : TextFormatting.DARK_GRAY;
+//      TextFormatting color = (lightEnabled) ? TextFormatting.WHITE : TextFormatting.DARK_GRAY;
       if (showOdds) // todo; super.tostring here?
-        return color + "[" + creatureTypeName + ", " + String.format("%03d", itemWeight) + "] " + displayName;
+        return "[" + creatureTypeName + ", " + String.format("%03d", itemWeight) + "] " + displayName;
       else
-        return color + displayName;
+        return displayName;
     }
   }
 }
