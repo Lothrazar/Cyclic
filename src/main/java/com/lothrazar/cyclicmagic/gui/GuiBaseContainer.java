b01337dd232cc0bbc7da460d6caf8988133ed572
Sam <samson.bassett@gmail.com>
2016-08-26 22:36:09 -0700

    refactor: moved background drawing to base class

--- src/main/java/com/lothrazar/cyclicmagic/gui/GuiBaseContainer.java
+++ src/main/java/com/lothrazar/cyclicmagic/gui/GuiBaseContainer.java
@@ -1,18 +1,22 @@
 package com.lothrazar.cyclicmagic.gui;
-
+import com.lothrazar.cyclicmagic.util.Const;
 import com.lothrazar.cyclicmagic.util.UtilChat;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.inventory.Container;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public abstract class GuiBaseContainer extends GuiContainer {
-
   public GuiBaseContainer(Container inventorySlotsIn) {
     super(inventorySlotsIn);
   }
-
   public abstract String getTitle();
+  public ResourceLocation getBackground(){
+    return Const.Res.TABLEDEFAULT;//can override
+  }
   @SideOnly(Side.CLIENT)
   @Override
   protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
@@ -20,6 +24,16 @@ public abstract class GuiBaseContainer extends GuiContainer {
     String s = UtilChat.lang(getTitle());// "tile.placer_block.name"
     this.fontRendererObj.drawString(s, this.xSize / 2 - this.fontRendererObj.getStringWidth(s) / 2, 6, 4210752);
   }
-
-
+  private static final int texture_width = 176;
+  private static final int texture_height = 166;
+  @Override
+  protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+    //    super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);// abstract
+    GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+    this.mc.getTextureManager().bindTexture(getBackground());
+    int thisX = (this.width - this.xSize) / 2;
+    int thisY = (this.height - this.ySize) / 2;
+    int u = 0, v = 0;
+    Gui.drawModalRectWithCustomSizedTexture(thisX, thisY, u, v, this.xSize, this.ySize, texture_width, texture_height);
+  }
 }
--- src/main/java/com/lothrazar/cyclicmagic/gui/placer/GuiPlacer.java
+++ src/main/java/com/lothrazar/cyclicmagic/gui/placer/GuiPlacer.java
@@ -3,20 +3,11 @@ import com.lothrazar.cyclicmagic.block.tileentity.TileMachinePlacer;
 import com.lothrazar.cyclicmagic.gui.GuiBaseContainer;
 import com.lothrazar.cyclicmagic.util.Const;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.util.ResourceLocation;
 
 public class GuiPlacer extends GuiBaseContainer {
-  private static final String folder = "textures/gui/";
-  private static final ResourceLocation table = new ResourceLocation(Const.MODID, folder + "table.png");
-  private static final ResourceLocation slot = new ResourceLocation(Const.MODID, folder + "inventory_slot.png");
-  private static final ResourceLocation progress = new ResourceLocation(Const.MODID, folder + "progress.png");
-  private static final ResourceLocation progressCtr = new ResourceLocation(Const.MODID, folder + "progress_ctr.png");
   private static final int progressWidth = 156;
   private static final int progressH = 7;
-  private static final int texture_width = 176;
-  private static final int texture_height = 166;
   static final int padding = 8;
   private TileMachinePlacer tile;
   boolean debugLabels = false;
@@ -30,22 +21,19 @@ public class GuiPlacer extends GuiBaseContainer {
   }
   @Override
   protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-    GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-    this.mc.getTextureManager().bindTexture(table);
-    int thisX = (this.width - this.xSize) / 2;
-    int thisY = (this.height - this.ySize) / 2;
+    super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
     int u = 0, v = 0;
-    Gui.drawModalRectWithCustomSizedTexture(thisX, thisY, u, v, this.xSize, this.ySize, texture_width, texture_height);
-    this.mc.getTextureManager().bindTexture(slot);
+
+    this.mc.getTextureManager().bindTexture(Const.Res.SLOT);
     for (int k = 0; k < this.tile.getSizeInventory(); k++) { // x had - 3 ??
       Gui.drawModalRectWithCustomSizedTexture(this.guiLeft + ContainerPlacer.SLOTX_START - 1 + k * Const.SQ, this.guiTop + ContainerPlacer.SLOTY - 1, u, v, Const.SQ, Const.SQ, Const.SQ, Const.SQ);
     }
 
     int belowSlots = this.guiTop + 9 + 3 * Const.SQ+5;
-    this.mc.getTextureManager().bindTexture(progressCtr);
+    this.mc.getTextureManager().bindTexture(Const.Res.PROGRESSCTR);
     Gui.drawModalRectWithCustomSizedTexture(this.guiLeft + 10, belowSlots, u, v, (int) progressWidth, progressH, progressWidth, progressH);
     if (tile.getTimer() > 0 && tile.getStackInSlot(0) != null) {
-      this.mc.getTextureManager().bindTexture(progress);
+      this.mc.getTextureManager().bindTexture(Const.Res.PROGRESS);
       float percent = ((float) tile.getTimer()) / ((float) TileMachinePlacer.TIMER_FULL);
       // maximum progress bar is 156, since the whole texture is 176 minus
       // 10 padding on each side
