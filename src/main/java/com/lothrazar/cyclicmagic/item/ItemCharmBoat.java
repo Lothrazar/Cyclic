b79e88dec35f57e92a6e12ac8678cd4887fb4669
Sam <samson.bassett@gmail.com>
2016-09-02 11:12:46 -0700

    ok, so vanilla crafting ignores potion nbt, works with waterbottle

--- src/main/java/com/lothrazar/cyclicmagic/item/ItemCharmBoat.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/ItemCharmBoat.java
@@ -1,16 +1,23 @@
 package com.lothrazar.cyclicmagic.item;
 
+import com.lothrazar.cyclicmagic.IHasRecipe;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityBoat;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.PotionTypes;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionUtils;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class ItemCharmBoat extends BaseCharm {
+public class ItemCharmBoat extends BaseCharm implements IHasRecipe{
+  private static final int durability = 2000;
   
 
   public ItemCharmBoat() {
-    super(64);
+    super(durability);
   }
 
   /**
@@ -25,17 +32,30 @@ public class ItemCharmBoat extends BaseCharm {
     if (entityIn instanceof EntityPlayer) {
       EntityPlayer living = (EntityPlayer) entityIn;
       
+//      if(living.getAir() < 2)
+      
+      
       if(entityIn.getRidingEntity() instanceof EntityBoat){
 
         EntityBoat boat = (EntityBoat)entityIn.getRidingEntity();
 
-        if (living.moveForward > 0.0F) {
-          System.out.println("Boat Power!!!");
+        if (living.moveForward > 0) {
+          float reduce = 0.08F;
           //pulled from private EntityBoat.controlBoat() fn
-          boat.motionX += net.minecraft.util.math.MathHelper.sin(-boat.rotationYaw * 0.017453292F);
-          boat.motionZ += net.minecraft.util.math.MathHelper.cos(boat.rotationYaw * 0.017453292F);
+          boat.motionX += net.minecraft.util.math.MathHelper.sin(-boat.rotationYaw * 0.017453292F)*reduce;
+          boat.motionZ += net.minecraft.util.math.MathHelper.cos(boat.rotationYaw * 0.017453292F)*reduce;
+          if(worldIn.rand.nextDouble() < 0.1){
+            super.damageCharm(living, stack, itemSlot);
+          }
         }
       }
     }
+  }
+
+  @Override
+  public void addRecipe() {
+    ItemStack pot = new ItemStack(Items.POTIONITEM);
+    PotionUtils.addPotionToItemStack(pot, PotionTypes.WATER_BREATHING);
+    GameRegistry.addShapelessRecipe(new ItemStack(this), Items.BOAT,pot);
   }
 }
