931822f5e75b1f97f5bd807e5b92a5c5ebd364b3
sam <samson.bassett@gmail.com>
2016-09-30 00:20:18 -0700

    fix JEI again since offhand slot pushed it over by 1

--- src/main/java/com/lothrazar/cyclicmagic/JEIPlugin.java
+++ src/main/java/com/lothrazar/cyclicmagic/JEIPlugin.java
@@ -14,19 +14,11 @@ public class JEIPlugin extends BlankModPlugin {
     //and of course readme on https://github.com/mezz/JustEnoughItems
 
       // setup [+] feature
-    //copied from source:
-
-    /**
-     * Basic method for adding a recipe transfer handler.
-     *
-     * @param containerClass     the class of the container that this recipe transfer handler is for
-     * @param recipeCategoryUid  the recipe categories that this container can use 
-     * @param recipeSlotStart    the first slot for recipe inputs // skip over the 1 output and the 4 armor slots
-     * @param recipeSlotCount    the number of slots for recipe inputs //3x3
-     * @param inventorySlotStart the first slot of the available inventory (usually player inventory) //top left
-     * @param inventorySlotCount the number of slots of the available inventory //top right including hotbar
-     */
-      registry.getRecipeTransferRegistry().addRecipeTransferHandler(ContainerPlayerExtWorkbench.class, VanillaRecipeCategoryUid.CRAFTING, 5, 9, 14, 36);
+      registry.getRecipeTransferRegistry().addRecipeTransferHandler(ContainerPlayerExtWorkbench.class, VanillaRecipeCategoryUid.CRAFTING, 
+          6, // @param recipeSlotStart    the first slot for recipe inputs // skip over the 1 output and the 5 armor slots
+          9, // @param recipeSlotCount    the number of slots for recipe inputs //3x3
+          15, //@param inventorySlotStart the first slot of the available inventory (usually player inventory) =9+6
+          36);//@param inventorySlotCount the number of slots of the available inventory //top right including hotbar =4*9
 
   }
 }
