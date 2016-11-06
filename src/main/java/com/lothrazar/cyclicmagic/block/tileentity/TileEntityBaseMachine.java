f6a3f93c22f48413eedc57c118ee7d95a0bf7ccd
sam <samson.bassett@gmail.com>
2016-11-06 15:55:31 -0800

    1.8.1 build number. fix metadata loss on rotate

--- src/main/java/com/lothrazar/cyclicmagic/block/tileentity/TileEntityBaseMachine.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/tileentity/TileEntityBaseMachine.java
@@ -2,11 +2,13 @@ package com.lothrazar.cyclicmagic.block.tileentity;
 import com.lothrazar.cyclicmagic.block.BlockBaseFacing;
 import com.lothrazar.cyclicmagic.block.BlockMiner;
 import com.lothrazar.cyclicmagic.util.UtilParticle;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 @SuppressWarnings("unused")
 public abstract class TileEntityBaseMachine extends TileEntity implements ITickable {
@@ -32,5 +34,13 @@ public abstract class TileEntityBaseMachine extends TileEntity implements ITicka
       double z = this.getPos().getZ() + 0.5;
       UtilParticle.spawnParticle(this.getWorld(), EnumParticleTypes.SMOKE_NORMAL, x, y, z);
     }
+  }
+  /**
+   * Block the data being lost when block stat e changes
+   * THANKS TO http://www.minecraftforge.net/forum/index.php?topic=29544.0
+   */
+  @Override
+  public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
+    return (oldState.getBlock() != newSate.getBlock());// : oldState != newSate;
   }
 }
