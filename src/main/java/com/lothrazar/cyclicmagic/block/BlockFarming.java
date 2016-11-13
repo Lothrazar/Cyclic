69058548a6008e1bce5e592faf8f5b6a50a71671
sam <samson.bassett@gmail.com>
2016-11-13 11:54:31 -0800

    failed to do ladder but success testing out cows

--- src/main/java/com/lothrazar/cyclicmagic/block/BlockFarming.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/BlockFarming.java
@@ -5,36 +5,68 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.passive.EntityMooshroom;
 import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockFarming extends Block implements IHasRecipe {
   // https://github.com/PrinceOfAmber/SamsPowerups/blob/master/FarmingBlocks/src/main/java/com/lothrazar/samsfarmblocks/BlockShearWool.java
   public BlockFarming() {
     super(Material.PISTON);
-    // TODO Auto-generated constructor stub
+    this.setHardness(5.0F);
+    this.setResistance(10.0F);
   }
+  public boolean isLadder(IBlockState state, IBlockAccess world, BlockPos pos, EntityLivingBase entity) { return true; }
+  @SideOnly(Side.CLIENT)
+  public BlockRenderLayer getBlockLayer()
+  {
+      return BlockRenderLayer.CUTOUT;
+  }
+  public boolean isOpaqueCube(IBlockState state)
+  {
+      return false;
+  }
+
+  public boolean isFullCube(IBlockState state)
+  {
+      return false;
+  }
+
   @Override
   public void addRecipe() {
-    // TODO Auto-generated method stub
+    GameRegistry.addShapelessRecipe(new ItemStack(this),
+        new ItemStack(Items.SHEARS, 1, 0),
+        Blocks.OBSIDIAN);
   }
   @Override
   public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity) {
-    if (entity instanceof EntityCow) {
+    if (entity instanceof EntityMooshroom) {
+      EntityMooshroom cow = (EntityMooshroom) entity;
+      //consume a BOWL
+      cow.playSound(SoundEvents.ENTITY_MOOSHROOM_SHEAR, 1.0F, 1.0F);
+      UtilEntity.dropItemStackInWorld(cow.worldObj, cow.getPosition(), new ItemStack(Blocks.RED_MUSHROOM));
+    }
+    else if (entity instanceof EntityCow) {
       EntityCow cow = (EntityCow) entity;
       cow.playSound(SoundEvents.ENTITY_COW_MILK, 1.0F, 1.0F);
       UtilEntity.dropItemStackInWorld(cow.worldObj, cow.getPosition(), new ItemStack(Items.MILK_BUCKET));
     }
-    if (entity instanceof EntitySheep) {
+    else if (entity instanceof EntitySheep) {
       EntitySheep sheep = (EntitySheep) entity;
       if (sheep.getSheared() == false && sheep.worldObj.isRemote == false) {
         //this part is the same as how EntitySheep goes
