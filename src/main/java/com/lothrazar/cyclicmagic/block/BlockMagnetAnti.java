8a1853eb20ac9e5283ba92add0c4728dd3f14410
sam <samson.bassett@gmail.com>
2016-11-19 21:12:23 -0800

    missing assets for antimagnet and changed recipe

--- src/main/java/com/lothrazar/cyclicmagic/block/BlockMagnetAnti.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/BlockMagnetAnti.java
@@ -1,7 +1,6 @@
 package com.lothrazar.cyclicmagic.block;
 import java.util.List;
 import javax.annotation.Nullable;
-import com.lothrazar.cyclicmagic.IHasConfig;
 import com.lothrazar.cyclicmagic.IHasRecipe;
 import com.lothrazar.cyclicmagic.block.tileentity.TileEntityMagnetAnti;
 import com.lothrazar.cyclicmagic.util.UtilChat;
@@ -11,7 +10,6 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
@@ -19,12 +17,11 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockMagnetAnti extends Block implements IHasRecipe, IHasConfig {
+public class BlockMagnetAnti extends Block implements IHasRecipe {
   protected static final AxisAlignedBB BOUNDS = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.03125D, 0.9375D);
   public BlockMagnetAnti() {
     super(Material.IRON);
@@ -65,16 +62,11 @@ public class BlockMagnetAnti extends Block implements IHasRecipe, IHasConfig {
   public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
     return true;
   }
-  //for transparency
   @SideOnly(Side.CLIENT)
   @Override
   public BlockRenderLayer getBlockLayer() {
-    return BlockRenderLayer.TRANSLUCENT;
+    return BlockRenderLayer.TRANSLUCENT; //for transparency
   }
-  /**
-   * Return true if an entity can be spawned inside the block (used to get the
-   * player's bed spawn location)
-   */
   public boolean canSpawnInBlock() {
     return true;
   }
@@ -84,20 +76,14 @@ public class BlockMagnetAnti extends Block implements IHasRecipe, IHasConfig {
         "sbs",
         "bxb",
         "sbs",
-        's', new ItemStack(Items.DIAMOND),
-        'b', new ItemStack(Blocks.field_189878_dg),//netherwart block
-        'x', new ItemStack(Blocks.field_189880_di));//bone block
-  }
-  @Override
-  public void syncConfig(Configuration config) {
-//    //old default was 140
-//    TileEntityMagnetAnti.TIMER_FULL = 10;//config.getInt("MagnetBlockTimer", Const.ConfigCategory.modpackMisc, 100, 5, 5000, "How fast it pulses.  Smaller numbers are faster");
-//    TileEntityMagnetAnti.ITEM_HRADIUS = 32;//config.getInt("MagnetBlockDistance", Const.ConfigCategory.modpackMisc, 16, 2, 128, "Distance it pulls items from.");
-//    TileEntityMagnetAnti.ITEM_VRADIUS = 2;//config.getInt("MagnetBlockHeight", Const.ConfigCategory.modpackMisc, 2, 1, 128, "Height it pulls at items");
+        's', new ItemStack(Blocks.DIAMOND_BLOCK),
+        'b', new ItemStack(Blocks.field_189878_dg), //netherwart block
+        'x', new ItemStack(Blocks.BEACON));
   }
   @SideOnly(Side.CLIENT)
   public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
     String s = UtilChat.lang("tile.magnet_anti_block.tooltip").replace("$t$", TileEntityMagnetAnti.TIMER_FULL + "");
+    s = s.replace("$r$", TileEntityMagnetAnti.ITEM_HRADIUS + "");
     tooltip.add(s);
   }
 }
