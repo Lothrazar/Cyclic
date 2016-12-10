aae7a16d1675d78cbf6aa3fd0234999b070a63b7
sam <samson.bassett@gmail.com>
2016-12-09 22:21:01 -0800

    #186 cyclic build scepter compatibility with chisel and bits blocks

--- src/main/java/com/lothrazar/cyclicmagic/spell/ISpellFromServer.java
+++ src/main/java/com/lothrazar/cyclicmagic/spell/ISpellFromServer.java
@@ -1,7 +1,8 @@
 package com.lothrazar.cyclicmagic.spell;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 
 public interface ISpellFromServer {
-  public void castFromServer(BlockPos posMouseover, BlockPos posOffset, EntityPlayer p);
+  public void castFromServer(BlockPos posMouseover, BlockPos posOffset, EnumFacing sideMouseover,EntityPlayer p);
 }
