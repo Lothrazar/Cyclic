f97c6da4a95949e1e75032ff40145eb732e9ecbc
sam <samson.bassett@gmail.com>
2016-12-27 13:18:24 -0800

    recipe added for detector

--- src/main/java/com/lothrazar/cyclicmagic/block/BlockDetector.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/BlockDetector.java
@@ -1,15 +1,20 @@
 package com.lothrazar.cyclicmagic.block;
+import com.lothrazar.cyclicmagic.IHasRecipe;
 import com.lothrazar.cyclicmagic.block.tileentity.TileEntityDetector;
 import com.lothrazar.cyclicmagic.gui.ModGuiHandler;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class BlockDetector extends BlockBaseHasTile {
+public class BlockDetector extends BlockBaseHasTile implements IHasRecipe {
   public BlockDetector() {
     super(Material.IRON);
     this.setTooltip("tile.entity_detector.tooltip");
@@ -19,7 +24,7 @@ public class BlockDetector extends BlockBaseHasTile {
   @Override
   public int getWeakPower(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
     TileEntityDetector te = (TileEntityDetector) blockAccess.getTileEntity(pos);
-    if(te == null){return 0;}
+    if (te == null) { return 0; }
     return te.isPowered() ? 15 : 0;
   }
   @Override
@@ -33,5 +38,16 @@ public class BlockDetector extends BlockBaseHasTile {
   @Override
   public TileEntity createTileEntity(World worldIn, IBlockState state) {
     return new TileEntityDetector();
+  }
+  @Override
+  public void addRecipe() {
+    GameRegistry.addRecipe(new ItemStack(this),
+        "dcd",
+        " q ",
+        "r r",
+        'r', Items.REDSTONE,
+        'q', Blocks.QUARTZ_BLOCK,
+        'c', Blocks.COAL_BLOCK,
+        'd', Items.DIAMOND);
   }
 }
