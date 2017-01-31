d7e10dbf0aaf3c6424d926f9e61b07f3bc673d36
sam <samson.bassett@gmail.com>
2017-01-31 00:26:51 -0800

    remote lever basically feature complete. not linked to password trigger at all. updated graphics and lang. needs config and deploy testing

--- src/main/java/com/lothrazar/cyclicmagic/item/ItemPasswordRemote.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/ItemPasswordRemote.java
@@ -1,12 +1,9 @@
 package com.lothrazar.cyclicmagic.item;
 import java.util.List;
 import com.lothrazar.cyclicmagic.IHasRecipe;
-import com.lothrazar.cyclicmagic.block.BlockPassword;
-import com.lothrazar.cyclicmagic.block.tileentity.TileEntityPassword;
 import com.lothrazar.cyclicmagic.util.UtilChat;
 import com.lothrazar.cyclicmagic.util.UtilNBT;
 import com.lothrazar.cyclicmagic.util.UtilSound;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockLever;
 import net.minecraft.block.BlockStoneSlab;
 import net.minecraft.block.state.IBlockState;
@@ -26,42 +23,37 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemPasswordRemote extends BaseItem implements IHasRecipe {
-  Block block;
-  public ItemPasswordRemote(Block passwordBlock) {
+  public ItemPasswordRemote() {
     this.setMaxStackSize(1);
-    block =  Blocks.LEVER;
   }
   @SideOnly(Side.CLIENT)
   public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
-   
     BlockPos pointer = UtilNBT.getItemStackBlockPos(stack);
-    if (pointer != null){
-      tooltip.add(TextFormatting.RED+UtilChat.blockPosToString(pointer));
+    if (pointer != null) {
+      tooltip.add(TextFormatting.RED + UtilChat.blockPosToString(pointer));
     }
     super.addInformation(stack, playerIn, tooltip, advanced);
   }
   public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
     if (worldIn.getBlockState(pos).getBlock() instanceof BlockLever) {
       UtilNBT.setItemStackBlockPos(stack, pos);
-      if(worldIn.isRemote)
-      UtilChat.addChatMessage(playerIn, this.getUnlocalizedName() +  ".saved");
-      
+      if (worldIn.isRemote)
+        UtilChat.addChatMessage(playerIn, this.getUnlocalizedName() + ".saved");
       UtilSound.playSound(playerIn, SoundEvents.BLOCK_LEVER_CLICK);
     }
-
     return EnumActionResult.PASS;
   }
   @Override
   public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World worldIn, EntityPlayer playerIn, EnumHand hand) {
     BlockPos pointer = UtilNBT.getItemStackBlockPos(stack);
-    if (pointer == null ) {
-      if(worldIn.isRemote)
-      UtilChat.addChatMessage(playerIn, this.getUnlocalizedName()+".invalid");
+    if (pointer == null) {
+      if (worldIn.isRemote)
+        UtilChat.addChatMessage(playerIn, this.getUnlocalizedName() + ".invalid");
       return new ActionResult<ItemStack>(EnumActionResult.FAIL, stack);
     }
- 
     IBlockState blockState = worldIn.getBlockState(pointer);
-    if(blockState == null || blockState.getBlock() != Blocks.LEVER){
+    if (blockState == null || blockState.getBlock() != Blocks.LEVER) { 
+      UtilChat.addChatMessage(playerIn, this.getUnlocalizedName() + ".invalid");
       return new ActionResult<ItemStack>(EnumActionResult.FAIL, stack);
     }
     boolean hasPowerHere = blockState.getValue(BlockLever.POWERED);//this.block.getStrongPower(blockState, worldIn, pointer, EnumFacing.UP) > 0;
@@ -74,6 +66,6 @@ public class ItemPasswordRemote extends BaseItem implements IHasRecipe {
     GameRegistry.addShapelessRecipe(new ItemStack(this),
         new ItemStack(Blocks.STONE_SLAB, 1, BlockStoneSlab.EnumType.STONE.getMetadata()),
         Blocks.STONE_BUTTON,
-        this.block);
+        Blocks.LEVER);
   }
 }
