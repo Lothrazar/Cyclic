97f9413ad0e29a92c9d1f3adc89cb26e0d800bf5
sam <samson.bassett@gmail.com>
2017-02-05 14:33:41 -0800

    custom material and resources for hermetic boots

--- src/main/java/com/lothrazar/cyclicmagic/item/gear/ItemPowerArmor.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/gear/ItemPowerArmor.java
@@ -1,4 +1,6 @@
 package com.lothrazar.cyclicmagic.item.gear;
+import java.util.List;
+import com.lothrazar.cyclicmagic.util.UtilChat;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
@@ -8,20 +10,22 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class ItemPowerArmor extends ItemArmor {
-  public ItemPowerArmor(EntityEquipmentSlot armorType) {
-    super(ArmorMaterial.LEATHER, 0, armorType);
+  public ItemPowerArmor(ArmorMaterial material, EntityEquipmentSlot armorType) {
+    super(material, 0, armorType);
   }
   @Override
   public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
     if (this.armorType == EntityEquipmentSlot.FEET) {
       BlockPos belowPos = player.getPosition().down();
       if (world.containsAnyLiquid(new AxisAlignedBB(belowPos)) && world.isAirBlock(player.getPosition()) && player.motionY < 0
-          && !player.isSneaking()) {// let them slip down into it
+          && !player.isSneaking()) {// let them slip down into it when sneaking
         player.motionY = 0;// stop falling
         player.onGround = true; // act as if on solid ground
-        //      player.setAIMoveSpeed(0.1F);// walking and not sprinting is this
       }
-    }
-    //else->in case i add other powers
+    } //else->in case i add other powers
+  }
+  @Override
+  public void addInformation(ItemStack held, EntityPlayer player, List<String> list, boolean par4) {
+    list.add(UtilChat.lang(this.getUnlocalizedName() + ".tooltip"));
   }
 }
--- src/main/java/com/lothrazar/cyclicmagic/module/GearBootsModule.java
+++ src/main/java/com/lothrazar/cyclicmagic/module/GearBootsModule.java
@@ -2,24 +2,45 @@ package com.lothrazar.cyclicmagic.module;
 import com.lothrazar.cyclicmagic.IHasConfig;
 import com.lothrazar.cyclicmagic.item.gear.ItemPowerArmor;
 import com.lothrazar.cyclicmagic.registry.ItemRegistry;
+import com.lothrazar.cyclicmagic.registry.MaterialRegistry;
+import com.lothrazar.cyclicmagic.util.Const;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class GearBootsModule extends BaseModule implements IHasConfig {
-  private boolean enableWaterGear=true;
-
+  private static final int maxDamageFactorLeather = 5;
+  private static final String materialName = "power";
+  private boolean enableWaterGear = true;
   @Override
   public void onInit() {
     if (enableWaterGear) {
- 
- 
-      Item water_boots = new ItemPowerArmor(EntityEquipmentSlot.FEET);
-      ItemRegistry.addItem(water_boots, "water_boots"); 
+      MaterialRegistry.powerArmorMaterial = EnumHelper.addArmorMaterial(materialName, Const.MODRES + materialName,
+          maxDamageFactorLeather, // affects DURABILITY 
+          new int[] {
+              ArmorMaterial.LEATHER.getDamageReductionAmount(EntityEquipmentSlot.FEET), ArmorMaterial.LEATHER.getDamageReductionAmount(EntityEquipmentSlot.LEGS), ArmorMaterial.LEATHER.getDamageReductionAmount(EntityEquipmentSlot.CHEST), ArmorMaterial.LEATHER.getDamageReductionAmount(EntityEquipmentSlot.HEAD)
+          },
+          ArmorMaterial.LEATHER.getEnchantability(),
+          ArmorMaterial.LEATHER.getSoundEvent(),
+          ArmorMaterial.LEATHER.getToughness());
+      MaterialRegistry.powerArmorMaterial.customCraftingMaterial = Items.LEATHER;
+      Item water_boots = new ItemPowerArmor(MaterialRegistry.powerArmorMaterial, EntityEquipmentSlot.FEET);
+      ItemRegistry.addItem(water_boots, "liquid_boots");
+      GameRegistry.addShapedRecipe(new ItemStack(water_boots),
+          "   ", "p p", "o o",
+          'o', Blocks.OBSIDIAN,
+          'p', new ItemStack(Items.DYE, 1, EnumDyeColor.MAGENTA.getDyeDamage()));
     }
   }
   @Override
   public void syncConfig(Configuration config) {
-//    enableEmeraldGear = config.getBoolean("Emerald Gear", Const.ConfigCategory.content, true, "Emerald armor and tools that are slightly weaker than diamond. " + Const.ConfigCategory.contentDefaultText);
+    enableWaterGear = config.getBoolean("HermeticBoots", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
   }
 }
