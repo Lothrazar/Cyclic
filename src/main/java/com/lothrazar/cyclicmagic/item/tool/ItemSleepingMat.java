b979a86eb0e94660129cec8c3aba7d537210101d
sam <samson.bassett@gmail.com>
2017-02-25 00:40:30 -0800

    some failattemps atn reflection to fix sleeping mat

--- src/main/java/com/lothrazar/cyclicmagic/item/tool/ItemSleepingMat.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/tool/ItemSleepingMat.java
@@ -8,20 +8,27 @@ import com.lothrazar.cyclicmagic.registry.CapabilityRegistry.IPlayerExtendedProp
 import com.lothrazar.cyclicmagic.util.Const;
 import com.lothrazar.cyclicmagic.util.UtilChat;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayer.SleepResult;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SPacketUseBed;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.event.entity.player.SleepingLocationCheckEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -34,11 +41,20 @@ public class ItemSleepingMat extends BaseTool implements IHasRecipe, IHasConfig 
     super(100);
   }
   @Override
-  public ActionResult<ItemStack> onItemRightClick(  World world, EntityPlayer player, EnumHand hand) {
+  public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
     ItemStack stack = player.getHeldItem(hand);
     if (!world.isRemote) {
-      final EntityPlayer.SleepResult result = player.trySleep(player.getPosition());
+      EntityPlayerMP mp = (EntityPlayerMP )player;
+      //      final EntityPlayer.SleepResult result = player.trySleep(player.getPosition());
+      //trySleep was changed in 1.11.2 to literally check for the specific exact  Blocks.BED in world. because fuck modders amirite?
+      //and it just assumes unsafely its there and then dies.
+      
+      EntityPlayer.SleepResult result = this.canPlayerSleep(player, world);
+
+      System.out.println("canPlayerSleep"+result);
+      
       if (result == EntityPlayer.SleepResult.OK) {
+        
         final IPlayerExtendedProperties sleep = CapabilityRegistry.getPlayerProperties(player);
         if (sleep != null) {
           sleep.setSleeping(true);
@@ -53,6 +69,23 @@ public class ItemSleepingMat extends BaseTool implements IHasRecipe, IHasConfig 
           //should never happen... but just in case
           UtilChat.addChatMessage(player, "tile.bed.noSleep");
         }
+
+
+        System.out.println("start");
+        ObfuscationReflectionHelper.setPrivateValue(EntityPlayer.class, player, true, "sleeping", "field_71083_bS");
+        ObfuscationReflectionHelper.setPrivateValue(EntityPlayer.class, player, 0, "sleepTimer", "field_71076_b");
+        player.motionX = player.motionZ = player.motionY = 0;
+        world.updateAllPlayersSleepingFlag();
+
+        world.setBlockState(player.getPosition(), Blocks.BED.getDefaultState());
+//        SPacketUseBed sleepPacket = new SPacketUseBed(player, player.getPosition());
+//        mp.getServerWorld().getEntityTracker().sendToTracking(player, sleepPacket);
+//        mp.connection.sendPacket(sleepPacket);
+        
+
+        System.out.println("end");
+        
+        //as with 1.10.2, we do not set   player.bedLocation = on purpose
         return ActionResult.newResult(EnumActionResult.SUCCESS, stack);
       }
       else {
@@ -61,18 +94,57 @@ public class ItemSleepingMat extends BaseTool implements IHasRecipe, IHasConfig 
     }
     return ActionResult.newResult(EnumActionResult.PASS, stack);
   }
+  /**
+   * hack in the vanilla sleep test, or at least something similar
+   * 
+   * @param player
+   * @param world
+   * @return
+   */
+  private SleepResult canPlayerSleep(EntityPlayer player, World world) {
+    if (player.isEntityAlive() == false) { return EntityPlayer.SleepResult.OTHER_PROBLEM; }
+    if (world.isDaytime()) { return EntityPlayer.SleepResult.NOT_POSSIBLE_NOW; }
+    PlayerSleepInBedEvent event = new PlayerSleepInBedEvent(player, player.getPosition());
+    MinecraftForge.EVENT_BUS.post(event);
+    if (event.getResultStatus() != null) { return event.getResultStatus(); }
+    return EntityPlayer.SleepResult.OK;
+  }
   @SubscribeEvent
   public void onBedCheck(SleepingLocationCheckEvent evt) {
-    final IPlayerExtendedProperties sleep = evt.getEntityPlayer().getCapability(ModCyclic.CAPABILITYSTORAGE, null);
+      final IPlayerExtendedProperties sleep = evt.getEntityPlayer().getCapability(ModCyclic.CAPABILITYSTORAGE, null);
     if (sleep != null && sleep.isSleeping()) {
+      System.out.println("onBedCheck  ALLOW");
       evt.setResult(Result.ALLOW);
     }
   }
   @SubscribeEvent
+  public void handleSleepInBed(PlayerSleepInBedEvent event) {
+    System.out.println("handleSleepInBed");
+    final IPlayerExtendedProperties sleep = event.getEntityPlayer().getCapability(ModCyclic.CAPABILITYSTORAGE, null);
+    if (sleep != null && sleep.isSleeping()) {
+      System.out.println("handleSleepInBed  OK");
+      event.setResult(EntityPlayer.SleepResult.OK);
+    }
+  }
+  @SubscribeEvent
   public void onWakeUp(PlayerWakeUpEvent evt) {
-    final IPlayerExtendedProperties sleep = evt.getEntityPlayer().getCapability(ModCyclic.CAPABILITYSTORAGE, null);
-    if (sleep != null) {
+    EntityPlayer p = evt.getEntityPlayer();
+    final IPlayerExtendedProperties sleep = p.getCapability(ModCyclic.CAPABILITYSTORAGE, null);
+    if (sleep != null && sleep.isSleeping()) {
+      
+      
+      World world = p.getEntityWorld();
+      BlockPos pos = p.getPosition();
       sleep.setSleeping(false);
+      
+      
+      if(
+          world.getBlockState(pos).getBlock() == Blocks.BED){
+
+        System.out.println("onWakeUpDESTRY");
+        world.setBlockToAir(pos); 
+      }
+      
     }
   }
   @Override
