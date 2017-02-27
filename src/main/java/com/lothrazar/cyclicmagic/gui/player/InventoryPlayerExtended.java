011ce2ed1f5f3f1132e446efaf883a0d8dc63841
sam <samson.bassett@gmail.com>
2017-02-26 20:58:35 -0800

    fix crash on startup #279

--- src/main/java/com/lothrazar/cyclicmagic/gui/player/InventoryPlayerExtended.java
+++ src/main/java/com/lothrazar/cyclicmagic/gui/player/InventoryPlayerExtended.java
@@ -24,7 +24,7 @@ public class InventoryPlayerExtended extends InventoryBase implements IInventory
   public static final int IROW = 4;
   public static final int ICOL = 8;
   public InventoryPlayerExtended(EntityPlayer player) {
-    super(IROW * ICOL+20);//...why is it 20??
+    super(IROW * ICOL + 20);//...why is it 20??
     this.player = new WeakReference<EntityPlayer>(player);
   }
   public Container getEventHandler() {
@@ -93,6 +93,31 @@ public class InventoryPlayerExtended extends InventoryBase implements IInventory
     syncSlotToClients(index);
   }
   @Override
+  public int getSizeInventory() {
+    return this.inv.size();
+  }
+  @Override
+  public ItemStack getStackInSlot(int s) {
+    //somehow here we are getting java.lang.AbstractMethodError: com.lothrazar.cyclicmagic.gui.player.InventoryPlayerExtended.func_70301_a(I)Lnet/minecraft/item/ItemStack;
+    //which translates to getStackInSlot() that returns ItemStack
+//    ModCyclic.logger.info("stack " + s);
+//    ModCyclic.logger.info("player?  " + this.player);
+//    if (this.player != null && this.player.get() != null) {
+//      ModCyclic.logger.info("isRemote    " + this.player.get().getEntityWorld().isRemote);
+//    }
+//    ModCyclic.logger.info("this " + this);
+//    ModCyclic.logger.info("this.inv " + this.inv);
+//    ModCyclic.logger.info("this.inv.size " + this.inv.size());
+    try {
+      return s >= this.getSizeInventory() ? ItemStack.EMPTY : this.inv.get(s);
+    }
+    catch (Exception e) {
+      ModCyclic.logger.error("[getStackInSlot] error: " + s);
+      e.printStackTrace();
+    }
+    return ItemStack.EMPTY;
+  }
+  @Override
   public int getInventoryStackLimit() {
     return 64;
   }
@@ -157,7 +182,7 @@ public class InventoryPlayerExtended extends InventoryBase implements IInventory
   }
   public void dropItems(List<EntityItem> drops, BlockPos pos) {
     for (int i = 0; i < this.getSizeInventory(); ++i) {
-      if (this.inv.get(i)!= null) {
+      if (this.inv.get(i) != ItemStack.EMPTY) {
         World world = player.get().getEntityWorld();
         EntityItem ei = new EntityItem(world, pos.getX(), pos.getY(), pos.getZ(), this.inv.get(i).copy());
         ei.setPickupDelay(40);
