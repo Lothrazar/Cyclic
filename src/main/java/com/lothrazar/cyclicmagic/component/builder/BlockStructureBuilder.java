1817867146fc61241ceca2df2ec469c10a6fc5fd
sam <samson.bassett@gmail.com>
2017-05-01 23:41:40 -0700

    structure builder preview render

--- src/main/java/com/lothrazar/cyclicmagic/component/builder/BlockStructureBuilder.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/builder/BlockStructureBuilder.java
@@ -33,7 +33,7 @@ public class BlockStructureBuilder extends BlockBaseFacingInventory implements I
   public void initModel() {
     ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     // Bind our TESR to our tile entity
-    ClientRegistry.bindTileEntitySpecialRenderer(TileEntityStructureBuilder.class, new MachineTESR(this.getUnlocalizedName(), 0));
+    ClientRegistry.bindTileEntitySpecialRenderer(TileEntityStructureBuilder.class, new StructureBuilderTESR(this.getUnlocalizedName(), 0));
   }
   @Override
   public TileEntity createTileEntity(World worldIn, IBlockState state) {
