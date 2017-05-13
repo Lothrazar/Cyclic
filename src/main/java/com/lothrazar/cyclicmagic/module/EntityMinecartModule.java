c06b7f7ac3b687e8fa736f5dbb743c93f0443d5a
sam <samson.bassett@gmail.com>
2017-05-12 23:47:38 -0700

    MC entities in own module

---
+++ src/main/java/com/lothrazar/cyclicmagic/module/EntityMinecartModule.java
@@ -0,0 +1,48 @@
+package com.lothrazar.cyclicmagic.module;
+import com.lothrazar.cyclicmagic.IHasConfig;
+import com.lothrazar.cyclicmagic.entity.EntityGoldFurnaceMinecart;
+import com.lothrazar.cyclicmagic.entity.EntityGoldMinecart;
+import com.lothrazar.cyclicmagic.entity.EntityStoneMinecart;
+import com.lothrazar.cyclicmagic.item.ItemGoldFurnaceMinecart;
+import com.lothrazar.cyclicmagic.item.ItemGoldMinecart;
+import com.lothrazar.cyclicmagic.item.ItemStoneMinecart;
+import com.lothrazar.cyclicmagic.registry.EntityProjectileRegistry;
+import com.lothrazar.cyclicmagic.registry.ItemRegistry;
+import com.lothrazar.cyclicmagic.util.Const;
+import net.minecraftforge.common.config.Configuration;
+
+public class EntityMinecartModule extends BaseModule implements IHasConfig {
+  private boolean goldMinecart;
+  private boolean stoneMinecart;
+  @Override
+  public void onPreInit() {
+    if (goldMinecart) {
+      ItemGoldMinecart gold_minecart = new ItemGoldMinecart();
+      ItemRegistry.register(gold_minecart, "gold_minecart");
+      EntityGoldMinecart.dropItem = gold_minecart;
+      EntityProjectileRegistry.registerModEntity(EntityGoldMinecart.class, "goldminecart", 1100);
+      ItemGoldFurnaceMinecart gold_furnace_minecart = new ItemGoldFurnaceMinecart();
+      ItemRegistry.register(gold_furnace_minecart, "gold_furnace_minecart");
+      EntityGoldFurnaceMinecart.dropItem = gold_furnace_minecart;
+      EntityProjectileRegistry.registerModEntity(EntityGoldFurnaceMinecart.class, "goldfurnaceminecart", 1101);
+    }
+    if (stoneMinecart) {
+      ItemStoneMinecart stone_minecart = new ItemStoneMinecart();
+      ItemRegistry.register(stone_minecart, "stone_minecart");
+      EntityStoneMinecart.dropItem = stone_minecart;
+      EntityProjectileRegistry.registerModEntity(EntityStoneMinecart.class, "stoneminecart", 1102);
+    }
+    //if i have a mob on a LEAD< i can put it in a minecart with thehit
+    //maybe 2 passengers..?? idk
+    //connect together??
+    //DISPENSERR minecart
+    //??FLUID CART?
+    //TURRET CART:? shoots arrows
+    //ONE THAT CAN HOLD ANY ITEM
+  }
+  @Override
+  public void syncConfig(Configuration config) {
+    goldMinecart = config.getBoolean("GoldMinecart", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
+    stoneMinecart = config.getBoolean("StoneMinecart", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
+  }
+}
