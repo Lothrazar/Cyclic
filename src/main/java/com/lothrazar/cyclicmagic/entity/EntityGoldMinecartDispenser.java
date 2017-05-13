2478f04c93b7746d08613e864dd5c6b78611f21f
sam <samson.bassett@gmail.com>
2017-05-13 13:28:43 -0700

    self collision fixes , well at least a hacky fix

--- src/main/java/com/lothrazar/cyclicmagic/entity/EntityGoldMinecartDispenser.java
+++ src/main/java/com/lothrazar/cyclicmagic/entity/EntityGoldMinecartDispenser.java
@@ -1,46 +1,52 @@
 package com.lothrazar.cyclicmagic.entity;
 import java.util.Random;
-import com.lothrazar.cyclicmagic.ModCyclic;
 import net.minecraft.block.BlockDispenser;
-import net.minecraft.block.BlockRailBase;
 import net.minecraft.block.BlockSourceImpl;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
 import net.minecraft.dispenser.IBehaviorDispenseItem;
-import net.minecraft.dispenser.IBlockSource;
-import net.minecraft.entity.item.EntityMinecart;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityMinecartChest;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerDispenser;
-import net.minecraft.item.ItemMinecart;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntityDispenser;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class EntityGoldMinecartDispenser extends EntityMinecartChest {
-  public BlockPos ORIGIN_POS = new BlockPos(0, 0, 0);
+  private static final int TIME_BTW_DROPS = 20;
+  private int timeSinceDropped = 0;
   private FakeWorld fakeWorld;
   public EntityGoldMinecartDispenser(World worldIn) {
     super(worldIn);
-    fakeWorld = new FakeWorld(worldIn,this);
+    fakeWorld = new FakeWorld(worldIn, this);
   }
   public EntityGoldMinecartDispenser(World worldIn, double x, double y, double z) {
     super(worldIn, x, y, z);
-    fakeWorld = new FakeWorld(worldIn,this);
+    fakeWorld = new FakeWorld(worldIn, this);
   }
   public int getSizeInventory() {
     return 9;
   }
   public IBlockState getDefaultDisplayTile() {
-   
     return Blocks.DISPENSER.getDefaultState();//.withProperty(BlockChest.FACING, EnumFacing.NORTH);
+  }
+  @Override
+  protected void writeEntityToNBT(NBTTagCompound compound) {
+    compound.setInteger("tdr", timeSinceDropped);
+    super.writeEntityToNBT(compound);
+  }
+  @Override
+  protected void readEntityFromNBT(NBTTagCompound compound) {
+    timeSinceDropped = compound.getInteger("tdr");
+    super.readEntityFromNBT(compound);
   }
   public String getGuiID() {
     return "minecraft:dispenser";
@@ -52,8 +58,10 @@ public class EntityGoldMinecartDispenser extends EntityMinecartChest {
   }
   @Override
   public void onActivatorRailPass(int x, int y, int z, boolean receivingPower) {
+    EnumFacing fac = this.getAdjustedHorizontalFacing();//.getOpposite();
+    this.setDisplayTile(getDefaultDisplayTile().withProperty(BlockDispenser.FACING, fac));
+    //ModCyclic.logger.info("this.getAdjustedHorizontalFacing()"+fac);
     if (receivingPower) {
-      //      this.minecartContainerItems.get(p_get_1_) 
       this.dispense(this.fakeWorld, new BlockPos(x, y, z));
     }
   }
@@ -64,33 +72,36 @@ public class EntityGoldMinecartDispenser extends EntityMinecartChest {
    * @param pos
    */
   protected void dispense(World worldIn, BlockPos pos) {
- 
+    if (this.timeSinceDropped > 0) {
+      this.timeSinceDropped--;
+      return;
+    }
     BlockSourceImpl source = new BlockSourceImpl(worldIn, pos);
- 
     int i = this.getDispenseSlot(worldIn.rand);
     if (i < 0) {
-      worldIn.playEvent(1001, pos, 0);
+      world.playEvent(1001, pos, 0);
     }
     else {
       ItemStack itemstack = this.getStackInSlot(i);
       IBehaviorDispenseItem ibehaviordispenseitem = this.getBehavior(itemstack);
-    ModCyclic.logger.info("BEHAVIOR "+ibehaviordispenseitem);
-      try{
-        ItemStack result = ibehaviordispenseitem.dispense(source, itemstack);
+      //    ModCyclic.logger.info("BEHAVIOR "+ibehaviordispenseitem);
+      ItemStack result = ibehaviordispenseitem.dispense(source, itemstack);
       this.setInventorySlotContents(i, result);
-      }catch(Exception e){
-        System.out.println("yep error eh "+e.getMessage());
-      }
+      this.timeSinceDropped = TIME_BTW_DROPS;
     }
-    //      }
   }
   @Override
-
-
-  public void killMinecart(DamageSource source)
-  {
-    ModCyclic.logger.info("killMinecart "+source);
-      super.killMinecart(source);
+  public boolean attackEntityFrom(DamageSource source, float amount) {
+    if (source.getEntity() == this || source.getEntity() instanceof EntityArrow) {
+      amount = 0;
+      return false;
+    }
+    return true;
+  }
+  @Override
+  public AxisAlignedBB getCollisionBox(Entity entityIn) {
+    if (entityIn instanceof EntityArrow) { return new AxisAlignedBB(this.getPosition(), this.getPosition()); }
+    return super.getCollisionBox(entityIn);
   }
   /**
    * from TileEntityDispenser
@@ -115,7 +126,7 @@ public class EntityGoldMinecartDispenser extends EntityMinecartChest {
    * @return
    */
   protected IBehaviorDispenseItem getBehavior(ItemStack stack) {
-    ModCyclic.logger.info("BEHAVIOR??? "+ stack);
+    //    ModCyclic.logger.info("BEHAVIOR??? "+ stack);
     return BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.getObject(stack.getItem());
   }
 }
