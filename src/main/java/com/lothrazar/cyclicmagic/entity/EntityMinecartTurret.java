a834d319ee7677624ec745cebd5d4c6a2779ae38
sam <samson.bassett@gmail.com>
2017-05-13 17:17:20 -0700

    turret minecart extends gold

--- src/main/java/com/lothrazar/cyclicmagic/entity/EntityMinecartTurret.java
+++ src/main/java/com/lothrazar/cyclicmagic/entity/EntityMinecartTurret.java
@@ -1,17 +1,11 @@
 package com.lothrazar.cyclicmagic.entity;
-import java.util.Random;
-import net.minecraft.block.BlockDispenser;
+import net.minecraft.block.BlockObserver;
 import net.minecraft.block.BlockRailBase;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.item.EntityMinecartChest;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntityTippedArrow;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.inventory.Container;
-import net.minecraft.inventory.ContainerDispenser;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -20,9 +14,12 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class EntityMinecartTurret extends EntityMinecartChest {
-  public static Item dropItem = Items.MINECART;//override with gold minecart on registry, this is here just for nonnull
+public class EntityMinecartTurret extends EntityGoldMinecart {
+  private static final float YAW = 0.1F;
+  private static final float VELOCITY = 1.1F;
+  private static final float INACCRACY = 6.0F;
   private static final int TIME_BTW_DROPS = 40;
+  public static Item dropItem = Items.MINECART;//override with gold minecart on registry, this is here just for nonnull
   private int timeSinceDropped = 0;
   public EntityMinecartTurret(World worldIn) {
     super(worldIn);
@@ -36,7 +33,7 @@ public class EntityMinecartTurret extends EntityMinecartChest {
     return 0;
   }
   public IBlockState getDefaultDisplayTile() {
-    return Blocks.DISPENSER.getDefaultState();//.withProperty(BlockChest.FACING, EnumFacing.NORTH);
+    return Blocks.OBSERVER.getDefaultState();//.withProperty(BlockChest.FACING, EnumFacing.NORTH);
   }
   @Override
   protected void writeEntityToNBT(NBTTagCompound compound) {
@@ -48,19 +45,8 @@ public class EntityMinecartTurret extends EntityMinecartChest {
     timeSinceDropped = compound.getInteger("tdr");
     super.readEntityFromNBT(compound);
   }
-  public String getGuiID() {
-    return "";// "minecraft:dispenser";
-  }
-  @Override
-  public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn) {
-    this.addLoot(playerIn);
-    return new ContainerDispenser(playerInventory, this);
-  }
   @Override
   public void onActivatorRailPass(int x, int y, int z, boolean receivingPower) {
-    //    EnumFacing fac = this.getAdjustedHorizontalFacing();//.getOpposite();
-    //    this.setDisplayTile(getDefaultDisplayTile().withProperty(BlockDispenser.FACING, fac));
-    //ModCyclic.logger.info("this.getAdjustedHorizontalFacing()"+fac);
     if (receivingPower) {
       this.dispense(new BlockPos(x, y, z));
     }
@@ -77,7 +63,7 @@ public class EntityMinecartTurret extends EntityMinecartChest {
       return;
     }
     this.timeSinceDropped = TIME_BTW_DROPS;
-    EnumFacing enumfacing = this.getDisplayTile().getValue(BlockDispenser.FACING);
+    EnumFacing enumfacing = this.getDisplayTile().getValue(BlockObserver.FACING);
     shootThisDirection(enumfacing);
     shootThisDirection(enumfacing.getOpposite());
   }
@@ -86,32 +72,8 @@ public class EntityMinecartTurret extends EntityMinecartChest {
     EntityTippedArrow entitytippedarrow = new EntityTippedArrow(world, position.getX(), position.getY(), position.getZ());
     entitytippedarrow.setPotionEffect(new ItemStack(Items.ARROW));
     entitytippedarrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
- 
-    entitytippedarrow.setThrowableHeading((double) enumfacing.getFrontOffsetX(), 0.1F, (double) enumfacing.getFrontOffsetZ(), this.getProjectileVelocity(), this.getProjectileInaccuracy());
-    
-    
+    entitytippedarrow.setThrowableHeading((double) enumfacing.getFrontOffsetX(), YAW, (double) enumfacing.getFrontOffsetZ(), VELOCITY, INACCRACY);
     world.spawnEntity(entitytippedarrow);
-  }
-  protected float getProjectileInaccuracy() {
-    return 6.0F;
-  }
-  protected float getProjectileVelocity() {
-    return 1.1F;
-  }
-  /**
-   * from TileEntityDispenser
-   * 
-   * @return
-   */
-  public int getDispenseSlot(Random rand) {
-    int i = -1;
-    int j = 1;
-    for (int k = 0; k < this.getSizeInventory(); ++k) {
-      if (!((ItemStack) this.getStackInSlot(k)).isEmpty() && rand.nextInt(j++) == 0) {
-        i = k;
-      }
-    }
-    return i;
   }
   @Override
   protected void moveAlongTrack(BlockPos pos, IBlockState state) {
@@ -149,7 +111,7 @@ public class EntityMinecartTurret extends EntityMinecartChest {
     super.moveAlongTrack(pos, state);
     if (fac != null) {
       //      ModCyclic.logger.info(raildirection+" setDisplayTile  "+fac);
-      this.setDisplayTile(getDefaultDisplayTile().withProperty(BlockDispenser.FACING, fac));
+      this.setDisplayTile(getDefaultDisplayTile().withProperty(BlockObserver.FACING, fac));
     }
   }
   @Override
