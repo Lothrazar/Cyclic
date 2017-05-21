c8f1a4b80ac2aa924988b931114e53e3d381bb21
sam <samson.bassett@gmail.com>
2017-05-21 16:55:39 -0700

    pattern builder with new ctr code

--- src/main/java/com/lothrazar/cyclicmagic/component/pattern/ContainerPattern.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/pattern/ContainerPattern.java
@@ -14,16 +14,7 @@ public class ContainerPattern extends ContainerBaseMachine {
   public static final int SLOTX_START = 8;
   public static final int SLOTY_START = 90;
   protected TileEntityPatternBuilder tileEntity;
-  private int tileOX;
-  private int tileOY;
-  private int tileOZ;
-  private int tileSOX;
-  private int tileSOY;
-  private int tileSOZ;
-  private int tileSIZER;
-  private int tileHEIGHT;
-  private int tileREDSTONE;
-  private int tileRENDERPARTICLES;
+
   public ContainerPattern(InventoryPlayer inventoryPlayer, TileEntityPatternBuilder te) {
     this.playerOffsetY = 130;
     tileEntity = te;
@@ -63,63 +54,6 @@ public class ContainerPattern extends ContainerBaseMachine {
       slotObject.onTake(player, stackInSlot);
     }
     return stack;
-  }
-  @Override
-  public void detectAndSendChanges() {
-    super.detectAndSendChanges();
-    for (int i = 0; i < this.listeners.size(); ++i) {
-      IContainerListener icontainerlistener = (IContainerListener) this.listeners.get(i);
-      int idx = TileEntityPatternBuilder.Fields.OFFTARGX.ordinal();
-      if (this.tileOX != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.OFFTARGY.ordinal();
-      if (this.tileOY != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.OFFTARGZ.ordinal();
-      if (this.tileOZ != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.OFFSRCX.ordinal();
-      if (this.tileSOX != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.OFFSRCY.ordinal();
-      if (this.tileSOY != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.OFFSRCZ.ordinal();
-      if (this.tileSOZ != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.SIZER.ordinal();
-      if (this.tileSIZER != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.HEIGHT.ordinal();
-      if (this.tileHEIGHT != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.REDSTONE.ordinal();
-      if (this.tileREDSTONE != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-      idx = TileEntityPatternBuilder.Fields.RENDERPARTICLES.ordinal();
-      if (this.tileRENDERPARTICLES != this.tileEntity.getField(idx)) {
-        icontainerlistener.sendProgressBarUpdate(this, idx, this.tileEntity.getField(idx));
-      }
-    }
-    this.tileOX = this.tileEntity.getField(TileEntityPatternBuilder.Fields.OFFTARGX);
-    this.tileOY = this.tileEntity.getField(TileEntityPatternBuilder.Fields.OFFTARGY);
-    this.tileOZ = this.tileEntity.getField(TileEntityPatternBuilder.Fields.OFFTARGZ);
-    this.tileSOX = this.tileEntity.getField(TileEntityPatternBuilder.Fields.OFFSRCX);
-    this.tileSOY = this.tileEntity.getField(TileEntityPatternBuilder.Fields.OFFSRCY);
-    this.tileSOZ = this.tileEntity.getField(TileEntityPatternBuilder.Fields.OFFSRCZ);
-    this.tileSIZER = this.tileEntity.getField(TileEntityPatternBuilder.Fields.SIZER);
-    this.tileHEIGHT = this.tileEntity.getField(TileEntityPatternBuilder.Fields.HEIGHT);
-    this.tileREDSTONE = this.tileEntity.getField(TileEntityPatternBuilder.Fields.REDSTONE);
-    this.tileRENDERPARTICLES = this.tileEntity.getField(TileEntityPatternBuilder.Fields.RENDERPARTICLES);
   }
   @Override
   @SideOnly(Side.CLIENT)
