67a9c3ee0dca3839157164dcc0a5bf426017c364
sam <samson.bassett@gmail.com>
2017-06-16 18:40:02 -0700

    import copyright notice for recipes and hook it all up

--- src/main/java/com/lothrazar/cyclicmagic/registry/RecipeRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/RecipeRegistry.java
@@ -2,17 +2,89 @@ package com.lothrazar.cyclicmagic.registry;
 import java.util.List;
 import javax.annotation.Nonnull;
 import com.google.common.collect.Lists;
+import com.lothrazar.cyclicmagic.data.Const;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+/** 
+ * 
+ * parts of this entire file as of 1.12 (not just this subclass) are from OSS 
+ * https://github.com/TechReborn/RebornCore/blob/1.12/src/main/java/reborncore/common/util/RebornCraftingHelper.java
+ * copy pasted their notice here
+ *  * Copyright (c) 2017 modmuss50 and Gigabit101
+*
+*
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+*
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+*
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+ * @author Sam
+ *
+ */
 public class RecipeRegistry {
+  public static class Util1pt12{
+  private static ResourceLocation  buildName(ItemStack output){
+    
+    ResourceLocation firstTry =new ResourceLocation(  Const.MODID, output.getUnlocalizedName());
+    int index = 0;
+    while (CraftingManager.field_193380_a.containsKey(firstTry)) { // REGISTRY
+      index++;
+      firstTry = new ResourceLocation(Const.MODID, firstTry.getResourcePath() + "_" + index);
+    }
+    
+    
+    return firstTry;
+    
+  }
+  private static NonNullList<Ingredient> convertToNonNullList (Object[] input) {
+    NonNullList<Ingredient> list = NonNullList.create();
+    for(Object any : input){
+      if(any instanceof Ingredient){
+        list.add((Ingredient) any);
+      } else {
+        Ingredient ing = CraftingHelper.getIngredient(any);
+        if(ing == null){
+          ing = Ingredient.field_193370_a;// EMPTY/.. same as new Ingredient(new ItemStack[0])
+        }
+        list.add(ing);
+      }
+    }
+    return list;
+  }
+  }
   /**
    * wrapper for Forge addShapeless recipe, except the difference is this
    * returns it after registering it
+   * 
+   * so
    * 
    * @param stack
    * @param recipeComponents
@@ -32,8 +104,16 @@ public class RecipeRegistry {
         list.add(new ItemStack((Block) object));
       }
     }
-    IRecipe recipe = new ShapelessRecipes(stack, list);
-    GameRegistry.addRecipe(recipe);
+    
+    ResourceLocation location = Util1pt12.buildName(stack);
+    ShapelessRecipes recipe = new ShapelessRecipes(location.getResourceDomain(), stack, Util1pt12.convertToNonNullList(recipeComponents));
+    recipe.setRegistryName(location);
+    GameRegistry.register(recipe);
+    
+    
+    
+//    IRecipe recipe = new ShapelessRecipes(stack, list);
+//    GameRegistry.addRecipe(recipe);
     return recipe;
   }
   /**
@@ -43,8 +123,16 @@ public class RecipeRegistry {
    * @param params
    * @return
    */
-  public static IRecipe addShapedRecipe(@Nonnull ItemStack output, Object... params) {
-    return GameRegistry.addShapedRecipe(output, params);
+//  public static IRecipe addShapedRecipe(@Nonnull ItemStack output, Object... params) {
+//    return GameRegistry.addShapedRecipe(output, params);
+//  }
+  public static IRecipe addShapedRecipe(ItemStack output, Object... params) {
+    ResourceLocation location = Util1pt12.buildName(output);
+    CraftingHelper.ShapedPrimer primer = CraftingHelper.parseShaped(params);
+    ShapedRecipes recipe = new ShapedRecipes(output.getItem().getRegistryName().toString(), primer.width, primer.height, primer.input, output);
+    recipe.setRegistryName(location);
+    GameRegistry.register(recipe);
+    return recipe;
   }
   public static IRecipe addRecipe(@Nonnull ItemStack output, Object... params) {
     return addShapedRecipe(output, params);
