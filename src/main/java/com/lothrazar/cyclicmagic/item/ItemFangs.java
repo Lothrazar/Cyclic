9685dfe112c9c063af9d0180e5c19dc7258407ee
sam <samson.bassett@gmail.com>
2017-06-18 20:11:00 -0700

    fang recipe and half second cooldown

--- src/main/java/com/lothrazar/cyclicmagic/item/ItemFangs.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/ItemFangs.java
@@ -1,10 +1,17 @@
 package com.lothrazar.cyclicmagic.item;
+import com.lothrazar.cyclicmagic.IHasRecipe;
 import com.lothrazar.cyclicmagic.item.base.BaseTool;
+import com.lothrazar.cyclicmagic.registry.RecipeRegistry;
 import com.lothrazar.cyclicmagic.util.UtilItemStack;
+import com.lothrazar.cyclicmagic.util.UtilSound;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityEvokerFangs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -12,24 +19,36 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
-public class ItemFangs extends BaseTool {
-  private static final int DURABILITY = 100;
+public class ItemFangs extends BaseTool implements IHasRecipe {
+  private static final int COOLDOWN = 10;//ticks not seconds
+  private static final int DURABILITY = 666;
   private static final int MAX_RANGE = 16;
   public ItemFangs() {
     super(DURABILITY);
   }
   @Override
   public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer player, EntityLivingBase entity, EnumHand hand) {
+    if (player.getCooldownTracker().hasCooldown(this)) { return false; }
     summonFangRay(player, entity.posX, entity.posY, entity.posZ);
     UtilItemStack.damageItem(player, player.getHeldItem(hand));
     return true;
   }
   @Override
   public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+    if (player.getCooldownTracker().hasCooldown(this)) { return EnumActionResult.PASS; }
     summonFangRay(player, pos.getX() + hitX, pos.getY() + hitY + 1, pos.getZ() + hitZ);
     UtilItemStack.damageItem(player, player.getHeldItem(hand));
     return EnumActionResult.SUCCESS;
   }
+  /**
+   * Summon a ray of fangs in the direction of these coordinates away from the
+   * caster
+   * 
+   * @param caster
+   * @param posX
+   * @param posY
+   * @param posZ
+   */
   private void summonFangRay(EntityPlayer caster, double posX, double posY, double posZ) {
     double minY = Math.min(posY, caster.posY);
     //double d1 = Math.max(posY,caster.posY) ;
@@ -42,10 +61,38 @@ public class ItemFangs extends BaseTool {
           caster.posZ + (double) MathHelper.sin(arctan) * fract,
           arctan, i);
     }
+    onCastSuccess(caster);
   }
+  /**
+   * cast a single fang from the caster towards the direction of the given
+   * coordinates with delay
+   * 
+   * @param caster
+   * @param x
+   * @param y
+   * @param z
+   * @param yaw
+   * @param delay
+   */
   private void summonFangSingle(EntityPlayer caster, double x, double y, double z, float yaw, int delay) {
-    //i was Y plus this + yHeight
     EntityEvokerFangs entityevokerfangs = new EntityEvokerFangs(caster.world, x, y, z, yaw, delay, caster);
     caster.world.spawnEntity(entityevokerfangs);
+  }
+  private void onCastSuccess(EntityPlayer caster) {
+    UtilSound.playSound(caster, SoundEvents.EVOCATION_ILLAGER_PREPARE_ATTACK);
+    caster.getCooldownTracker().setCooldown(this, COOLDOWN);
+  }
+  @Override
+  public IRecipe addRecipe() {
+    return RecipeRegistry.addRecipe(new ItemStack(this),
+        "wpc",
+        " dp",
+        "r w",
+        'w', Blocks.WEB,
+        'r', Blocks.END_ROD,
+//        's', Blocks.END_STONE,
+        'c', Items.END_CRYSTAL,
+        'p', Blocks.PACKED_ICE,
+        'd', Blocks.EMERALD_BLOCK);
   }
 }
