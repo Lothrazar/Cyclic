61b1c72df370ad2f7f270cff047a75cd39bdb203
sam <samson.bassett@gmail.com>
2017-07-22 22:39:59 -0700

    add block rotate animation to hydrator

--- src/main/java/com/lothrazar/cyclicmagic/component/hydrator/BlockHydrator.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/hydrator/BlockHydrator.java
@@ -1,17 +1,22 @@
 package com.lothrazar.cyclicmagic.component.hydrator;
 import java.util.ArrayList;
 import com.lothrazar.cyclicmagic.IHasRecipe;
+import com.lothrazar.cyclicmagic.block.IBlockHasTESR;
 import com.lothrazar.cyclicmagic.block.base.BlockBaseHasTile;
 import com.lothrazar.cyclicmagic.component.hydrator.TileEntityHydrator.Fields;
+import com.lothrazar.cyclicmagic.component.pylonexp.TileEntityXpPylon;
+import com.lothrazar.cyclicmagic.component.pylonexp.XpPylonTESR;
 import com.lothrazar.cyclicmagic.gui.ForgeGuiHandler;
 import com.lothrazar.cyclicmagic.registry.RecipeRegistry;
 import com.lothrazar.cyclicmagic.util.UtilChat;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.tileentity.TileEntity;
@@ -20,9 +25,13 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockHydrator extends BlockBaseHasTile implements IHasRecipe {
+public class BlockHydrator extends BlockBaseHasTile implements IHasRecipe, IBlockHasTESR {
   public static ArrayList<IRecipe> recipeList = new ArrayList<IRecipe>();
   public BlockHydrator() {
     super(Material.IRON);
@@ -71,6 +80,11 @@ public class BlockHydrator extends BlockBaseHasTile implements IHasRecipe {
       return (int) (15 * fill);
     }
     return 0;
+  }
+  @SideOnly(Side.CLIENT)
+  public void initModel() {
+    ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    ClientRegistry.bindTileEntitySpecialRenderer(TileEntityHydrator.class, new HydratorTESR(0, 4));
   }
   /**
    * with thanks and gratitude @KnightMiner
