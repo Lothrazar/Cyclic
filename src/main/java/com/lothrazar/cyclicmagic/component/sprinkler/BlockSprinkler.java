364532248ad507408388d29eca469553196ec59e
sam <samson.bassett@gmail.com>
2017-08-25 21:24:19 -0700

    YEAHHH! working rotations

--- src/main/java/com/lothrazar/cyclicmagic/component/sprinkler/BlockSprinkler.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/sprinkler/BlockSprinkler.java
@@ -1,29 +1,37 @@
 package com.lothrazar.cyclicmagic.component.sprinkler;
-import java.util.Random;
-import com.lothrazar.cyclicmagic.block.base.BlockBase;
+import com.lothrazar.cyclicmagic.block.IBlockHasTESR;
 import com.lothrazar.cyclicmagic.block.base.BlockBaseHasTile;
-import com.lothrazar.cyclicmagic.component.magnetanti.TileEntityMagnetAnti;
-import com.lothrazar.cyclicmagic.util.UtilParticle;
-import net.minecraft.block.Block;
-import net.minecraft.block.IGrowable;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockSprinkler extends BlockBaseHasTile {
- 
+public class BlockSprinkler extends BlockBaseHasTile implements IBlockHasTESR {
   public BlockSprinkler() {
     super(Material.IRON);
     this.setTranslucent();
-//    setTickRandomly(true);
   }
   @Override
   public TileEntity createTileEntity(World worldIn, IBlockState state) {
     return new TileSprinkler();
   }
- 
+  @Override
+  public boolean isSideSolid(IBlockState base_state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+    return false;
+  }
+  @SideOnly(Side.CLIENT)
+  public void initModel() {
+    ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    // Bind our TESR to our tile entity
+    ClientRegistry.bindTileEntitySpecialRenderer(TileSprinkler.class, new SprinklerTESR(this.getUnlocalizedName()));
+  }
 }
---
+++ src/main/java/com/lothrazar/cyclicmagic/component/sprinkler/SprinklerTESR.java
@@ -0,0 +1,111 @@
+package com.lothrazar.cyclicmagic.component.sprinkler;
+
+import org.lwjgl.opengl.GL11;
+import com.google.common.base.Function;
+import com.lothrazar.cyclicmagic.block.base.TileEntityBaseMachineInvo;
+import com.lothrazar.cyclicmagic.data.Const;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.IModel;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.common.model.TRSRTransformation;
+
+public class SprinklerTESR<T extends TileSprinkler>  extends TileEntitySpecialRenderer<T> {
+  
+  private IModel model;
+  private IBakedModel bakedModel;
+  private String resource = null;
+  public SprinklerTESR(String block){
+    resource = "tesr/" + block.replace("tile.", "").replace(".name", "");
+  }
+  protected IBakedModel getBakedModel() {
+    // Since we cannot bake in preInit() we do lazy baking of the model as soon as we need it
+    if (bakedModel == null && resource != null) {
+      try {
+        model = ModelLoaderRegistry.getModel(new ResourceLocation(Const.MODID, resource));
+      }
+      catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+      bakedModel = model.bake(TRSRTransformation.identity(), DefaultVertexFormats.ITEM,
+          new Function<ResourceLocation, TextureAtlasSprite>() {
+            @Override
+            public TextureAtlasSprite apply(ResourceLocation location) {
+              return Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(location.toString());
+            }
+          });
+    }
+    return bakedModel;
+  }
+
+  @Override
+  public void render(TileSprinkler te, double x, double y, double z,
+      float partialTicks, int destroyStage, float alpha
+  //, net.minecraft.client.renderer.BufferBuilder buffer
+  ) {
+    GlStateManager.pushAttrib();
+    GlStateManager.pushMatrix();
+    // Translate to the location of our tile entity
+    GlStateManager.translate(x, y, z);
+    GlStateManager.disableRescaleNormal();
+//    if (te.isRunning() && te.hasFuel()) {
+      this.renderAnimation(te,x,y,z,partialTicks);
+//    GlStateManager.rotate(90, 0, 1, 0);
+//    GlStateManager.translate(-1, 0, 0);//fix position and such
+
+//  }
+    GlStateManager.popMatrix();
+    GlStateManager.popAttrib();
+  }
+  protected void renderAnimation(TileEntityBaseMachineInvo te,double x, double y, double z,
+      float partialTicks) {
+    GlStateManager.pushMatrix();
+//    EnumFacing facing = te.getCurrentFacing();
+//    if (facing == EnumFacing.NORTH || facing == EnumFacing.SOUTH) {
+//      GlStateManager.rotate(90, 0, 1, 0);
+//      GlStateManager.translate(-1, 0, 0);//fix position and such
+//    }
+    //start of rotate
+    GlStateManager.translate(0.5, 0, 0.5);
+    long angle =   (System.currentTimeMillis() / 10) % 360;
+    GlStateManager.rotate(angle, 0, 1, 0);
+    GlStateManager.translate(-.5, 0, -.5);
+    //end of rotate
+    ////do the sliding across animation
+    double currTenthOfSec = System.currentTimeMillis() / 100;//move speed
+//    double ratio = (currTenthOfSec % 8) / 10.00;//this is dong modulo 0.8 since there are 8 locations to move over
+//    GlStateManager.translate(0, 0, -1 * ratio);
+    RenderHelper.disableStandardItemLighting();
+    this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+    if (Minecraft.isAmbientOcclusionEnabled()) {
+      GlStateManager.shadeModel(GL11.GL_SMOOTH);
+    }
+    else {
+      GlStateManager.shadeModel(GL11.GL_FLAT);
+    }
+    World world = te.getWorld();
+    // Translate back to local view coordinates so that we can do the acual rendering here
+    GlStateManager.translate(-te.getPos().getX(), -te.getPos().getY(), -te.getPos().getZ());
+    Tessellator tessellator = Tessellator.getInstance();
+    tessellator.getBuffer().begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
+    Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelRenderer().renderModel(
+        world,
+        getBakedModel(),
+        world.getBlockState(te.getPos()),
+        te.getPos(),
+        Tessellator.getInstance().getBuffer(), false);
+    tessellator.draw();
+    RenderHelper.enableStandardItemLighting();
+    GlStateManager.popMatrix();
+  }
+}
