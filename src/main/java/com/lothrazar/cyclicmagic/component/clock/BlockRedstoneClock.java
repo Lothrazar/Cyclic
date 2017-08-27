44c7d18c7210b9b0e3d2acea79115ed7516aabc0
sam <samson.bassett@gmail.com>
2017-08-26 18:35:45 -0700

    save fields to nbt standard way

--- src/main/java/com/lothrazar/cyclicmagic/component/clock/BlockRedstoneClock.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/clock/BlockRedstoneClock.java
@@ -17,10 +17,10 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockRedstoneClock extends BlockBaseHasTile {
+  private static final int PARTICLE_DENSITY = 2;
   public static final PropertyBool POWERED = net.minecraft.block.BlockLever.POWERED;//PropertyBool.create("powered");
   public BlockRedstoneClock() {
     super(Material.IRON);
-   
   }
   @Override
   protected BlockStateContainer createBlockState() {
@@ -28,22 +28,19 @@ public class BlockRedstoneClock extends BlockBaseHasTile {
   }
   @Override
   public IBlockState getStateFromMeta(int meta) {
-//      if (meta <= 5)
-          return this.getDefaultState().withProperty(POWERED,false);
-//      else
-//          return this.getDefaultState().withProperty(BlockButton.FACING, EnumFacing.VALUES[meta - 6]).withProperty(POWERED, Boolean.TRUE);
+    //      if (meta <= 5)
+    return this.getDefaultState().withProperty(POWERED, false);
+    //      else
+    //          return this.getDefaultState().withProperty(BlockButton.FACING, EnumFacing.VALUES[meta - 6]).withProperty(POWERED, Boolean.TRUE);
   }
   @Override
-  public boolean canProvidePower(IBlockState state)
-  {
-      return true;
+  public boolean canProvidePower(IBlockState state) {
+    return true;
   }
-
   @Override
   public int getMetaFromState(IBlockState state) {
-      return  (state.getValue(POWERED) ? 1 : 0);
+    return (state.getValue(POWERED) ? 1 : 0);
   }
-
   @Override
   public TileEntity createTileEntity(World worldIn, IBlockState state) {
     return new TileEntityClock();
@@ -51,7 +48,7 @@ public class BlockRedstoneClock extends BlockBaseHasTile {
   @SideOnly(Side.CLIENT)
   public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
     if (stateIn.getValue(POWERED)) {
-      UtilParticle.spawnParticle(worldIn, EnumParticleTypes.REDSTONE, pos);
+      UtilParticle.spawnParticle(worldIn, EnumParticleTypes.REDSTONE, pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5, PARTICLE_DENSITY);
     }
   }
   private int getPower(IBlockAccess world, BlockPos pos) {
@@ -61,10 +58,10 @@ public class BlockRedstoneClock extends BlockBaseHasTile {
   }
   @Override
   public int getWeakPower(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-    return blockState.getValue(POWERED) ? getPower(blockAccess,pos) : 0;
+    return blockState.getValue(POWERED) ? getPower(blockAccess, pos) : 0;
   }
   @Override
   public int getStrongPower(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-    return blockState.getValue(POWERED) ? getPower(blockAccess,pos) : 0;
+    return blockState.getValue(POWERED) ? getPower(blockAccess, pos) : 0;
   }
 }
--- src/main/java/com/lothrazar/cyclicmagic/component/clock/TileEntityClock.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/clock/TileEntityClock.java
@@ -1,41 +1,99 @@
- package com.lothrazar.cyclicmagic.component.clock;
-
-import com.lothrazar.cyclicmagic.block.base.TileEntityBaseMachine;
-import net.minecraft.tileentity.TileEntity;
+package com.lothrazar.cyclicmagic.component.clock;
+import com.lothrazar.cyclicmagic.block.base.TileEntityBaseMachineInvo;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ITickable;
 
-public class TileEntityClock extends TileEntityBaseMachine implements ITickable{
-  
-  
+public class TileEntityClock extends TileEntityBaseMachineInvo implements ITickable {
+  public static enum Fields {
+    TIMER, TOFF, TON, POWER;
+  }
+  public TileEntityClock() {
+    super(0);
+    timer = 0;
+  }
   private int timeOff = 60;
   private int timeOn = 60;
-  private int power = 12;
-  private int timer = 0;
-  public int getPower(){
+  private int power = 15;
+  public int getPower() {
     return this.power;
   }
   @Override
-  public void update(){
-    
-    if(world.isRemote == false){
+  public int[] getFieldOrdinals() {
+    return super.getFieldArray(Fields.values().length);
+  }
+  @Override
+  public void update() {
+    if (world.isRemote == false) {
       this.timer++;
       boolean powered;
-      if(timer < timeOff){
+      if (timer < timeOff) {
         powered = false;
       }
-      else if( timer < timeOff + timeOn){
+      else if (timer < timeOff + timeOn) {
         //we are in the ON section
         powered = true;
       }
-      else{
+      else {
         timer = 0;
         powered = false;
       }
-      
-System.out.println(timer+"__"+powered);
+      this.power = 3;
       world.setBlockState(pos, world.getBlockState(pos).withProperty(BlockRedstoneClock.POWERED, powered));
-      
     }
-    
+  }
+  @Override
+  public int getField(int id) {
+    switch (Fields.values()[id]) {
+      case POWER:
+        return power;
+      case TIMER:
+        return timer;
+      case TOFF:
+        return timeOff;
+      case TON:
+        return timeOn;
+      default:
+      break;
+    }
+    return 0;
+  }
+  @Override
+  public void setField(int id, int value) {
+    switch (Fields.values()[id]) {
+      case POWER:
+        if (value < 0) {
+          value = 0;
+        }
+        if (value > 15) {
+          value = 15;
+        }
+        power = value;
+      break;
+      case TIMER:
+        timer = value;
+      break;
+      case TOFF:
+        timeOff = Math.max(value, 0);
+      break;
+      case TON:
+        timeOn =  Math.max(value, 0);
+      break;
+      default:
+      break;
+    }
+  }
+  @Override
+  public void readFromNBT(NBTTagCompound compound) {
+    super.readFromNBT(compound);
+    compound.setInteger("off", timeOff);
+    compound.setInteger("on", timeOn);
+    compound.setInteger("power", power);
+  }
+  @Override
+  public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+    timeOff = compound.getInteger("off");
+    timeOn = compound.getInteger("on");
+    power = compound.getInteger("power");
+    return super.writeToNBT(compound);
   }
 }
