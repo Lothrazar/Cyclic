b6df88aa1c49b8e1bf85129684b40ce96793308a
sam <samson.bassett@gmail.com>
2017-09-10 08:14:12 -0700

    183 waterwalking hotfix

--- src/main/java/com/lothrazar/cyclicmagic/enchantment/EnchantWaterwalking.java
+++ src/main/java/com/lothrazar/cyclicmagic/enchantment/EnchantWaterwalking.java
@@ -2,6 +2,7 @@ package com.lothrazar.cyclicmagic.enchantment;
 import java.util.ArrayList;
 import java.util.Arrays;
 import com.lothrazar.cyclicmagic.registry.GuideRegistry;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
@@ -38,17 +39,27 @@ public class EnchantWaterwalking extends EnchantBase {
   public void onEntityUpdate(LivingUpdateEvent event) {
     if (event.getEntity() instanceof EntityPlayer) {
       EntityPlayer p = (EntityPlayer) event.getEntity();
-      ItemStack feet = p.getItemStackFromSlot(EntityEquipmentSlot.FEET);
-      if (feet.isEmpty()) { return; }
-      setLiquidWalk(p);
+      ItemStack armor = p.getItemStackFromSlot(EntityEquipmentSlot.FEET);
+      int level = 0;
+      if (armor.isEmpty() == false && EnchantmentHelper.getEnchantments(armor) != null
+          && EnchantmentHelper.getEnchantments(armor).containsKey(this)) {
+        //todo: maybe any armor?
+        level = EnchantmentHelper.getEnchantments(armor).get(this);
+      }
+      if (level > 0) {
+        setLiquidWalk(p);
+      }
     }
   }
   private void setLiquidWalk(EntityPlayer player) {
     BlockPos belowPos = player.getPosition().down();
     if (player.world.containsAnyLiquid(new AxisAlignedBB(belowPos)) && player.world.isAirBlock(player.getPosition()) && player.motionY < 0
         && !player.isSneaking()) {// let them slip down into it when sneaking
-      player.motionY = 0;// stop falling
-      player.onGround = true; // act as if on solid ground
+      double diff = player.posY - ((double) player.getPosition().getY());
+      if (diff < 0.1) {
+        player.motionY = 0;// stop falling
+        player.onGround = true; // act as if on solid ground
+      }
     }
   }
 }
