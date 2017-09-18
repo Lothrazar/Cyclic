9c11334777ae960801e099efbf50a00d27f09430
sam <samson.bassett@gmail.com>
2017-09-17 17:58:24 -0700

    hotbar swap feature complete

--- src/main/java/com/lothrazar/cyclicmagic/component/playerext/storage/GuiPlayerExtended.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/playerext/storage/GuiPlayerExtended.java
@@ -1,6 +1,7 @@
 package com.lothrazar.cyclicmagic.component.playerext.storage;
 import com.lothrazar.cyclicmagic.component.playerext.ButtonToggleHotbar;
 import com.lothrazar.cyclicmagic.data.Const;
+import com.lothrazar.cyclicmagic.gui.ITooltipButton;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.achievement.GuiStats;
 import net.minecraft.client.renderer.GlStateManager;
@@ -15,39 +16,45 @@ public class GuiPlayerExtended extends InventoryEffectRenderer {
   }
   @Override
   public void updateScreen() {
-//    try {
-//      ((ContainerPlayerExtended) inventorySlots).inventory.blockEvents = false;
-//    }
-//    catch (Exception e) {}
     this.updateActivePotionEffects();
   }
   @Override
   public void initGui() {
     super.initGui();
-    int id = 0, x=this.guiLeft + this.width,y=this.guiTop;
-    
-    
-    
-    ButtonToggleHotbar btn = new ButtonToggleHotbar(id,x,y);
+    int id = 7, x = this.guiLeft + 168, y = this.guiTop + Const.PAD - 1, w = Const.PAD, h = Const.SQ, row = 1;
+    ButtonToggleHotbar btn = new ButtonToggleHotbar(id, x, y, w, h, row);
     this.buttonList.add(btn);
-    
+    row++;
     id++;
     y += Const.SQ;
-
-     btn = new ButtonToggleHotbar(id,x,y);
+    btn = new ButtonToggleHotbar(id, x, y, w, h, row);
     this.buttonList.add(btn);
-    
-
+    row++;
     id++;
     y += Const.SQ;
-
-     btn = new ButtonToggleHotbar(id,x,y);
+    btn = new ButtonToggleHotbar(id, x, y, w, h, row);
     this.buttonList.add(btn);
-    
+    row++;
+    id++;
+    y += Const.SQ;
+    btn = new ButtonToggleHotbar(id, x, y, w, h, row);
+    this.buttonList.add(btn);
   }
   @Override
   public void drawScreen(int mouseX, int mouseY, float partialTicks) {
     super.drawScreen(mouseX, mouseY, partialTicks);
+    
+    ITooltipButton btn;
+    for (int i = 0; i < buttonList.size(); i++) {
+      if (buttonList.get(i).isMouseOver() && buttonList.get(i) instanceof ITooltipButton) {
+        btn = (ITooltipButton) buttonList.get(i);
+        if (btn.getTooltips() != null) {
+          drawHoveringText(btn.getTooltips(), mouseX, mouseY);
+        }
+        break;// cant hover on 2 at once
+      }
+    }
+    
     this.renderHoveredToolTip(mouseX, mouseY);
   }
   @Override
@@ -57,12 +64,5 @@ public class GuiPlayerExtended extends InventoryEffectRenderer {
     int k = this.guiLeft;
     int l = this.guiTop;
     this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-  }
-  @Override
-  protected void actionPerformed(GuiButton button) {
-
-    if (button.id == 1) {
-      this.mc.displayGuiScreen(new GuiStats(this, this.mc.player.getStatFileWriter()));
-    }
   }
 }
