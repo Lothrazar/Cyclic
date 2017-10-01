a13f4a0cb8a7bf763621f0da4dcaeaf403875da3
sam <samson.bassett@gmail.com>
2017-10-01 08:21:01 -0700

    rm unused potion icons. add beacon model

--- src/main/java/com/lothrazar/cyclicmagic/component/beaconpotion/TileEntityBeaconPotionRenderer.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/beaconpotion/TileEntityBeaconPotionRenderer.java
@@ -3,6 +3,7 @@ import java.util.List;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntityBeaconRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -10,9 +11,16 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class TileEntityBeaconPotionRenderer extends TileEntitySpecialRenderer<TileEntityBeaconPotion> {
   public static final ResourceLocation TEXTURE_BEACON_BEAM = TileEntityBeaconRenderer.TEXTURE_BEACON_BEAM;// new ResourceLocation("textures/entity/beacon_beam.png");
+  @Override
   public void render(TileEntityBeaconPotion te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
     this.renderBeacon(x, y, z, (double) partialTicks, (double) te.shouldBeamRender(), te.getBeamSegments(),
         (double) te.getWorld().getTotalWorldTime());
+    
+//    ItemStack stack = te.getStackInSlot(0);
+//    if (stack.isEmpty()) {
+//      renderItem(te, stack, 0.5f);
+//    }
+//    
   }
   public void renderBeacon(double x, double y, double z, double partialTicks, double textureScale, List<TileEntityBeaconPotion.BeamSegment> beamSegments, double totalWorldTime) {
     GlStateManager.alphaFunc(516, 0.1F);
