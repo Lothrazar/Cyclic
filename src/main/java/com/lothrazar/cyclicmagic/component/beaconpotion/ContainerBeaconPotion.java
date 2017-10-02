109bc14fcd738bb44372a17212d025a71c493486
sam <samson.bassett@gmail.com>
2017-10-02 07:42:45 -0700

    add shift click to beacon gui

--- src/main/java/com/lothrazar/cyclicmagic/component/beaconpotion/ContainerBeaconPotion.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/beaconpotion/ContainerBeaconPotion.java
@@ -2,8 +2,11 @@ package com.lothrazar.cyclicmagic.component.beaconpotion;
 import com.lothrazar.cyclicmagic.data.Const;
 import com.lothrazar.cyclicmagic.gui.base.ContainerBaseMachine;
 import com.lothrazar.cyclicmagic.gui.slot.SlotPotion;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IContainerListener;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -26,5 +29,39 @@ public class ContainerBeaconPotion extends ContainerBaseMachine {
   public void addListener(IContainerListener listener) {
     super.addListener(listener);
     listener.sendAllWindowProperties(this, this.tile);
+  }
+  
+
+  @Override
+  public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
+    ItemStack stack = ItemStack.EMPTY;
+    Slot slotObject = (Slot) inventorySlots.get(slot);
+    // null checks and checks if the item can be stacked (maxStackSize > 1)
+    if (slotObject != null && slotObject.getHasStack()) {
+      ItemStack stackInSlot = slotObject.getStack();
+      stack = stackInSlot.copy();
+      // merges the item into player inventory since its in the tileEntity
+      if (slot < tile.getSizeInventory()) {
+        if (!this.mergeItemStack(stackInSlot, tile.getSizeInventory(), 36 + tile.getSizeInventory(), true)) {
+          return ItemStack.EMPTY;
+        }
+      }
+      // places it into the tileEntity is possible since its in the player
+      // inventory
+      else if (!this.mergeItemStack(stackInSlot, 0, tile.getSizeInventory(), false)) {
+        return ItemStack.EMPTY;
+      }
+      if (stackInSlot.getCount() == 0) {
+        slotObject.putStack(ItemStack.EMPTY);
+      }
+      else {
+        slotObject.onSlotChanged();
+      }
+      if (stackInSlot.getCount() == stack.getCount()) {
+        return ItemStack.EMPTY;
+      }
+      slotObject.onTake(player, stackInSlot);
+    }
+    return stack;
   }
 }
