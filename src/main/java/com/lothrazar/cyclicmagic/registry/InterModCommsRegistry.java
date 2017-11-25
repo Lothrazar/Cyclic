cd8fb059778af79334a636037b77a94415ac2fa6
sam <samson.bassett@gmail.com>
2017-11-24 23:06:50 -0800

    the other crafting grids too

--- src/main/java/com/lothrazar/cyclicmagic/registry/InterModCommsRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/InterModCommsRegistry.java
@@ -1,5 +1,7 @@
 package com.lothrazar.cyclicmagic.registry;
+import com.lothrazar.cyclicmagic.component.crafter.ContainerCrafter;
 import com.lothrazar.cyclicmagic.component.playerext.crafting.ContainerPlayerExtWorkbench;
+import com.lothrazar.cyclicmagic.component.workbench.ContainerWorkBench;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 
@@ -16,10 +18,21 @@ public class InterModCommsRegistry {
    * https://github.com/blay09/CraftingTweaks/blob/1.12/README.md
    */
   private static void registerCraftingTweaks() {
+    //first the players inventory 
     NBTTagCompound tagCompound = new NBTTagCompound();
     tagCompound.setString("ContainerClass", ContainerPlayerExtWorkbench.class.getName());
     tagCompound.setInteger("GridSlotNumber", 6);
-    tagCompound.setInteger("GridSize", 9);
+//    tagCompound.setInteger("GridSize", 9);
+    FMLInterModComms.sendMessage("craftingtweaks", "RegisterProvider", tagCompound);
+    //then the grey workbench 
+    tagCompound = new NBTTagCompound();
+    tagCompound.setString("ContainerClass", ContainerWorkBench.class.getName());
+    tagCompound.setInteger("GridSlotNumber", 1);
+    FMLInterModComms.sendMessage("craftingtweaks", "RegisterProvider", tagCompound);
+    //then the purple autocrafter
+    tagCompound = new NBTTagCompound();
+    tagCompound.setString("ContainerClass", ContainerCrafter.class.getName());
+    tagCompound.setInteger("GridSlotNumber", 10);
     FMLInterModComms.sendMessage("craftingtweaks", "RegisterProvider", tagCompound);
   }
 }
