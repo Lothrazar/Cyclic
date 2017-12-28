e4c27e67eeac18c7d511d3a31db48361a1cfdb79
sam <samson.bassett@gmail.com>
2017-12-28 09:28:10 -0800

    TESR for text: starting to refactor for all sides

--- src/main/java/com/lothrazar/cyclicmagic/component/library/LibraryTESR.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/library/LibraryTESR.java
@@ -27,59 +27,47 @@ public class LibraryTESR<T extends TileEntityLibrary> extends BaseTESR<T> {
   public void render(TileEntityLibrary te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
     //TODO: refactor so i can say
     //writeText(String, EnumFacing, u, v)
-    
- 
-    GlStateManager.pushMatrix();
-    float f = 0.6666667F;
-    GlStateManager.translate((float) x + 0.5F, (float) y + 0.5F, (float) z + 0.5F);
-    //          GlStateManager.rotate(-f2, 0.0F, 1.0F, 0.0F);
-    //          GlStateManager.translate(0.0F, -0.3125F, -0.4375F);
-    //          this.model.signStick.showModel = false;
-    //
-    //      if (destroyStage >= 0)
-    //      {
-    //          this.bindTexture(DESTROY_STAGES[destroyStage]);
-    //          GlStateManager.matrixMode(5890);
-    //          GlStateManager.pushMatrix();
-    //          GlStateManager.scale(4.0F, 2.0F, 1.0F);
-    //          GlStateManager.translate(0.0625F, 0.0625F, 0.0625F);
-    //          GlStateManager.matrixMode(5888);
-    //      }
-    //      else
-    //      {
-    //          this.bindTexture(SIGN_TEXTURE);
-    //      }
+    for (int j = 0; j < te.storage.length; ++j) {
+
+      GlStateManager.pushMatrix();
+      GlStateManager.translate((float) x + 0.5F, (float) y + 0.5F, (float) z + 0.5F);
+      
+      
+      renderOnSouthFace("Hi Twitter");
+      
+      GlStateManager.popMatrix();
+      if (destroyStage >= 0) {
+        GlStateManager.matrixMode(5890);
+        GlStateManager.popMatrix();
+        GlStateManager.matrixMode(5888);
+      }
+    }
+  }
+  //TODO: take in the face or render on all faces?
+  private void renderOnSouthFace(String s) {
+    //initial setup
+    float scaleTo = 0.6666667F;
     GlStateManager.enableRescaleNormal();
     GlStateManager.pushMatrix();
-    GlStateManager.scale(0.6666667F, -0.6666667F, -0.6666667F);
-    //      this.model.renderSign();
+    GlStateManager.scale(scaleTo, -1 * scaleTo, -1 * scaleTo);
     GlStateManager.popMatrix();
     FontRenderer fontrenderer = this.getFontRenderer();
     float f3 = 0.010416667F;
+    
     GlStateManager.translate(-2.0F, 1.33333334F, 0.046666667F);
     //180 so its not upside down
-//    GlStateManager.rotate(180, 1, 0, 0);
+    //    GlStateManager.rotate(180, 1, 0, 0);
     //below sets position
     GlStateManager.translate(1.6F, -1.4125F, 0.500005F);
     //sake makes it the right size do not touch
-          GlStateManager.scale(0.010416667F, -0.010416667F, 0.010416667F);
+    GlStateManager.scale(0.010416667F, -0.010416667F, 0.010416667F);
     GlStateManager.glNormal3f(0.0F, 0.0F, -0.010416667F);//no idea what this does
     GlStateManager.depthMask(false);
-    int i = 0;
-    //    if (destroyStage < 0) {
-    //      for (int j = 0; j < te.storage.length; ++j) {
-    String s = "HI TWITTER!";
-    //                  fontRenderer.drawString(text, 0, (int) (y * fontRenderer.FONT_HEIGHT), 0xFFFF00FF);
+ 
     fontrenderer.drawString(s, 0, 0, 0xFF0000);
-    //      }
-    //    }
+    
     GlStateManager.depthMask(true);
     GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-    GlStateManager.popMatrix();
-    if (destroyStage >= 0) {
-      GlStateManager.matrixMode(5890);
-      GlStateManager.popMatrix();
-      GlStateManager.matrixMode(5888);
-    }
+ 
   }
 }
