96f92c9ebab02cb0377dba75d65151b0b04932d8
sam <samson.bassett@gmail.com>
2017-12-29 14:26:27 -0800

    fix light issue

--- src/main/java/com/lothrazar/cyclicmagic/block/base/BaseTESR.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/base/BaseTESR.java
@@ -77,6 +77,9 @@ public abstract class BaseTESR<T extends TileEntity> extends TileEntitySpecialRe
     // Thank you for helping me understand lighting @storagedrawers  https://github.com/jaquadro/StorageDrawers/blob/40737fb2254d68020a30f80977c84fd50a9b0f26/src/com/jaquadro/minecraft/storagedrawers/client/renderer/TileEntityDrawersRenderer.java#L96
     //start of 'fix lighting' 
     int ambLight = getWorld().getCombinedLight(te.getPos().offset(EnumFacing.UP), 0);
+    if (ambLight == 0) {
+      ambLight = 15728656;//if there is a block above blocking light, dont make it dark
+    }
     int lu = ambLight % 65536;
     int lv = ambLight / 65536;
     OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) lu / 1.0F, (float) lv / 1.0F);
@@ -85,7 +88,8 @@ public abstract class BaseTESR<T extends TileEntity> extends TileEntitySpecialRe
     GlStateManager.popMatrix();
   }
   /**
-   * TextColor similar to  int textColor = 0xFF0055;
+   * TextColor similar to int textColor = 0xFF0055;
+   * 
    * @param s
    * @param x
    * @param y
@@ -97,9 +101,7 @@ public abstract class BaseTESR<T extends TileEntity> extends TileEntitySpecialRe
    * @param angle
    * @param textColor
    */
-  public void renderTextAt(String s, double x, double y, double z, int destroyStage, float xt, float yt, float zt, float angle
-      , int textColor) {
-   
+  public void renderTextAt(String s, double x, double y, double z, int destroyStage, float xt, float yt, float zt, float angle, int textColor) {
     GlStateManager.pushMatrix();
     GlStateManager.translate((float) x + 0.5F, (float) y + 0.5F, (float) z + 0.5F);
     if (angle != 0) {
