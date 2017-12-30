ef4b805ebedccc11b2b7ab9f7ef1344b285a5766
sam <samson.bassett@gmail.com>
2017-12-30 14:38:16 -0800

    3d model for screen

--- src/main/java/com/lothrazar/cyclicmagic/component/screen/BlockScreen.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/screen/BlockScreen.java
@@ -8,25 +8,36 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockScreen extends BlockBaseFacing implements IBlockHasTESR {
   public BlockScreen() {
     super(Material.WOOD);
     this.setGuiId(ForgeGuiHandler.GUI_INDEX_SCREEN);
+    //    this.setTranslucent();
+    this.setLightOpacity(0);
+  }
+  @Override
+  public boolean isOpaqueCube(IBlockState state) {
+    return false;
+  }
+  @Override
+  @SideOnly(Side.CLIENT)
+  public BlockRenderLayer getBlockLayer() {
+    return BlockRenderLayer.SOLID;
   }
   @Override
   public TileEntity createTileEntity(World worldIn, IBlockState state) {
     return new TileEntityScreen();
   }
- 
   @Override
   public void initModel() {
     ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
-    // Bind our TESR to our tile entity
     ClientRegistry.bindTileEntitySpecialRenderer(TileEntityScreen.class, new ScreenTESR<TileEntityScreen>(this));
   }
- 
 }
