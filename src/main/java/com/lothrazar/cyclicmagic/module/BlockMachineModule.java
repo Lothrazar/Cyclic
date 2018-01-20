4694daf0c5b9bf0073bbcaa85ff3e1673c93899d
sam <samson.bassett@gmail.com>
2018-01-20 00:17:08 -0800

    re enable enchanter/fuel for the future

--- src/main/java/com/lothrazar/cyclicmagic/module/BlockMachineModule.java
+++ src/main/java/com/lothrazar/cyclicmagic/module/BlockMachineModule.java
@@ -75,6 +75,7 @@ public class BlockMachineModule extends BaseModule implements IHasConfig {
   private boolean wireless;
   private boolean forester;
   private boolean enchanter;
+  private boolean anvil;
   public void onPreInit() {
     BlockFireSafe fire = new BlockFireSafe();
     BlockRegistry.registerBlock(fire, "fire_dark", null);
@@ -180,20 +181,22 @@ public class BlockMachineModule extends BaseModule implements IHasConfig {
       BlockRegistry.registerBlock(block_user, "block_user", GuideCategory.BLOCKMACHINE);
       GameRegistry.registerTileEntity(TileEntityUser.class, Const.MODID + "block_user_te");
     }
-//    if (enchanter) {
-//      FluidsRegistry.registerExp();
-//      BlockEnchanter block_enchanter = new BlockEnchanter();
-//      BlockRegistry.registerBlock(block_enchanter, "block_enchanter", GuideCategory.BLOCKMACHINE);
-//      GameRegistry.registerTileEntity(TileEntityEnchanter.class, Const.MODID + "block_enchanter_te");
-//    }
-//    BlockAnvilAuto block_anvil = new BlockAnvilAuto();
-//    BlockRegistry.registerBlock(block_anvil, "block_anvil", GuideCategory.BLOCKMACHINE);
-//    GameRegistry.registerTileEntity(TileEntityAnvilAuto.class, Const.MODID + "block_anvil_te");
-
+    if (enchanter) {
+      FluidsRegistry.registerExp();
+      BlockEnchanter block_enchanter = new BlockEnchanter();
+      BlockRegistry.registerBlock(block_enchanter, "block_enchanter", GuideCategory.BLOCKMACHINE);
+      GameRegistry.registerTileEntity(TileEntityEnchanter.class, Const.MODID + "block_enchanter_te");
+    }
+    if (anvil) {
+      BlockAnvilAuto block_anvil = new BlockAnvilAuto();
+      BlockRegistry.registerBlock(block_anvil, "block_anvil", GuideCategory.BLOCKMACHINE);
+      GameRegistry.registerTileEntity(TileEntityAnvilAuto.class, Const.MODID + "block_anvil_te");
+    }
   }
   @Override
   public void syncConfig(Configuration config) {
-    //enchanter = config.getBoolean("block_enchanter", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
+    anvil = config.getBoolean("block_anvil", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
+    enchanter = config.getBoolean("block_enchanter", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
     wireless = config.getBoolean("wireless_transmitter", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
     forester = config.getBoolean("block_forester", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
     beaconPotion = config.getBoolean("PotionBeacon", Const.ConfigCategory.content, true, Const.ConfigCategory.contentDefaultText);
