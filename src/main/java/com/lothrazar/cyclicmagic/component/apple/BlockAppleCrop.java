49c33b1cc8cffa4e23b31e8260f45e12bc82cae1
sam <samson.bassett@gmail.com>
2018-01-20 10:13:18 -0800

    apple sprout feature completed. removed JEI api from gradle dep as its not used

--- src/main/java/com/lothrazar/cyclicmagic/component/apple/BlockAppleCrop.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/apple/BlockAppleCrop.java
@@ -2,7 +2,7 @@ package com.lothrazar.cyclicmagic.component.apple;
 import java.util.Random;
 import javax.annotation.Nullable;
 import com.lothrazar.cyclicmagic.IHasRecipe;
-import com.lothrazar.cyclicmagic.ModCyclic;
+import com.lothrazar.cyclicmagic.block.base.BlockBase;
 import com.lothrazar.cyclicmagic.registry.RecipeRegistry;
 import com.lothrazar.cyclicmagic.util.UtilOreDictionary;
 import net.minecraft.block.Block;
@@ -14,7 +14,6 @@ import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -28,10 +27,13 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockAppleCrop extends Block implements IGrowable, IHasRecipe {
+public class BlockAppleCrop extends BlockBase implements IGrowable, IHasRecipe {
+  private static final int LIGHT_GROWTH = 5;
+  private static final double BONEMEAL_CHANCE = 0.45D;
   private static final int MAX_AGE = 7;
   private static final PropertyInteger AGE = BlockCarrot.AGE;
   private static final AxisAlignedBB[] GROWING_AABB = { new AxisAlignedBB(0.25D, 0.9D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.8D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.7D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.5D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.4D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.3D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.2D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.2D, 0.25D, 0.75D, 1.0D, 0.75D) };
+  private static final double CHANCE_TREASURE = 0.05;
   public BlockAppleCrop() {
     super(Material.PLANTS);
     setHardness(0.5F);
@@ -40,64 +42,79 @@ public class BlockAppleCrop extends Block implements IGrowable, IHasRecipe {
   }
   @Override
   public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
-    //  world.scheduleUpdate(new BlockPos(pos), this, tickRate(world));
+    //wont grow without this i guess?
+    world.scheduleUpdate(new BlockPos(pos), this, tickRate(world));
   }
   @Override
-  public void observedNeighborChange(IBlockState observerState, World world, BlockPos observerPos, Block changedBlock, BlockPos changedBlockPos) {
-    //can only grow/survive if leaves above
-    Block blockAbove = world.getBlockState(observerPos.up()).getBlock();
-    if (UtilOreDictionary.doesMatchOreDict(new ItemStack(blockAbove), "treeLeaves") == false) {
-      world.destroyBlock(observerPos, true);
+  public void observedNeighborChange(IBlockState observerState, World world, BlockPos pos, Block changedBlock, BlockPos changedBlockPos) {
+    if (canStay(world, pos) == false) {
+      world.destroyBlock(pos, true);
+    }
+    else {
+      super.observedNeighborChange(observerState, world, pos, changedBlock, changedBlockPos);
     }
   }
   @Override
-  public boolean canPlaceBlockOnSide(World worldIn, BlockPos pos, EnumFacing side) {
-    //TODO: block me from being placed on wood etc. reuse fn in Observed
-    ModCyclic.logger.error(side.toString());
-    return this.canPlaceBlockAt(worldIn, pos);
-  }
-  @Override
-  public void onBlockDestroyedByPlayer(World world, BlockPos pos, IBlockState state) {
-    //    if (isMaxAge(state)) {
-    //      world.setBlockState(pos, getDefaultState(), 3);
-    //    }
+  public boolean canPlaceBlockOnSide(World world, BlockPos pos, EnumFacing side) {
+    if (canStay(world, pos) == false) {
+      return false;
+    }
+    return this.canPlaceBlockAt(world, pos);
   }
   @Override
   public int tickRate(World world) {
-    return 9800;// CONIFG?
+    return 11800;
   }
   @Override
   public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
-    grow(world, rand, pos, state);
+    if (canStay(world, pos) == false) {
+      world.destroyBlock(pos, true);
+    }
+    else {
+      grow(world, rand, pos, state);
+    }
   }
   @Override
   public boolean canGrow(World world, BlockPos pos, IBlockState state, boolean isClient) {
     //needs at least some light. values [0,15] ish
-    return world.getLight(pos) > 5;
+    return world.getLight(pos) > LIGHT_GROWTH;
   }
   @Override
   public boolean canUseBonemeal(World world, Random rand, BlockPos pos, IBlockState state) {
-    return world.rand.nextFloat() < 0.45D;
+    return world.rand.nextFloat() < BONEMEAL_CHANCE;
   }
   @Override
   public void grow(World world, Random rand, BlockPos pos, IBlockState state) {
-    if (world.isRemote == false) {
+    if (canStay(world, pos) == false) {
+      world.destroyBlock(pos, true);
+    }
+    else if (world.isRemote == false) {
       int age = state.getValue(AGE).intValue();
       if (age < MAX_AGE) {
         world.setBlockState(pos, getStateForAge(age + 1), 3);
-        //        world.scheduleUpdate(new BlockPos(pos), this, tickRate(world));
+        world.scheduleUpdate(new BlockPos(pos), this, tickRate(world));
       }
     }
   }
-  private Item getCrop() {
+  private Item getCropFullGrown(Random rand) {
+    
     // TODO: RANDOMLY a golden apple 
     // or a poison apple (that we need to make item for)
     //maybe emerald also!?!?!
+    if (rand.nextDouble() < CHANCE_TREASURE) {
+      return Items.GOLDEN_APPLE;
+    }
+//    if (rand.nextDouble() < CHANCE_TREASURE && Item.getByNameOrId("cyclicmagic:apple_emerald") != null) {
+//      return Item.getByNameOrId("cyclicmagic:apple_emerald");
+//    }
+//    if (rand.nextDouble() < CHANCE_TREASURE && Item.getByNameOrId("cyclicmagic:apple_lapis") != null) {
+//      return Item.getByNameOrId("cyclicmagic:apple_lapis");
+//    }
     return Items.APPLE;
   }
   @Nullable
   public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-    return isMaxAge(state) ? getCrop() : null;
+    return isMaxAge(state) ? getCropFullGrown(rand) : Item.getItemFromBlock(this);
   }
   private IBlockState getStateForAge(int age) {
     return getDefaultState().withProperty(AGE, age);
@@ -144,6 +161,11 @@ public class BlockAppleCrop extends Block implements IGrowable, IHasRecipe {
   }
   @Override
   public IRecipe addRecipe() {
-    return RecipeRegistry.addShapelessRecipe(new ItemStack(this, 2), new ItemStack(Items.APPLE), new ItemStack(Items.STICK));
+    return RecipeRegistry.addShapelessRecipe(new ItemStack(this, 2), new ItemStack(Items.APPLE), "stickWood");
+  }
+  private boolean canStay(World world, BlockPos pos) {
+    //can only grow/survive if leaves above 
+    Block blockAbove = world.getBlockState(pos.up()).getBlock();
+    return UtilOreDictionary.doesMatchOreDict(new ItemStack(blockAbove), "treeLeaves");
   }
 }
