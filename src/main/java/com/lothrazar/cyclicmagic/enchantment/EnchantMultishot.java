9b5406c4d37110a591207cac51c0c4037efcd5d1
sam <samson.bassett@gmail.com>
2018-01-20 23:09:38 -0800

    multishot moved to horizontal not vert

--- src/main/java/com/lothrazar/cyclicmagic/enchantment/EnchantMultishot.java
+++ src/main/java/com/lothrazar/cyclicmagic/enchantment/EnchantMultishot.java
@@ -1,6 +1,7 @@
 package com.lothrazar.cyclicmagic.enchantment;
 import java.util.ArrayList;
 import com.lothrazar.cyclicmagic.registry.GuideRegistry;
+import com.lothrazar.cyclicmagic.util.UtilEntity;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,6 +12,7 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.ArrowLooseEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -27,12 +29,10 @@ public class EnchantMultishot extends EnchantBase {
   }
   @Override
   public int getMaxLevel() {
-    //number of EXRA arrows, so max level V is six arrows total
-    return 5;
+    return 1;
   }
   @SubscribeEvent
   public void onPlayerUpdate(ArrowLooseEvent event) {
- 
     EntityPlayer player = event.getEntityPlayer();
     ItemStack stack = event.getBow();
     World worldIn = player.world;
@@ -40,23 +40,30 @@ public class EnchantMultishot extends EnchantBase {
     if (level <= 0) {
       return;
     }
- 
     if (!worldIn.isRemote) {
       float charge = ItemBow.getArrowVelocity(stack.getMaxItemUseDuration() - event.getCharge());
-      float offsetPerLevel = 0.3F;
       for (int i = 1; i <= level; i++) {
         //TODO: how to gethorizontal offsets based on player facing?
-        spawnArrow(worldIn, player, stack, charge, 0, i * offsetPerLevel, 0);
+        //use cross product to push arrows out to left and right
+        Vec3d playerDirection = UtilEntity.lookVector(player.rotationYaw, player.rotationPitch);
+        Vec3d left = playerDirection.crossProduct(new Vec3d(0, 1, 0));
+        Vec3d right = playerDirection.crossProduct(new Vec3d(0, -1, 0));
+        spawnArrow(worldIn, player, stack, charge, left.normalize());
+        spawnArrow(worldIn, player, stack, charge, right.normalize());
       }
     }
   }
-  public void spawnArrow(World worldIn, EntityPlayer player, ItemStack stackBow, float charge, float offsetX, float offsetY, float offsetZ) {
+  public void spawnArrow(World worldIn, EntityPlayer player, ItemStack stackBow, float charge, Vec3d offsetVector) {
     //this is from vanilla ItemBow.class
     ItemArrow itemarrow = (ItemArrow) (stackBow.getItem() instanceof ItemArrow ? stackBow.getItem() : Items.ARROW);
     EntityArrow entityarrow = itemarrow.createArrow(worldIn, stackBow, player);
-    entityarrow.posX += offsetX;
-    entityarrow.posY += offsetY;
-    entityarrow.posZ += offsetZ;
+    entityarrow.posX += offsetVector.x;
+    entityarrow.posY += offsetVector.y;
+    entityarrow.posZ += offsetVector.z;
+    //    
+    //    entityarrow.posX += offsetX;
+    //    entityarrow.posY += offsetY;
+    //    entityarrow.posZ += offsetZ;
     entityarrow.setAim(player, player.rotationPitch, player.rotationYaw, 0.0F, charge * 3.0F, 1.0F);
     if (charge == 1.0F) {
       entityarrow.setIsCritical(true);
