a7c4005ea0eb2ef3be9372d0f9e4ed59e98bd735
sam <samson.bassett@gmail.com>
2018-02-02 18:18:45 -0800

    fix changelog for #663 which was fixed inprevious commit

--- src/main/java/com/lothrazar/cyclicmagic/ModCyclic.java
+++ src/main/java/com/lothrazar/cyclicmagic/ModCyclic.java
@@ -33,7 +33,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
