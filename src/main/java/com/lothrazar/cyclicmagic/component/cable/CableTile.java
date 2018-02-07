a0f92cf3d76a202032cf24f0622f35afdf1822e4
sam <samson.bassett@gmail.com>
2018-02-07 00:11:05 -0800

    OOOooYeahhh! kittys tile entity now inherits ours and works

--- src/main/java/com/lothrazar/cyclicmagic/component/cable/CableTile.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/cable/CableTile.java
@@ -1,19 +1,21 @@
 package com.lothrazar.cyclicmagic.component.cable;
-
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+
 /**
  * 
  * @author insomniaKitten
  *
  */
-public final class CableTile extends TileEntity implements ITickable {
-
-    public CableTile() {}
-
-    @Override
-    public void update() {
-
-    }
-
+public final class CableTile extends TileEntityBaseCable {
+  public CableTile() {
+    super(1, 0, 0);
+    this.setItemTransport();
+    this.setSlotsForBoth();
+  }
+  @Override
+  public int[] getSlotsForFace(EnumFacing side) {
+    return new int[] { 0 };
+  }
 }
