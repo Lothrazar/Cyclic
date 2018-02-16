612211a6efd0af9df9baa7a2fa88e471e1fc20b9
sam <samson.bassett@gmail.com>
2018-02-15 23:11:53 -0800

    peat farm action: plants and harvests peat. drains items fuel and power as it goes.

--- src/main/java/com/lothrazar/cyclicmagic/component/peat/BlockPeat.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/peat/BlockPeat.java
@@ -1,21 +1,22 @@
 package com.lothrazar.cyclicmagic.component.peat;
+import java.util.List;
 import java.util.Random;
-import com.lothrazar.cyclicmagic.ModCyclic;
 import com.lothrazar.cyclicmagic.block.base.BlockBase;
 import com.lothrazar.cyclicmagic.data.Const;
 import com.lothrazar.cyclicmagic.util.UtilParticle;
+import com.lothrazar.cyclicmagic.util.UtilShape;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class BlockPeat extends BlockBase {
+  private static final double CHANCE_BAKE_PCT = 0.05;
   private boolean isBaked;
   public BlockPeat(boolean baked) {
     super(Material.GROUND);
@@ -40,23 +41,22 @@ public class BlockPeat extends BlockBase {
   }
   @Override
   public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
-    
     if (this.isBaked) {
       return;
     }
     Block bSide;
-    for (EnumFacing f : EnumFacing.VALUES) {
-      bSide = world.getBlockState(pos.offset(f)).getBlock();
-      if (bSide == Blocks.WATER || bSide == Blocks.FLOWING_WATER) {
+    List<BlockPos> around = UtilShape.squareHorizontalHollow(pos, 1);
+    for (BlockPos p : around) {
+      //try to bake if SOURCE water is nearby 
+      bSide = world.getBlockState(p).getBlock();
+      if (bSide == Blocks.WATER) {
         tryBake(world, pos);
         return;
       }
     }
   }
   private void tryBake(World world, BlockPos pos) {
-    //TODO: is water nearby
-    //ModCyclic.logger.log("tryBake");
-    if (this.isBaked == false && world.rand.nextDouble() < 0.1) {
+    if (this.isBaked == false && world.rand.nextDouble() < CHANCE_BAKE_PCT) {
       world.setBlockToAir(pos);
       world.setBlockState(pos, Block.getBlockFromName(Const.MODRES + "peat_baked").getDefaultState());
       UtilParticle.spawnParticle(world, EnumParticleTypes.WATER_BUBBLE, pos);
--- src/main/java/com/lothrazar/cyclicmagic/component/peat/farm/TileEntityPeatFarm.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/peat/farm/TileEntityPeatFarm.java
@@ -1,13 +1,22 @@
 package com.lothrazar.cyclicmagic.component.peat.farm;
 import java.util.Arrays;
+import java.util.List;
 import javax.annotation.Nullable;
+import com.lothrazar.cyclicmagic.ModCyclic;
 import com.lothrazar.cyclicmagic.block.EnergyStore;
 import com.lothrazar.cyclicmagic.block.base.TileEntityBaseMachineInvo;
+import com.lothrazar.cyclicmagic.data.Const;
 import com.lothrazar.cyclicmagic.fluid.FluidTankBase;
 import com.lothrazar.cyclicmagic.gui.ITileRedstoneToggle;
+import com.lothrazar.cyclicmagic.util.UtilShape;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -19,30 +28,110 @@ import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidTankProperties;
 
 public class TileEntityPeatFarm extends TileEntityBaseMachineInvo implements ITileRedstoneToggle, ITickable, IFluidHandler {
-  public static final int TANK_FULL = 10000;
-  public static final int TIMER_FULL = 200;
-  private static final int PER_TICK = 64;
-  private static final int CAPACITY = PER_TICK * 1000;
+  private static final int DRAIN_COST = 1000;
+  public static final int TANK_FULL = DRAIN_COST * 10;
+  public static final int TIMER_FULL = 5;
+  private static final int PER_TICK = 5;
+  private static final int CAPACITY = 60 * 1000;
   public static enum Fields {
     REDSTONE, TIMER, FLUID;
   }
   private int needsRedstone = 1;
   public FluidTankBase tank = new FluidTankBase(TANK_FULL);
   private EnergyStore energy;
+  private int blockPointer = 0;
   public TileEntityPeatFarm() {
     super(8);//inventory, water
     tank.setTileEntity(this);
     tank.setFluidAllowed(FluidRegistry.WATER);
     energy = new EnergyStore(CAPACITY);
-    timer = 0;
+    timer = TIMER_FULL;
     this.setSlotsForInsert(Arrays.asList(0, 1, 2, 3));
     this.setSlotsForExtract(Arrays.asList(4, 5, 6, 7));
   }
+  Block baked = null;
+  Block unbaked = null;
+  // Item plantMatter = null;
+  List<BlockPos> outer = null;
+  private void init() {
+    if (baked == null)
+      baked = Block.getBlockFromName(Const.MODRES + "peat_baked");
+    if (unbaked == null)
+      unbaked = Block.getBlockFromName(Const.MODRES + "peat_unbaked");
+    //    if (plantMatter == null)
+    //      plantMatter = Item.getByNameOrId(Const.MODRES + "peat_biomass");
+    if (outer == null) {
+      outer = getShape();
+      List<BlockPos> waterShape = UtilShape.squareHorizontalHollow(this.pos, 6);
+      outer.addAll(waterShape);
+    }
+  }
   @Override
   public void update() {
+    this.init();
     if (isRunning() == false) {
       return;
     }
+    energy.extractEnergy(PER_TICK, false);
+    if (energy.getEnergyStored() == 0) {
+      return;
+    }
+    //GET VALIDATE ITEM
+    this.timer--;
+    if (this.timer > 0) {
+      return;
+    }
+    //now loop for build/harvest 
+    if (blockPointer < outer.size()) {
+      BlockPos target = outer.get(blockPointer);
+    //  ModCyclic.logger.error("?"+blockPointer+"  "+target);
+      boolean placeWater = (target.getX() - pos.getX()) % 3 == 0
+          && (target.getZ() - pos.getZ()) % 3 == 0;
+      //based on pattern, try to place either water or the block
+      if (placeWater) {
+        tryPlaceWater(target);
+      }
+      else {
+        tryPlacePeat(target);
+      }
+      //and move along
+      this.timer = TIMER_FULL;
+      blockPointer++;
+    }
+    else {
+      blockPointer = 0;
+      //    ModCyclic.logger.error("RESET");
+    }
+  }
+  private void tryPlacePeat(BlockPos target) {
+    for (int i = 0; i < 4; i++) {
+      if (this.getStackInSlot(i).isItemEqual(new ItemStack(unbaked))) {
+        tryHarvest(target);
+        if (world.getBlockState(target).getBlock().isReplaceable(world, target)) {
+          world.setBlockState(target, unbaked.getDefaultState());
+          this.decrStackSize(i);
+          return;
+        }
+      }
+    }
+    //  ModCyclic.logger.error("not enough blocks");
+  }
+  private void tryPlaceWater(BlockPos target) {
+    if (world.isAirBlock(target) && tank.getFluidAmount() > DRAIN_COST) {
+      tank.drain(DRAIN_COST, false);
+      world.setBlockState(target, Blocks.FLOWING_WATER.getDefaultState());
+    }
+  }
+  private void tryHarvest(BlockPos target) {
+    if (world.getBlockState(target).getBlock() == baked) {
+      //    ModCyclic.logger.error("HARVEST peat " + target);
+      world.destroyBlock(target, true);
+    }
+  }
+  private List<BlockPos> getShape() {
+    List<BlockPos> outer = UtilShape.squareHorizontalHollow(this.pos, 7);
+    outer.addAll(UtilShape.squareHorizontalHollow(this.pos, 5));
+    return outer;
   }
   public int[] getFieldOrdinals() {
     return super.getFieldArray(Fields.values().length);
@@ -55,6 +144,7 @@ public class TileEntityPeatFarm extends TileEntityBaseMachineInvo implements ITi
   public void readFromNBT(NBTTagCompound compound) {
     super.readFromNBT(compound);
     this.needsRedstone = compound.getInteger(NBT_REDST);
+    blockPointer = compound.getInteger("blockPointer");
     tank.readFromNBT(compound.getCompoundTag(NBT_TANK));
     CapabilityEnergy.ENERGY.readNBT(energy, null, compound.getTag("powercable"));
   }
@@ -63,6 +153,7 @@ public class TileEntityPeatFarm extends TileEntityBaseMachineInvo implements ITi
     compound.setInteger(NBT_REDST, this.needsRedstone);
     compound.setTag(NBT_TANK, tank.writeToNBT(new NBTTagCompound()));
     compound.setTag("powercable", CapabilityEnergy.ENERGY.writeNBT(energy, null));
+    compound.setInteger("blockPointer", blockPointer);
     return super.writeToNBT(compound);
   }
   @Override
@@ -120,9 +211,8 @@ public class TileEntityPeatFarm extends TileEntityBaseMachineInvo implements ITi
       fluid = new FluidStack(FluidRegistry.WATER, amt);
     }
     fluid.amount = amt;
-     this.tank.setFluid(fluid);
+    this.tank.setFluid(fluid);
   }
- 
   @Override
   public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing) {
     if (capability == CapabilityEnergy.ENERGY || capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
