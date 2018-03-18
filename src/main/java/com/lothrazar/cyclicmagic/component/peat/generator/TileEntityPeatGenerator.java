c89ca38c71e3327b277d818c9b6503648eed4017
sam <samson.bassett@gmail.com>
2018-03-17 20:26:23 -0700

    1.14.4 #733 auto output

--- src/main/java/com/lothrazar/cyclicmagic/component/peat/generator/TileEntityPeatGenerator.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/peat/generator/TileEntityPeatGenerator.java
@@ -25,14 +25,18 @@ package com.lothrazar.cyclicmagic.component.peat.generator;
 import javax.annotation.Nullable;
 import com.lothrazar.cyclicmagic.block.EnergyStore;
 import com.lothrazar.cyclicmagic.block.base.TileEntityBaseMachineInvo;
+import com.lothrazar.cyclicmagic.component.cable.TileEntityCableBase;
 import com.lothrazar.cyclicmagic.data.Const;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
 
 public class TileEntityPeatGenerator extends TileEntityBaseMachineInvo implements ITickable {
   public final static int TIMER_FULL = 200;
@@ -43,6 +47,8 @@ public class TileEntityPeatGenerator extends TileEntityBaseMachineInvo implement
   public static final int PER_TICK = 128;
   //total energy made per item is PER_TICK * TIMER_FULL
   private static final int CAPACITY = PER_TICK * 100;
+  //output slower than we generate
+  private static final int TRANSFER_ENERGY_PER_TICK = PER_TICK / 2;
   public static enum Fields {
     TIMER;
   }
@@ -55,9 +61,10 @@ public class TileEntityPeatGenerator extends TileEntityBaseMachineInvo implement
   }
   @Override
   public void update() {
-    if (isRunning() == false) {
+    if (isValid() == false) {
       return;
     }
+    this.tryOutputPower();
     // only if burning peat 
     if (timer > 0) {
       int actuallyGained = this.energy.receiveEnergy(PER_TICK, true);
@@ -75,6 +82,36 @@ public class TileEntityPeatGenerator extends TileEntityBaseMachineInvo implement
       if (this.isValidFuel(this.getStackInSlot(SLOT_INPUT))) {
         this.decrStackSize(SLOT_INPUT);
         timer = TIMER_FULL;
+      }
+    }
+  }
+  private void tryOutputPower() {
+    // TODO share code in base class somehow? with CableBase maybe?
+    for (EnumFacing f : EnumFacing.values()) {
+      BlockPos posTarget = pos.offset(f);
+      IEnergyStorage handlerHere = this.getCapability(CapabilityEnergy.ENERGY, f);
+      TileEntity tileTarget = world.getTileEntity(posTarget);
+      if (tileTarget == null) {
+        continue;
+      }
+      IEnergyStorage handlerOutput = tileTarget.getCapability(CapabilityEnergy.ENERGY, f);
+      if (handlerHere != null && handlerOutput != null
+          && handlerHere.canExtract() && handlerOutput.canReceive()) {
+        //first simulate
+        int drain = handlerHere.extractEnergy(TRANSFER_ENERGY_PER_TICK, true);
+        if (drain > 0) {
+          //now push it into output, but find out what was ACTUALLY taken
+          int filled = handlerOutput.receiveEnergy(drain, false);
+          //now actually drain that much from here
+          handlerHere.extractEnergy(filled, false);
+          if (tileTarget instanceof TileEntityCableBase) {
+            //TODO: not so compatible with other fluid systems. itl do i guess
+            TileEntityCableBase cable = (TileEntityCableBase) tileTarget;
+            if (cable.isEnergyPipe())
+              cable.updateIncomingEnergyFace(f.getOpposite());
+          }
+          //              return;// stop now because only pull from one side at a time
+        }
       }
     }
   }
