7e16cbe75d88b16422e6429a19e9d9193f4e3d3b
sam <samson.bassett@gmail.com>
2018-05-20 19:57:38 -0700

    1.15.4 sfx block resources and recipe set

--- src/main/java/com/lothrazar/cyclicmagic/block/sound/BlockSoundPlayer.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/sound/BlockSoundPlayer.java
@@ -7,7 +7,6 @@ import com.lothrazar.cyclicmagic.gui.ForgeGuiHandler;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.tileentity.TileEntity;
@@ -17,9 +16,7 @@ public class BlockSoundPlayer extends BlockBaseHasTile implements IHasRecipe {
 
   public BlockSoundPlayer() {
     super(Material.ROCK);
-
     super.setGuiId(ForgeGuiHandler.GUI_INDEX_SOUNDPL);
-
   }
 
   @Override
@@ -29,21 +26,11 @@ public class BlockSoundPlayer extends BlockBaseHasTile implements IHasRecipe {
 
   @Override
   public IRecipe addRecipe() {
-    return RecipeRegistry.addShapedRecipe(new ItemStack(this), "rsr", "gbg", "ooo",
-        'o', "cobblestone",
-        'g', "nuggetIron",
-        's', Blocks.ANVIL,
-        'r', "blockRedstone",
-        'b', Items.FLINT);
+    return RecipeRegistry.addShapedRecipe(new ItemStack(this),
+        "sss",
+        "sns",
+        "sss",
+        's', Blocks.NOTEBLOCK,
+        'n', Blocks.JUKEBOX);
   }
-
-  //  @Override
-  //  public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
-  //    boolean powered = worldIn.isBlockPowered(pos);
-  //    ModCyclic.logger.log("nbr changed and powerd=" + powered);
-  //    if (powered) {
-  //      TileEntitySoundPlayer te = (TileEntitySoundPlayer) worldIn.getTileEntity(pos);
-  //      te.triggerSoundIfSet();
-  //    }
-  //  }
 }
