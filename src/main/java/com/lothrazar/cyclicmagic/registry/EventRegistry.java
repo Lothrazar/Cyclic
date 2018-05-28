5c28117bf233502c091f626d03bf0cb6f5f4d2b1
sam <samson.bassett@gmail.com>
2018-05-27 22:55:17 -0700

    1.15.7 refactor: dont need duplicate event classes for player features

--- src/main/java/com/lothrazar/cyclicmagic/registry/EventRegistry.java
+++ src/main/java/com/lothrazar/cyclicmagic/registry/EventRegistry.java
@@ -30,7 +30,6 @@ import com.lothrazar.cyclicmagic.event.EventPlayerData;
 import com.lothrazar.cyclicmagic.event.EventRender;
 import com.lothrazar.cyclicmagic.item.mobs.EventHorseFeed;
 import com.lothrazar.cyclicmagic.playerupgrade.EventExtendedInventory;
-import com.lothrazar.cyclicmagic.playerupgrade.EventGuiInvoButtons;
 import com.lothrazar.cyclicmagic.potion.EventPotionTick;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -43,7 +42,6 @@ public class EventRegistry {
     this.register(new EventExtendedInventory());
     this.register(new EventKeyInput());
     this.register(new EventPlayerData());
-    this.register(new EventGuiInvoButtons());
     this.register(new EventPotionTick());
     this.register(new EventRender());
     this.register(new EventHorseFeed());
