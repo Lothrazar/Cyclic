bd588745c383a5f074a3ea913f481e1711955255
sam <samson.bassett@gmail.com>
2018-06-24 15:23:16 -0700

    1.15.14 Darkosto fix wooden pickaxe vs furnaces

--- src/main/java/com/lothrazar/cyclicmagic/playerupgrade/PlayerAbilitiesModule.java
+++ src/main/java/com/lothrazar/cyclicmagic/playerupgrade/PlayerAbilitiesModule.java
@@ -38,6 +38,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraft.nbt.NBTTagCompound;
@@ -196,12 +197,13 @@ public class PlayerAbilitiesModule extends BaseEventModule implements IHasConfig
     ItemStack held = event.getItemStack();
     if (stardewFurnace) {
       // ignore in creative// left clicking just breaks it anyway
-      if (entityPlayer.capabilities.isCreativeMode) {
+      if (entityPlayer.capabilities.isCreativeMode
+          || pos == null
+          || held.getItem() instanceof ItemPickaxe
+      ) {
         return;
       }
-      if (pos == null) {
-        return;
-      }
+
       int playerSlot = 0;// entityPlayer.inventory.currentItem;
       boolean wasMain = event.getHand() == EnumHand.MAIN_HAND;
       if (wasMain) {
