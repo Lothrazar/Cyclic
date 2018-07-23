f47cf8b147895034dae7bc14dc7119523eacb3c1
sam <samson.bassett@gmail.com>
2018-07-23 00:20:24 -0700

    packager: temp resources and default recipes

--- src/main/java/com/lothrazar/cyclicmagic/block/packager/RecipePackage.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/packager/RecipePackage.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.world.World;
@@ -49,6 +50,17 @@ public class RecipePackage extends IForgeRegistryEntry.Impl<IRecipe> implements 
 
   public static void initAllRecipes() {
     addRecipe(new RecipePackage(new ItemStack(Items.IRON_INGOT, 9), new ItemStack(Blocks.IRON_BLOCK)));
+    addRecipe(new RecipePackage(new ItemStack(Items.IRON_NUGGET, 9), new ItemStack(Items.IRON_INGOT)));
+    addRecipe(new RecipePackage(new ItemStack(Items.GOLD_INGOT, 9), new ItemStack(Blocks.GOLD_BLOCK)));
+    addRecipe(new RecipePackage(new ItemStack(Items.GOLD_NUGGET, 9), new ItemStack(Items.GOLD_INGOT)));
+    addRecipe(new RecipePackage(new ItemStack(Items.MELON, 9), new ItemStack(Blocks.MELON_BLOCK)));
+    addRecipe(new RecipePackage(new ItemStack(Items.COAL, 9), new ItemStack(Blocks.COAL_BLOCK)));
+    addRecipe(new RecipePackage(new ItemStack(Items.REDSTONE, 9), new ItemStack(Blocks.REDSTONE_BLOCK)));
+    addRecipe(new RecipePackage(new ItemStack(Items.GLOWSTONE_DUST, 4), new ItemStack(Blocks.GLOWSTONE)));
+    addRecipe(new RecipePackage(new ItemStack(Items.QUARTZ, 4), new ItemStack(Blocks.QUARTZ_BLOCK)));
+    addRecipe(new RecipePackage(new ItemStack(Items.WHEAT, 9), new ItemStack(Blocks.HAY_BLOCK)));
+    addRecipe(new RecipePackage(new ItemStack(Items.NETHER_WART, 9), new ItemStack(Blocks.NETHER_WART_BLOCK)));
+    addRecipe(new RecipePackage(new ItemStack(Items.DYE, 9, EnumDyeColor.BLUE.getDyeDamage()), new ItemStack(Blocks.LAPIS_BLOCK)));
   }
 
   public static void addRecipe(RecipePackage rec) {
