5f0dbbd17ce34e368eee4d19b50366036443a059
sam <samson.bassett@gmail.com>
2018-09-02 21:05:52 -0700

    1.17.1 candle refactor smaller methods

--- src/main/java/com/lothrazar/cyclicmagic/block/watercandle/BlockWaterCandle.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/watercandle/BlockWaterCandle.java
@@ -33,6 +33,8 @@ import com.lothrazar.cyclicmagic.guide.GuideCategory;
 import com.lothrazar.cyclicmagic.registry.BlockRegistry;
 import com.lothrazar.cyclicmagic.registry.RecipeRegistry;
 import com.lothrazar.cyclicmagic.util.Const;
+import com.lothrazar.cyclicmagic.util.UtilParticle;
+import com.lothrazar.cyclicmagic.util.UtilSound;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
@@ -43,11 +45,15 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockAccess;
@@ -61,31 +67,54 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class BlockWaterCandle extends BlockBase implements IHasRecipe, IContent {
 
-  public static final PropertyBool POWERED = PropertyBool.create("lit");
+  public static final PropertyBool IS_LIT = PropertyBool.create("lit");
   private static final int TICK_RATE = 50;
   private static final int RADIUS = 5;
   private static final double CHANCE_OFF = 0.02;
-  EnumCreatureType type = EnumCreatureType.MONSTER;
+  private EnumCreatureType type = EnumCreatureType.MONSTER;
 
+  private static final double BOUNDS = 0.0625 * 3.0;
+  private static final AxisAlignedBB AABB = new AxisAlignedBB(BOUNDS, 0, BOUNDS, 1.0 - BOUNDS, 1.0 - BOUNDS, 1.0 - BOUNDS);
+  private static final double CHANCE_SOUND = 0.3;
   public BlockWaterCandle() {
-    super(Material.GROUND);
-    this.setSoundType(SoundType.GROUND);
-    //    this.setHarvestLevel(Const.ToolStrings.shovel, 2);
+    super(Material.IRON);
+    this.setSoundType(SoundType.METAL);
     this.setTickRandomly(true);
     this.setTranslucent();
   }
 
   @Override
+  public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    return AABB;
+  }
+
+  @Override
+  public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+    return AABB;
+  }
+
+  @Override
+  public boolean isFullCube(IBlockState state) {
+    return false;
+  }
+
+  @Override
   public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-    if (state.getValue(POWERED).booleanValue() == false
+    if (state.getValue(IS_LIT).booleanValue() == false
         && player.getHeldItem(hand).getItem() == Items.FLINT_AND_STEEL) {
-
-      world.setBlockState(pos, state.withProperty(POWERED, true));
-
+      world.setBlockState(pos, state.withProperty(IS_LIT, true));
+      UtilSound.playSound(world, pos, SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.BLOCKS);
       return true;
+    }
+    else if (state.getValue(IS_LIT).booleanValue()
+        && player.getHeldItem(hand).isEmpty()) {
+      //turn it off
+      world.setBlockState(pos, state.withProperty(IS_LIT, false));
+      UtilSound.playSound(world, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS);
     }
     return false;
   }
+
   @Override
   public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
     return false;
@@ -101,22 +130,22 @@ public class BlockWaterCandle extends BlockBase implements IHasRecipe, IContent 
     return 0;
   }
   //
-  //  @Override
-  //  public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
-  //    try {
-  //      //      if (rand.nextDouble() < CHANCE)
-  //        trySpawn(world, pos, rand);
-  //    }
-  //    catch (Exception exception) {
-  //      ModCyclic.logger.error("Error spawning monster ", exception);
-  //    }
-  //  }
+  @Override
+  public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+    triggerUpdate(world, pos, rand);
+  }
 
   @Override
   public void randomTick(World world, BlockPos pos, IBlockState state, Random rand) {
+    triggerUpdate(world, pos, rand);
+  }
+
+  private void triggerUpdate(World world, BlockPos pos, Random rand) {
+    if (rand.nextDouble() < CHANCE_SOUND) {
+      UtilSound.playSound(world, pos, SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.BLOCKS);
+    }
     try {
-      //      if (rand.nextDouble() < CHANCE) 
-        trySpawn(world, pos, rand);
+      trySpawn(world, pos, rand);
     }
     catch (Exception exception) {
       ModCyclic.logger.error("Error spawning monster ", exception);
@@ -124,22 +153,7 @@ public class BlockWaterCandle extends BlockBase implements IHasRecipe, IContent 
   }
 
   private void trySpawn(World world, BlockPos pos, Random rand) throws Exception {
-
-    List<SpawnListEntry> spawnOptions = world.getBiome(pos).getSpawnableList(type);
-    if (spawnOptions == null) {
-      return;
-    }
-    //end is inclusive 
-    int found = MathHelper.getInt(rand, 0, spawnOptions.size() - 1);
-    SpawnListEntry entry = spawnOptions.get(found);
-    if (entry == null || entry.entityClass == null) {
-      return;
-    }
-    EntityEntry entityEntry = EntityRegistry.getEntry(entry.entityClass);
-    EntityLiving monster = null;
-    Entity ent = entityEntry.newInstance(world);
-    if (ent instanceof EntityLiving)
-      monster = (EntityLiving) ent;
+    EntityLiving monster = findMonsterToSpawn(world, pos, rand);
     if (monster == null) {
       return;
     }
@@ -156,13 +170,40 @@ public class BlockWaterCandle extends BlockBase implements IHasRecipe, IContent 
     //we can spawn here
     if (world.spawnEntity(monster)) {
       monster.onInitialSpawn(world.getDifficultyForLocation(pos), null);//i hope null is ok? 
-      if (rand.nextDouble() < CHANCE_OFF) {
-        world.setBlockState(pos, getDefaultState());
-      }
-      else {
-        world.scheduleUpdate(pos, this, TICK_RATE);
-      }
+      afterSpawnSuccess(world, pos, rand);
     }
+  }
+
+  private void afterSpawnSuccess(World world, BlockPos pos, Random rand) {
+    if (rand.nextDouble() < CHANCE_OFF) {
+      UtilSound.playSound(world, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS);
+      UtilParticle.spawnParticle(world, EnumParticleTypes.WATER_SPLASH, pos);
+      UtilParticle.spawnParticle(world, EnumParticleTypes.WATER_SPLASH, pos.up());
+      world.setBlockState(pos, getDefaultState());
+    }
+    else {
+      world.scheduleUpdate(pos, this, TICK_RATE);
+    }
+  }
+
+  private EntityLiving findMonsterToSpawn(World world, BlockPos pos, Random rand) {
+    List<SpawnListEntry> spawnOptions = world.getBiome(pos).getSpawnableList(type);
+    if (spawnOptions == null) {
+      return null;
+    }
+    //end is inclusive 
+    int found = MathHelper.getInt(rand, 0, spawnOptions.size() - 1);
+    SpawnListEntry entry = spawnOptions.get(found);
+    if (entry == null || entry.entityClass == null) {
+      return null;
+    }
+    EntityEntry entityEntry = EntityRegistry.getEntry(entry.entityClass);
+    EntityLiving monster = null;
+    Entity ent = entityEntry.newInstance(world);
+    if (ent instanceof EntityLiving)
+      monster = (EntityLiving) ent;
+
+    return monster;
   }
 
   @Override
@@ -189,26 +230,27 @@ public class BlockWaterCandle extends BlockBase implements IHasRecipe, IContent 
 
   @Override
   protected BlockStateContainer createBlockState() {
-    return new BlockStateContainer(this, POWERED);
+    return new BlockStateContainer(this, IS_LIT);
   }
 
   @Override
   public int getMetaFromState(IBlockState state) {
-    return (state.getValue(POWERED) ? 1 : 0);
+    return (state.getValue(IS_LIT) ? 1 : 0);
   }
 
   @Override
   public IBlockState getStateFromMeta(int meta) {
-    return this.getDefaultState().withProperty(POWERED, meta == 1);
+    return this.getDefaultState().withProperty(IS_LIT, meta == 1);
   }
 
   @Override
   public int getStrongPower(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-    if (blockState.getValue(POWERED).booleanValue()) {
+    if (blockState.getValue(IS_LIT).booleanValue()) {
       return 15;
     }
     return 0;
   }
+
   @Override
   public IRecipe addRecipe() {
     return RecipeRegistry.addShapedRecipe(new ItemStack(this, 4),
