8f94cae6ad3be7b620cb8a985a520f5c37237d1c
sam <samson.bassett@gmail.com>
2018-09-10 23:26:21 -0700

    1.17.2 #845 fixed apple sprout info integration using baseclass

--- src/main/java/com/lothrazar/cyclicmagic/block/applesprout/BlockAppleCrop.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/applesprout/BlockAppleCrop.java
@@ -23,30 +23,23 @@
  ******************************************************************************/
 package com.lothrazar.cyclicmagic.block.applesprout;
 
+import java.util.List;
 import java.util.Random;
-import javax.annotation.Nullable;
 import com.lothrazar.cyclicmagic.IContent;
-import com.lothrazar.cyclicmagic.block.core.BlockBase;
 import com.lothrazar.cyclicmagic.data.IHasRecipe;
 import com.lothrazar.cyclicmagic.guide.GuideCategory;
 import com.lothrazar.cyclicmagic.registry.BlockRegistry;
 import com.lothrazar.cyclicmagic.registry.RecipeRegistry;
 import com.lothrazar.cyclicmagic.util.Const;
+import com.lothrazar.cyclicmagic.util.UtilChat;
 import com.lothrazar.cyclicmagic.util.UtilOreDictionary;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockCarrot;
-import net.minecraft.block.IGrowable;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyInteger;
-import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.BlockCrops;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -56,26 +49,23 @@ import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockAppleCrop extends BlockBase implements IGrowable, IHasRecipe, IContent {
+public class BlockAppleCrop extends BlockCrops implements IHasRecipe, IContent {
 
   private static int GROWTH_TICKRATE = 500;
-  private static int LIGHT_GROWTH = 5;
-  private static final double BONEMEAL_CHANCE = 0.45D;
-  private static final int MAX_AGE = 7;
-  private static final PropertyInteger AGE = BlockCarrot.AGE;
+
+  private static final double BONEMEAL_CHANCE = 0.35D;
   private static final AxisAlignedBB[] GROWING_AABB = { new AxisAlignedBB(0.25D, 0.9D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.8D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.7D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.5D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.4D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.3D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.2D, 0.25D, 0.75D, 1.0D, 0.75D), new AxisAlignedBB(0.25D, 0.2D, 0.25D, 0.75D, 1.0D, 0.75D) };
 
   public BlockAppleCrop() {
-    super(Material.PLANTS);
-    setHardness(0.5F);
+    super();
     setLightOpacity(0);
-    setSoundType(SoundType.PLANT);
   }
 
   @Override
-  public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
-    //wont grow without this i guess?
-    world.scheduleUpdate(new BlockPos(pos), this, tickRate(world));
+  @SideOnly(Side.CLIENT)
+  public void addInformation(ItemStack stack, World playerIn, List<String> tooltip, net.minecraft.client.util.ITooltipFlag advanced) {
+    String myTooltip = this.getTranslationKey() + ".tooltip";
+    tooltip.add(UtilChat.lang(myTooltip));
   }
 
   @Override
@@ -89,16 +79,21 @@ public class BlockAppleCrop extends BlockBase implements IGrowable, IHasRecipe, 
   }
 
   @Override
+  public int tickRate(World world) {
+    return GROWTH_TICKRATE;
+  }
+
+  @Override
+  protected boolean canSustainBush(IBlockState state) {
+    return true;//override the looking for farmland thing
+  }
+
+  @Override
   public boolean canPlaceBlockOnSide(World world, BlockPos pos, EnumFacing side) {
     if (canStay(world, pos) == false) {
       return false;
     }
     return this.canPlaceBlockAt(world, pos);
-  }
-
-  @Override
-  public int tickRate(World world) {
-    return GROWTH_TICKRATE;
   }
 
   @Override
@@ -109,12 +104,6 @@ public class BlockAppleCrop extends BlockBase implements IGrowable, IHasRecipe, 
     else {
       grow(world, rand, pos, state);
     }
-  }
-
-  @Override
-  public boolean canGrow(World world, BlockPos pos, IBlockState state, boolean isClient) {
-    //needs at least some light. values [0,15] ish
-    return world.getLight(pos) >= LIGHT_GROWTH;
   }
 
   @Override
@@ -129,67 +118,32 @@ public class BlockAppleCrop extends BlockBase implements IGrowable, IHasRecipe, 
     }
     else if (world.isRemote == false) {
       int age = state.getValue(AGE).intValue();
-      if (age < MAX_AGE) {
+      if (age < this.getMaxAge()) {
         world.setBlockState(pos, getStateForAge(age + 1), 3);
         world.scheduleUpdate(new BlockPos(pos), this, tickRate(world));
       }
     }
   }
 
-  @Override
-  @Nullable
-  public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-    return isMaxAge(state) ? Items.APPLE : Item.getItemFromBlock(this);
-  }
-
   private IBlockState getStateForAge(int age) {
     return getDefaultState().withProperty(AGE, age);
   }
 
-  public boolean isMaxAge(IBlockState state) {
-    return state.getValue(AGE).intValue() >= MAX_AGE;
+  @Override
+  protected Item getSeed() {
+    return Item.getItemFromBlock(this);
   }
 
   @Override
-  public IBlockState getStateFromMeta(int meta) {
-    return getStateForAge(meta);
+  protected Item getCrop() {
+    return Items.APPLE;
   }
 
-  @Override
-  public int getMetaFromState(IBlockState state) {
-    return state.getValue(AGE).intValue();
-  }
-
-  @Override
-  protected BlockStateContainer createBlockState() {
-    return new BlockStateContainer(this, new IProperty[] { AGE });
-  }
-
-  //transparency stuff
-  @Override
-  public boolean isOpaqueCube(IBlockState state) {
-    return false;
-  }
-
-  @Override
-  @SideOnly(Side.CLIENT)
-  public BlockRenderLayer getRenderLayer() {
-    return BlockRenderLayer.CUTOUT;
-  }
-
-  @Override
-  public boolean isFullCube(IBlockState state) {
-    return false;
-  }
-
-  // no collision
-  @Nullable
   @Override
   public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess world, BlockPos pos) {
     return NULL_AABB;
   }
 
-  //bounding box growth
   @Override
   public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
     return GROWING_AABB[state.getValue(AGE).intValue()];
@@ -197,7 +151,11 @@ public class BlockAppleCrop extends BlockBase implements IGrowable, IHasRecipe, 
 
   @Override
   public IRecipe addRecipe() {
-    return RecipeRegistry.addShapelessRecipe(new ItemStack(this, 2), new ItemStack(Items.APPLE), "stickWood");
+    return RecipeRegistry.addShapedRecipe(new ItemStack(this, 2),
+        "as",
+        "sa",
+        'a', new ItemStack(Items.APPLE),
+        's', "stickWood");
   }
 
   private boolean canStay(World world, BlockPos pos) {
@@ -223,6 +181,5 @@ public class BlockAppleCrop extends BlockBase implements IGrowable, IHasRecipe, 
     String category = Const.ConfigCategory.content;
     enabled = config.getBoolean("apple", category, true, Const.ConfigCategory.contentDefaultText);
     GROWTH_TICKRATE = config.getInt("AppleGrowthTicks", Const.ConfigCategory.blocks, 500, 1, 99999, "Ticks for apple sprout to grow, 1 will grow almost instantly");
-    LIGHT_GROWTH = config.getInt("AppleLightLevel", Const.ConfigCategory.blocks, 5, 0, 15, "Light required for apple to grow");
   }
 }
