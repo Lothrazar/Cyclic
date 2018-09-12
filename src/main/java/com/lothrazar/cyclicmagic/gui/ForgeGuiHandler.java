367abee2e1e43999007fbfccfa300c2225367efe
sam <samson.bassett@gmail.com>
2018-09-11 20:43:19 -0700

    1.17.2 fix server not starting up

--- src/main/java/com/lothrazar/cyclicmagic/gui/ForgeGuiHandler.java
+++ src/main/java/com/lothrazar/cyclicmagic/gui/ForgeGuiHandler.java
@@ -185,6 +185,8 @@ import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ForgeGuiHandler implements IGuiHandler {
 
@@ -486,6 +488,7 @@ public class ForgeGuiHandler implements IGuiHandler {
     return null;
   }
 
+  @SideOnly(Side.CLIENT)
   @Override
   public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
     BlockPos p = new BlockPos(x, y, z);
