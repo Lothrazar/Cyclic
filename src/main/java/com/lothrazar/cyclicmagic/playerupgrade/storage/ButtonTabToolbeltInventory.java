b78bfc92abaece2b11ca6a1d2a038517f5792d00
sam <samson.bassett@gmail.com>
2018-12-15 21:08:33 -0800

    1.17.11 unused btn

---
+++ src/main/java/com/lothrazar/cyclicmagic/playerupgrade/storage/ButtonTabToolbeltInventory.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (C) 2014-2018 Sam Bassett (aka Lothrazar)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+package com.lothrazar.cyclicmagic.playerupgrade.storage;
+
+import com.lothrazar.cyclicmagic.ModCyclic;
+import com.lothrazar.cyclicmagic.gui.ForgeGuiHandler;
+import com.lothrazar.cyclicmagic.gui.core.GuiButtonTooltip;
+import com.lothrazar.cyclicmagic.playerupgrade.PacketOpenGuiOnServer;
+import com.lothrazar.cyclicmagic.proxy.ClientProxy;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class ButtonTabToolbeltInventory extends GuiButtonTooltip {
+
+  private GuiScreen gui;
+
+  public ButtonTabToolbeltInventory(GuiScreen g, int x, int y) {
+    super(51, x, y, 15, 10, "");
+    gui = g;
+    if (ClientProxy.keyWheel != null && ClientProxy.keyWheel.getDisplayName() != null &&
+        ClientProxy.keyWheel.getDisplayName().equals("NONE") == false) {
+      this.displayString = ClientProxy.keyWheel.getDisplayName();
+    }
+    else {
+      this.displayString = "R";//the legacy one. in case someone is just running with the key unbound
+    }
+  }
+
+  @Override
+  public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
+    boolean pressed = super.mousePressed(mc, mouseX, mouseY);
+    if (pressed) {
+      //      if (this.gui instanceof GuiInventory || this.gui instanceof GuiPlayerExtWorkbench) {
+        ModCyclic.network.sendToServer(new PacketOpenGuiOnServer(ForgeGuiHandler.GUI_INDEX_TOOLSWAPPER));
+      //      }
+      //      else {
+      //        this.gui.mc.displayGuiScreen(new GuiInventory(gui.mc.player));
+      //        ModCyclic.network.sendToServer(new PacketOpenNormalInventory(this.gui.mc.player));
+      //      }
+    }
+    return pressed;
+  }
+}
