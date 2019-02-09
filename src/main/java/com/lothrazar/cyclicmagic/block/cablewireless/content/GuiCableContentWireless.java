7beb3bf330237ea444a903bb3df885331397ca75
sam <samson.bassett@gmail.com>
2019-02-08 16:11:39 -0800

    1.18.0 item wireless gui

--- src/main/java/com/lothrazar/cyclicmagic/block/cablewireless/content/GuiCableContentWireless.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/cablewireless/content/GuiCableContentWireless.java
@@ -99,13 +99,14 @@ public class GuiCableContentWireless extends GuiBaseContainer {
   protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
     super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
     int u = 0, v = 0, x, y;
-    this.mc.getTextureManager().bindTexture(Const.Res.SLOT);
-    // item transfer slot 
-    x = this.guiLeft + 42;
-    y = this.guiTop + 42;
+    this.mc.getTextureManager().bindTexture(Const.Res.SLOT_LARGE);
+    // item transfer slot   
+    x = this.guiLeft + 142;
+    y = this.guiTop + 38;
+    int s = Const.SQ + 4;
     Gui.drawModalRectWithCustomSizedTexture(
         x, y,
-        u, v, Const.SQ, Const.SQ, Const.SQ, Const.SQ);
+        u, v, s, s, s, s);
     //now draw target location card slots 
     this.mc.getTextureManager().bindTexture(Const.Res.SLOT_GPS);
     x = this.guiLeft + 8;
