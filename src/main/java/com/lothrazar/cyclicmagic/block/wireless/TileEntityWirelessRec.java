994600383287e5ec2a39511b916b411576a1bc04
sam <samson.bassett@gmail.com>
2019-03-31 08:56:53 -0700

    1.18.6 #1090 wireless tr blockstate

--- src/main/java/com/lothrazar/cyclicmagic/block/wireless/TileEntityWirelessRec.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/wireless/TileEntityWirelessRec.java
@@ -38,31 +38,13 @@ public class TileEntityWirelessRec extends TileEntityBaseMachineInvo implements 
 
   @Override
   public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
-    //oldState.getBlock() instanceof BlockRedstoneClock &&
     return !(newSate.getBlock() instanceof BlockRedstoneWireless);// : oldState != newSate;
   }
 
   @Override
   public void update() {
-    //    targetPos = new BlockPos(-269, 64, 343);
-    //    IBlockState target = world.getBlockState(targetPos);
-    //    System.out.println(target.getBlock());
-    //    if (target.getBlock() instanceof BlockRedstoneWireless) {
-    //      boolean targetPowered = target.getValue(BlockRedstoneWireless.POWERED);
-    //      //update target based on my state
-    //      boolean isPowered = world.getBlockState(pos).getValue(BlockRedstoneWireless.POWERED);
-    //      if (targetPowered != isPowered) {
-    //        world.setBlockState(targetPos, target.withProperty(BlockRedstoneWireless.POWERED, isPowered));
-    //      }
-    //    } 
   }
 
-  //  @Override
-  //  public int getField(int id) {
-  //    return 0;
-  //  }
-  //  @Override
-  //  public void setField(int id, int value) {}
   @Override
   public NBTTagCompound writeToNBT(NBTTagCompound compound) {
     return super.writeToNBT(compound);
