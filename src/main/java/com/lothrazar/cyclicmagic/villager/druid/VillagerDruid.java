15cd8e170b9cee1c4e73ae851b653b45291da4a3
sam <samson.bassett@gmail.com>
2019-04-08 16:30:42 -0700

    1.19.0 melter singletons. villager only sell now

--- src/main/java/com/lothrazar/cyclicmagic/villager/druid/VillagerDruid.java
+++ src/main/java/com/lothrazar/cyclicmagic/villager/druid/VillagerDruid.java
@@ -1,11 +1,14 @@
 package com.lothrazar.cyclicmagic.villager.druid;
 
+import com.lothrazar.cyclicmagic.util.Const;
+import com.lothrazar.cyclicmagic.villager.ListItemForEmeraldsFixed;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.passive.EntityVillager.EmeraldForItems;
 import net.minecraft.entity.passive.EntityVillager.PriceInfo;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class VillagerDruid {
 
@@ -30,12 +33,17 @@ public class VillagerDruid {
             new EmeraldForItems(Items.RABBIT, new PriceInfo(14, 17)),
             new EmeraldForItems(Items.CHICKEN, new PriceInfo(14, 17))
         }, {
-            new EmeraldForItems(Items.WRITTEN_BOOK, new PriceInfo(1, 1)), //GROUP 5
             new EmeraldForItems(Items.FISH, new PriceInfo(9, 12)),
             new EmeraldForItems(Items.SPIDER_EYE, new PriceInfo(3, 6))
         }, {
-            new EmeraldForItems(Items.SADDLE, new PriceInfo(1, 1)), //GROUP 6 
             new EmeraldForItems(Items.EXPERIENCE_BOTTLE, new PriceInfo(1, 1))
+        }, { //GROUP 7
+            // hardcoded in ItemSkull i found
+            //  private static final String[] SKULL_TYPES = new String[] {"skeleton", "wither", "zombie", "char", "creeper", "dragon"};
+            new ListItemForEmeraldsFixed(new ItemStack(Items.SKULL, 1, Const.skull_skeleton), new PriceInfo(28, 32)),
+            new ListItemForEmeraldsFixed(new ItemStack(Items.SKULL, 1, Const.skull_creeper), new PriceInfo(28, 32)),
+            new ListItemForEmeraldsFixed(new ItemStack(Items.SKULL, 1, Const.skull_zombie), new PriceInfo(28, 32)),
+            new ListItemForEmeraldsFixed(new ItemStack(Items.SKULL, 1, Const.skull_player), new PriceInfo(28, 32))
         }
     };
   }
--- src/main/java/com/lothrazar/cyclicmagic/villager/sage/VillagerSage.java
+++ src/main/java/com/lothrazar/cyclicmagic/villager/sage/VillagerSage.java
@@ -1,12 +1,9 @@
 package com.lothrazar.cyclicmagic.villager.sage;
 
-import com.lothrazar.cyclicmagic.util.Const;
-import com.lothrazar.cyclicmagic.villager.ListItemForEmeraldsFixed;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.passive.EntityVillager.EmeraldForItems;
 import net.minecraft.entity.passive.EntityVillager.PriceInfo;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 
 public class VillagerSage {
 
@@ -29,15 +26,12 @@ public class VillagerSage {
         }, {
             new EmeraldForItems(Items.GLOWSTONE_DUST, new PriceInfo(6, 8)), //GROUP 5
             new EmeraldForItems(Items.ENDER_PEARL, new PriceInfo(16, 16))
+        }, { new EmeraldForItems(Items.WRITTEN_BOOK, new PriceInfo(1, 1)), //GROUP 6
+            new EmeraldForItems(Items.SADDLE, new PriceInfo(1, 1)),
+            new EmeraldForItems(Items.NAME_TAG, new PriceInfo(1, 1))
         }, {
-            new EmeraldForItems(Items.NAME_TAG, new PriceInfo(1, 1)) //GROUP 6 
-        }, { //GROUP 7
-            // hardcoded in ItemSkull i found
-            //  private static final String[] SKULL_TYPES = new String[] {"skeleton", "wither", "zombie", "char", "creeper", "dragon"};
-            new ListItemForEmeraldsFixed(new ItemStack(Items.SKULL, 1, Const.skull_skeleton), new PriceInfo(28, 32)),
-            new ListItemForEmeraldsFixed(new ItemStack(Items.SKULL, 1, Const.skull_creeper), new PriceInfo(28, 32)),
-            new ListItemForEmeraldsFixed(new ItemStack(Items.SKULL, 1, Const.skull_zombie), new PriceInfo(28, 32)),
-            new ListItemForEmeraldsFixed(new ItemStack(Items.SKULL, 1, Const.skull_player), new PriceInfo(28, 32))
+            new EmeraldForItems(Items.CAKE, new PriceInfo(1, 1)),
+            new EmeraldForItems(Items.PUMPKIN_PIE, new PriceInfo(1, 1))
         }
     };
   }
