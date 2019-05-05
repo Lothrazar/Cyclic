672a94f96e45ff4eda6cb55c0a1acfe1614ba7ca
sam <samson.bassett@gmail.com>
2019-05-04 23:29:58 -0700

    1.19.9 exchange silk feature

--- src/main/java/com/lothrazar/cyclicmagic/item/buildswap/ItemBuildSwapper.java
+++ src/main/java/com/lothrazar/cyclicmagic/item/buildswap/ItemBuildSwapper.java
@@ -42,6 +42,7 @@ import com.lothrazar.cyclicmagic.util.UtilSound;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.enchantment.EnchantmentUntouching;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -135,6 +136,18 @@ public class ItemBuildSwapper extends BaseTool implements IRenderOutline, IHasRe
       tags.setInteger(NBT, type);
       wand.setTagCompound(tags);
     }
+  }
+
+  @Override
+  public boolean canApplyAtEnchantingTable(ItemStack stack, net.minecraft.enchantment.Enchantment enchantment) {
+    if (enchantment instanceof EnchantmentUntouching) {
+      return true;
+    }
+    return super.canApplyAtEnchantingTable(stack, enchantment);
+  }
+  @Override
+  public boolean isEnchantable(ItemStack stack) {
+    return true;//     return this.getItemStackLimit(stack) == 1 && this.isDamageable();
   }
 
   @SubscribeEvent
