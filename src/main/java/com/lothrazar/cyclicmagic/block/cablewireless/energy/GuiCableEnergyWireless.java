22a7bfbc6c04484ba5b0377bb66fd9edd23aee0a
sam <samson.bassett@gmail.com>
2019-05-05 18:10:26 -0700

    1.19.9 wireless: misc gui improvements

--- src/main/java/com/lothrazar/cyclicmagic/block/cablewireless/energy/GuiCableEnergyWireless.java
+++ src/main/java/com/lothrazar/cyclicmagic/block/cablewireless/energy/GuiCableEnergyWireless.java
@@ -72,9 +72,9 @@ public class GuiCableEnergyWireless extends GuiBaseContainer {
       this.addButton(btnSize);
     }
     int x = this.guiLeft + 6;
-    y = this.guiTop + 38;
+    y = this.guiTop + 64;
     slider = new GuiSliderInteger(tile, 77,
-        x, y, 140, 20,
+        x, y, 140, 14,
         1, TileCableEnergyWireless.MAX_TRANSFER * 16,
         TileCableEnergyWireless.Fields.TRANSFER_RATE.ordinal());
     slider.setTooltip("pump.rate");
