6a191883b81600db00f15c3e96f763846eb0004c
sam <samson.bassett@gmail.com>
2019-08-17 16:31:20 -0700

    0.2.0 and update to latest forge

--- langCopy.bat
+++
@@ -1,9 +0,0 @@
-cp src/main/resources/assets/cyclicmagic/lang/en_AU.lang src/main/resources/assets/cyclicmagic/lang/en_CA.lang
-cp src/main/resources/assets/cyclicmagic/lang/en_AU.lang src/main/resources/assets/cyclicmagic/lang/en_GB.lang
-cp src/main/resources/assets/cyclicmagic/lang/en_AU.lang src/main/resources/assets/cyclicmagic/lang/en_US.lang
-cp src/main/resources/assets/cyclicmagic/lang/en_AU.lang src/main/resources/assets/cyclicmagic/lang/en_NZ.lang
-
-cp run/config/cyclicmagic.cfg config_example_1.12/cyclicmagic.cfg
-cp run/config/cyclic_ores.cfg config_example_1.12/cyclic_ores.cfg
-
-cp -r scripts_zen_example/* run/scripts
--- refresh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-bash gradlew setupDecompWorkspace eclipse --refresh-dependencies
