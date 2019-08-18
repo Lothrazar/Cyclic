70dd13fe1be3c3fc824319d55741944dacaa8953
sam <samson.bassett@gmail.com>
2019-08-18 09:52:43 -0700

    0.0.2 collector model

--- src/main/java/com/lothrazar/cyclic/UtilStuff.java
+++ src/main/java/com/lothrazar/cyclic/UtilStuff.java
@@ -1,6 +1,9 @@
 package com.lothrazar.cyclic;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
 
 public class UtilStuff {
@@ -16,5 +19,18 @@ public class UtilStuff {
 
   public static void messageStatus(PlayerEntity player, String message) {
     player.sendStatusMessage(new TranslationTextComponent(message), true);
+  }
+
+  public static Direction getFacingFromEntity(BlockPos clickedBlock, LivingEntity entity) {
+    return Direction.getFacingFromVector((float) (entity.posX - clickedBlock.getX()), (float) (entity.posY - clickedBlock.getY()), (float) (entity.posZ - clickedBlock.getZ()));
+  }
+
+  public static Direction getFacingFromEntityHorizontal(BlockPos clickedBlock, LivingEntity entity) {
+    Direction d = getFacingFromEntity(clickedBlock, entity);
+    //if only horizontal is allowed
+    if (d == Direction.UP || d == Direction.DOWN) {
+      return entity.getHorizontalFacing().getOpposite();
+    }
+    return d;
   }
 }
--- src/main/java/com/lothrazar/cyclic/block/itemcollect/BlockCollector.java
+++ src/main/java/com/lothrazar/cyclic/block/itemcollect/BlockCollector.java
@@ -2,23 +2,41 @@ package com.lothrazar.cyclic.block.itemcollect;
 
 import java.util.List;
 import javax.annotation.Nullable;
+import com.lothrazar.cyclic.UtilStuff;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class BlockCollector extends Block {
 
   public BlockCollector(Properties properties) {
-    super(properties.hardnessAndResistance(1.8F).sound(SoundType.GLASS));
+    super(properties.hardnessAndResistance(1.8F).sound(SoundType.STONE));
+  }
+
+  @Override
+  public void onBlockPlacedBy(World world, BlockPos pos, BlockState state, @Nullable LivingEntity entity, ItemStack stack) {
+    if (entity != null) {
+      world.setBlockState(pos, state.with(ChestBlock.FACING, UtilStuff.getFacingFromEntityHorizontal(pos, entity)), 2);
+    }
+  }
+
+  @Override
+  protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+    builder.add(ChestBlock.FACING);
   }
 
   @Override
