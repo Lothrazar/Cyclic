ddec48bce0d4eb3a3cc10f55bf709e48d920fc76
sam <samson.bassett@gmail.com>
2019-08-17 21:13:59 -0700

    0.2.0 xppylon tweaks and tooltips

--- src/main/java/com/lothrazar/cyclic/item/ItemExp.java
+++ src/main/java/com/lothrazar/cyclic/item/ItemExp.java
@@ -1,18 +1,31 @@
 package com.lothrazar.cyclic.item;
 
+import java.util.List;
+import javax.annotation.Nullable;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class ItemExp extends Item {
 
-  public static final int EXP_PER_FOOD = 100;
+  public static final int EXP_PER_FOOD = 50;
 
   public ItemExp(Properties properties) {
     super(properties.food(new Food.Builder().setAlwaysEdible().fastToEat().build()));
+  }
+
+  @Override
+  @OnlyIn(Dist.CLIENT)
+  public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+    tooltip.add(new TranslationTextComponent(getTranslationKey() + ".tooltip"));
   }
 
   @Override
