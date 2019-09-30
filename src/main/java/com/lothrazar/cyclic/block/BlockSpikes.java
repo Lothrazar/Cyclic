0dd34adfc883a7c1b06388a472252008e8d6cf9b
sam <samson.bassett@gmail.com>
2019-09-29 20:02:06 -0700

    spike enums

--- src/main/java/com/lothrazar/cyclic/block/BlockSpikes.java
+++ src/main/java/com/lothrazar/cyclic/block/BlockSpikes.java
@@ -1,24 +1,25 @@
 package com.lothrazar.cyclic.block;
 
-import javax.annotation.Nullable;
-import com.lothrazar.cyclic.ModCyclic;
+import java.util.Locale;
 import com.lothrazar.cyclic.base.BlockBase;
-import com.lothrazar.cyclic.util.UtilStuff;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Direction;
-import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.Hand;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
@@ -27,32 +28,49 @@ import net.minecraft.world.World;
 
 public class BlockSpikes extends BlockBase {
 
+  public static enum EnumSpikeType implements IStringSerializable {
+    PLAIN, FIRE, CURSE;
+
+    @Override
+    public String getName() {
+      return this.name().toLowerCase(Locale.ENGLISH);
+    }
+  }
+
+  public static final EnumProperty<EnumSpikeType> TYPE = EnumProperty.create("type", EnumSpikeType.class);
   public static final BooleanProperty ACTIVATED = BooleanProperty.create("lit");
   private static final float LARGE = 0.9375F;
   private static final float SMALL = 0.0625F;
   private static final VoxelShape NORTH_BOX = Block.makeCuboidShape(0.0F, 0.0F, LARGE, 15.0F, 15.0F, 1.0F);
-  private static final VoxelShape EAST_BOX = Block.makeCuboidShape(0.0F, 0.0F, 0.0F, SMALL*15, 15.0F, 15.0F);
-  private static final VoxelShape SOUTH_BOX = Block.makeCuboidShape(0.0F, 0.0F, 0.0F,    15.0F, 15.0F, SMALL*15);
-  private static final VoxelShape WEST_BOX = Block.makeCuboidShape(LARGE, 0.0F, 0.0F,    15.0F, 15.0F, 15.0F);
-  private static final VoxelShape UP_BOX = Block.makeCuboidShape(0.0F, 0.0F, 0.0F, 15.0F, SMALL*15, 15.0F);
+  private static final VoxelShape EAST_BOX = Block.makeCuboidShape(0.0F, 0.0F, 0.0F, SMALL * 15, 15.0F, 15.0F);
+  private static final VoxelShape SOUTH_BOX = Block.makeCuboidShape(0.0F, 0.0F, 0.0F, 15.0F, 15.0F, SMALL * 15);
+  private static final VoxelShape WEST_BOX = Block.makeCuboidShape(LARGE, 0.0F, 0.0F, 15.0F, 15.0F, 15.0F);
+  private static final VoxelShape UP_BOX = Block.makeCuboidShape(0.0F, 0.0F, 0.0F, 15.0F, SMALL * 15, 15.0F);
   private static final VoxelShape DOWN_BOX = Block.makeCuboidShape(0.0F, LARGE, 0.0F, 15.0F, 15.0F, 15.0F);
- 
   protected static final VoxelShape UNPRESSED_AABB = Block.makeCuboidShape(
       1.0D, 0.0D, 1.0D,
-      15.0D, 1.0D, 15.0D); 
+      15.0D, 1.0D, 15.0D);
+
   public BlockSpikes(Properties properties) {
-    super(properties.hardnessAndResistance(1.1F));
-//   Blocks.ACACIA_PRESSURE_PLATE 
-    //    Blocks.LADDER?
+    super(properties.hardnessAndResistance(1.1F)); 
   }
-//  public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-//    return state.get(ACTIVATED).booleanValue() ? PRESSED_AABB : UNPRESSED_AABB;
-// }
-//
+
+  @Override
+  public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+    if (player.getHeldItem(handIn).getItem() == Items.BLAZE_POWDER) {
+      worldIn.setBlockState(pos, worldIn.getBlockState(pos).with(TYPE, EnumSpikeType.FIRE));
+    }
+    if (player.getHeldItem(handIn).getItem() == Items.SPIDER_EYE) {
+      worldIn.setBlockState(pos, worldIn.getBlockState(pos).with(TYPE, EnumSpikeType.CURSE));
+    }
+    if (player.getHeldItem(handIn).getItem() == Items.BREAD) {
+      worldIn.setBlockState(pos, worldIn.getBlockState(pos).with(TYPE, EnumSpikeType.PLAIN));
+    }
+    return false;
+  }
+
   @Override
   public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-    
-  
     switch ((Direction) state.get(BlockStateProperties.FACING)) {
       case NORTH:
         return NORTH_BOX;
@@ -111,13 +129,12 @@ public class BlockSpikes extends BlockBase {
     BlockPos pos = context.getPos();
     Direction facing = context.getFace();
     return worldIn.getBlockState(pos.offset(facing.getOpposite())).isSolid() //worldIn.isSideSolid(pos.offset(facing.getOpposite()), facing, true) 
-        ? this.getDefaultState().with(BlockStateProperties.FACING, facing).with(ACTIVATED, false)
-        : this.getDefaultState().with(BlockStateProperties.FACING, Direction.DOWN).with(ACTIVATED, false);
+        ? this.getDefaultState().with(BlockStateProperties.FACING, facing).with(ACTIVATED, false).with(TYPE, EnumSpikeType.PLAIN)
+        : this.getDefaultState().with(BlockStateProperties.FACING, Direction.DOWN).with(ACTIVATED, false).with(TYPE, EnumSpikeType.PLAIN);
   }
- 
 
   @Override
   protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-    builder.add(BlockStateProperties.FACING).add(ACTIVATED);
+    builder.add(BlockStateProperties.FACING).add(ACTIVATED).add(TYPE);
   }
 }
