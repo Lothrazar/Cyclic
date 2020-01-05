ed5f2fc395eb6ec4e1747ce32febe16d98a3eef5
lothrazar <samson.bassett@gmail.com>
2020-01-04 22:17:44 -0800

    curios update version

--- src/main/java/com/lothrazar/cyclic/ModCyclic.java
+++ src/main/java/com/lothrazar/cyclic/ModCyclic.java
@@ -39,8 +39,11 @@ public class ModCyclic {
   }
 
   private void setup(final FMLCommonSetupEvent event) {
+    //https://github.com/TheIllusiveC4/Curios/blob/fc77c876b630dc6e4a325cb9ac627b551749a19b/src/main/java/top/theillusivec4/curios/api/CurioTags.java
     InterModComms.sendTo("curios", "register_type", () -> new CurioIMCMessage("charm").setSize(2).setEnabled(true).setHidden(false));
     InterModComms.sendTo("curios", "register_type", () -> new CurioIMCMessage("necklace").setSize(1).setEnabled(true).setHidden(false));
+    InterModComms.sendTo("curios", "register_type", () -> new CurioIMCMessage("hands").setSize(2).setEnabled(true).setHidden(false));
+    InterModComms.sendTo("curios", "register_type", () -> new CurioIMCMessage("belt").setSize(2).setEnabled(true).setHidden(false));
     InterModComms.sendTo("curios", "register_type", () -> new CurioIMCMessage("ring").setSize(2).setEnabled(true).setHidden(false));
     //now all blocks/items exist
     PacketRegistry.init();
