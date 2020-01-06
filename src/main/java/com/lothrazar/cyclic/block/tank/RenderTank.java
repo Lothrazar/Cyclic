33bd9b0648ba6cbd77026934268550421496ec77
lothrazar <samson.bassett@gmail.com>
2020-01-05 20:35:02 -0800

    fluid tank save nbt and renderer

--- src/main/java/com/lothrazar/cyclic/block/tank/RenderTank.java
+++ src/main/java/com/lothrazar/cyclic/block/tank/RenderTank.java
@@ -26,10 +26,11 @@ public class RenderTank extends TileEntityRenderer<TileTank> {
     if (fluidStack.isEmpty()) {
       return;
     }
+    //    ModCyclic.LOGGER.info("has " + fluidStack);
     GlStateManager.pushMatrix();
     GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
     GL11.glEnable(GL11.GL_CULL_FACE);
-    GL11.glDisable(GL11.GL_LIGHTING);
+    GL11.glEnable(GL11.GL_LIGHTING);
     //   
     //START
     Tessellator tessy = Tessellator.getInstance();
