ca87f3446c5204f27b590c59516f1dd45da5d9dc
lothrazar <samson.bassett@gmail.com>
2020-03-18 09:26:51 -0700

    #1342 speed up torch back to regular

--- src/main/java/com/lothrazar/cyclic/item/torchthrow/ItemTorchThrower.java
+++ src/main/java/com/lothrazar/cyclic/item/torchthrow/ItemTorchThrower.java
@@ -12,6 +12,10 @@ import net.minecraft.world.World;
 
 public class ItemTorchThrower extends ItemBase {
 
+  private static final float INACCURACY_DEFAULT = 1.0F;
+  private static final float PITCHOFFSET = 0.0F;
+  private static final float VELOCITY_MAX = 1.5F;
+
   public ItemTorchThrower(Properties properties) {
     super(properties);
   }
@@ -28,7 +32,9 @@ public class ItemTorchThrower extends ItemBase {
   public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, @Nonnull Hand hand) {
     if (!world.isRemote) {
       EntityTorchBolt ball = new EntityTorchBolt(player, world);
-      ball.shoot(player, player.rotationPitch, player.rotationYaw, 0, 0.5F, 1);
+      //      ball.shoot(player, player.rotationPitch, player.rotationYaw, 0, 0.5F, 1); 
+      float velocityFactor = 1.5F;
+      ball.shoot(player, player.rotationPitch, player.rotationYaw, PITCHOFFSET, velocityFactor * VELOCITY_MAX, INACCURACY_DEFAULT);
       player.getHeldItem(hand).damageItem(1, player, (p) -> {
         p.sendBreakAnimation(hand);
       });
