17591096b0608df1e44fdc67b2c2823968a22065
lothrazar <samson.bassett@gmail.com>
2020-03-31 00:03:09 -0700

    multiple sounds working

--- src/main/java/com/lothrazar/cyclic/registry/SoundRegistry.java
+++ src/main/java/com/lothrazar/cyclic/registry/SoundRegistry.java
@@ -16,8 +16,8 @@ public class SoundRegistry {
 
   private static List<SoundEvent> sounds = new ArrayList<>();
   public static SoundEvent block_scaffolding_0 = make("block_scaffolding_0");
-  //  public static SoundEvent block_scaffolding_1 = make("block_scaffolding_1");
-  public static SoundType SCAFFOLD = new SoundType(1.0F, 1.0F, block_scaffolding_0, block_scaffolding_0, block_scaffolding_0, block_scaffolding_0, block_scaffolding_0);
+  public static SoundEvent block_scaffolding_1 = make("block_scaffolding_1");
+  public static SoundType SCAFFOLD = new SoundType(1.0F, 1.0F, block_scaffolding_0, block_scaffolding_1, block_scaffolding_0, block_scaffolding_1, block_scaffolding_1);
 
   private static SoundEvent make(String s) {
     SoundEvent se = new SoundEvent(new ResourceLocation(ModCyclic.MODID, s));
