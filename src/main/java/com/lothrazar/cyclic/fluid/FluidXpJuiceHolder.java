27290435baa4ad01cb47af136a38080af49e4934
lothrazar <samson.bassett@gmail.com>
2020-04-20 09:40:43 -0700

    fluid registry refactor again

--- src/main/java/com/lothrazar/cyclic/fluid/FluidXpJuiceHolder.java
+++ src/main/java/com/lothrazar/cyclic/fluid/FluidXpJuiceHolder.java
@@ -4,6 +4,7 @@ import com.lothrazar.cyclic.ModCyclic;
 import com.lothrazar.cyclic.registry.FluidRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.block.material.Material;
 import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
@@ -19,20 +20,20 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 //Thanks to example https://github.com/MinecraftForge/MinecraftForge/blob/1.15.x/src/test/java/net/minecraftforge/debug/fluid/NewFluidTest.java
 public class FluidXpJuiceHolder {
 
-  /**
-   * xpjuice
-   */
-  public static final String xpjuiceId = "xpjuice";
-  public static final ResourceLocation XP_STILL = new ResourceLocation(ModCyclic.MODID + ":fluid/" + xpjuiceId + "_base");
-  public static final ResourceLocation XP_FLOWING = new ResourceLocation(ModCyclic.MODID + ":fluid/" + xpjuiceId + "_flowing");
-  public static RegistryObject<FlowingFluid> xpjuice = FluidRegistry.FLUIDS.register(xpjuiceId, () -> new ForgeFlowingFluid.Source(FluidXpJuiceHolder.xpjuice_properties));
-  public static RegistryObject<FlowingFluid> xpjuice_flowing = FluidRegistry.FLUIDS.register(xpjuiceId + "_flowing", () -> new ForgeFlowingFluid.Flowing(FluidXpJuiceHolder.xpjuice_properties));
-  public static RegistryObject<FlowingFluidBlock> xpjuice_block = FluidRegistry.BLOCKS.register(xpjuiceId + "_block",
-      () -> new FlowingFluidBlock(xpjuice, Block.Properties.create(net.minecraft.block.material.Material.WATER).doesNotBlockMovement().hardnessAndResistance(100.0F).noDrops()));
-  public static RegistryObject<Item> xpjuice_bucket = FluidRegistry.ITEMS.register(xpjuiceId + "_bucket",
-      () -> new BucketItem(xpjuice, new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(ItemGroup.MISC)));
-  public static final ForgeFlowingFluid.Properties xpjuice_properties = new ForgeFlowingFluid.Properties(xpjuice, xpjuice_flowing,
-      FluidAttributes.builder(XP_STILL, XP_FLOWING)).bucket(xpjuice_bucket).block(xpjuice_block);
+  private static final String id = "xpjuice";
+  public static RegistryObject<FlowingFluid> STILL = FluidRegistry.FLUIDS.register(id, () -> new ForgeFlowingFluid.Source(FluidXpJuiceHolder.properties));
+  public static RegistryObject<FlowingFluid> FLOWING = FluidRegistry.FLUIDS.register(id + "_flowing", () -> new ForgeFlowingFluid.Flowing(FluidXpJuiceHolder.properties));
+  public static RegistryObject<FlowingFluidBlock> BLOCK = FluidRegistry.BLOCKS.register(id + "_block",
+      () -> new FlowingFluidBlock(STILL, Block.Properties.create(Material.WATER).doesNotBlockMovement().hardnessAndResistance(100.0F).noDrops()));
+  public static RegistryObject<Item> BUCKET = FluidRegistry.ITEMS.register(id + "_bucket",
+      () -> new BucketItem(STILL, new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(ItemGroup.MISC)));
+  private static final ForgeFlowingFluid.Properties properties = new ForgeFlowingFluid.Properties(
+      STILL,
+      FLOWING,
+      FluidAttributes.builder(
+          new ResourceLocation(ModCyclic.MODID + ":fluid/" + id + "_base"),
+          new ResourceLocation(ModCyclic.MODID + ":fluid/" + id + "_flowing")))
+              .bucket(BUCKET).block(BLOCK);
 
   public FluidXpJuiceHolder() {
     IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
