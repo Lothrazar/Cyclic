e396eaa78e8deec68857a292a5acfced738aa3ea
lothrazar <samson.bassett@gmail.com>
2020-04-21 15:35:23 -0700

    melter JEI getting started

---
+++ src/main/java/com/lothrazar/cyclic/jei/CyclicJEI.java
@@ -0,0 +1,46 @@
+package com.lothrazar.cyclic.jei;
+
+import com.lothrazar.cyclic.ModCyclic;
+import com.lothrazar.cyclic.block.melter.ScreenMelter;
+import com.lothrazar.cyclic.recipe.RecipeMelter;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
+import net.minecraft.util.ResourceLocation;
+
+@JeiPlugin
+public class CyclicJEI implements IModPlugin {
+
+  private ResourceLocation id = new ResourceLocation(ModCyclic.MODID, "jei");
+
+  @Override
+  public ResourceLocation getPluginUid() {
+    return id;
+  }
+
+  @Override
+  public void registerCategories(IRecipeCategoryRegistration registry) {
+    IGuiHelper guiHelper = registry.getJeiHelpers().getGuiHelper();
+    registry.addRecipeCategories(new MelterRecipeCategory(guiHelper));
+  }
+
+  @Override
+  public void registerRecipes(IRecipeRegistration registry) {
+    registry.addRecipes(RecipeMelter.RECIPES, MelterRecipeCategory.id);
+  }
+
+  @Override
+  public void registerGuiHandlers(IGuiHandlerRegistration registry) {
+    registry.addRecipeClickArea(ScreenMelter.class, 75, 0, 40, 26, MelterRecipeCategory.id);
+  }
+
+  @Override
+  public void registerRecipeTransferHandlers(IRecipeTransferRegistration registry) {
+    //    registry.addRecipeTransferHandler(ContainerMelter.class, MelterRecipeCategory.id,
+    //        75, 0, 40, 26);
+  }
+}
---
+++ src/main/java/com/lothrazar/cyclic/jei/MelterRecipeCategory.java
@@ -0,0 +1,92 @@
+package com.lothrazar.cyclic.jei;
+
+import java.util.List;
+import com.lothrazar.cyclic.ModCyclic;
+import com.lothrazar.cyclic.data.Const;
+import com.lothrazar.cyclic.recipe.RecipeMelter;
+import com.lothrazar.cyclic.registry.BlockRegistry;
+import com.lothrazar.cyclic.util.UtilChat;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidAttributes;
+
+public class MelterRecipeCategory implements IRecipeCategory<RecipeMelter> {
+
+  static ResourceLocation id = new ResourceLocation(ModCyclic.MODID, "jei");
+  private IDrawable gui;
+  private IDrawable icon;
+
+  public MelterRecipeCategory(IGuiHelper helper) {
+    gui = helper.drawableBuilder(new ResourceLocation(ModCyclic.MODID, "textures/gui/melter_recipe.png"), 0, 0, 169, 69).setTextureSize(169, 69).build();
+    icon = helper.drawableBuilder(new ResourceLocation(ModCyclic.MODID, "textures/blocks/melter.png"), 0, 0, 16, 16).setTextureSize(16, 16).build();
+  }
+
+  @Override
+  public String getTitle() {
+    return UtilChat.lang(BlockRegistry.melter.getTranslationKey());
+  }
+
+  @Override
+  public IDrawable getIcon() {
+    return icon;
+  }
+
+  @Override
+  public IDrawable getBackground() {
+    return gui;
+  }
+
+  @Override
+  public Class<? extends RecipeMelter> getRecipeClass() {
+    return RecipeMelter.class;
+  }
+
+  @Override
+  public ResourceLocation getUid() {
+    return id;
+  }
+
+  @Override
+  public void setIngredients(RecipeMelter recipe, IIngredients ingredients) {
+    //    recipeLayout.getFluidStacks().init(0, true, x, y, Const.SQ - 2, Const.SQ - 2, Fluid.BUCKET_VOLUME, false,
+    //        null);
+    //    recipeLayout.getFluidStacks().set(0, recipe.getOutputFluid());
+  }
+
+  @Override
+  public void setRecipe(IRecipeLayout recipeLayout, RecipeMelter recipe, IIngredients ingredients) {
+    IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
+    int x = 3, y = 18;
+    int s = Const.SQ;
+    guiItemStacks.init(0, true, x, y);
+    ingredients.setOutput(VanillaTypes.FLUID, recipe.getRecipeFluidOutput());
+    //    //getname is the same   
+    List<List<ItemStack>> inputs = ingredients.getInputs(VanillaTypes.ITEM);
+    for (int i = 0; i < inputs.size(); i++) {
+      List<ItemStack> input = inputs.get(i);
+      if (input != null && input.isEmpty() == false) {
+        //ok so
+        guiItemStacks.set(i, input.get(0));
+      }
+    }
+    //
+    try {
+      ingredients.setOutput(VanillaTypes.FLUID, recipe.getRecipeFluidOutput());
+      //getname is the same   
+      recipeLayout.getFluidStacks().init(0, true, x, y, Const.SQ - 2, Const.SQ - 2,
+          FluidAttributes.BUCKET_VOLUME, false,
+          null);
+      recipeLayout.getFluidStacks().set(0, recipe.getRecipeFluidOutput());
+    }
+    catch (Exception e) {
+      //
+    }
+  }
+}
