22c8ddce000c39fd729bdcf5a69f67c77f5b8847
lothrazar <samson.bassett@gmail.com>
2020-04-23 20:15:37 -0700

    fluids biomass

---
+++ src/main/java/com/lothrazar/cyclic/fluid/block/BiomassFluidBlock.java
@@ -0,0 +1,34 @@
+package com.lothrazar.cyclic.fluid.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.fluid.FlowingFluid;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
+
+public class BiomassFluidBlock extends FlowingFluidBlock {
+
+  public BiomassFluidBlock(java.util.function.Supplier<? extends FlowingFluid> supplier, Block.Properties props) {
+    super(supplier, props);
+  }
+
+  @Override
+  public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+    if (entityIn instanceof LivingEntity && worldIn.rand.nextDouble() < 000.1F) {
+      LivingEntity ent = (LivingEntity) entityIn;
+      ent.addPotionEffect(new EffectInstance(Effects.POISON, 40, 0));
+    }
+    super.onEntityCollision(state, worldIn, pos, entityIn);
+  }
+
+  @Override
+  public int tickRate(IWorldReader worldIn) {
+    return super.tickRate(worldIn) + 1;
+  }
+}
