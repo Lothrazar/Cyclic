36c1bae952188e11b0d6efd68a1dfc4003de8bee
lothrazar <samson.bassett@gmail.com>
2020-04-24 15:04:52 -0700

    recipes added

--- src/main/java/com/lothrazar/cyclic/block/solidifier/ScreenSolidifier.java
+++ src/main/java/com/lothrazar/cyclic/block/solidifier/ScreenSolidifier.java
@@ -37,7 +37,7 @@ public class ScreenSolidifier extends ScreenBase<ContainerSolidifier> {
   @Override
   protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
     this.drawButtonTooltips(mouseX, mouseY);
-    this.drawName(this.title.getFormattedText());
+    //    this.drawName(this.title.getFormattedText());
   }
 
   @Override
