ae4a34492414045dedba8b87835bc461f53ffe4a
lothrazar <samson.bassett@gmail.com>
2020-07-02 23:50:36 -0700

    #1440 shears data tag compat

--- src/main/java/com/lothrazar/cyclic/item/ShearsMaterial.java
+++ src/main/java/com/lothrazar/cyclic/item/ShearsMaterial.java
@@ -3,37 +3,14 @@ package com.lothrazar.cyclic.item;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShearsItem;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class ShearsMaterial extends ShearsItem {
 
   public ShearsMaterial(Properties builder) {
     super(builder);
-  }
-
-  @Override
-  public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
-    Block block = state.getBlock();
-    if (state.isIn(BlockTags.LEAVES) || block != Blocks.COBWEB && block == Blocks.VINE) {
-      ItemStack drop = new ItemStack(block);
-      worldIn.addEntity(new ItemEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), drop));
-    }
-    return super.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
-  }
-
-  @Override
-  public boolean canHarvestBlock(BlockState state) {
-    Block block = state.getBlock();
-    //    BlockTags.?
-    //    VineBlock y;
-    return block == Blocks.VINE || state.isIn(BlockTags.LEAVES) || // block == Blocks.ACACIA_LEAVES || //TOOD: leaf tag?
-        block == Blocks.MELON || block == Blocks.PUMPKIN || super.canHarvestBlock(state);
   }
 
   @Override
