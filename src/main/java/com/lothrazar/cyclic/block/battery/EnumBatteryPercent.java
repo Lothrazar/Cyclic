0ae65a558830846b9a880cddc77459d500f25bcc
trunksbomb <trunskbomb@msn.com>
2020-10-10 21:34:51 -0400

    Update Battery to visually represent its charge percentage in the world via a new meter on its side textures.

---
+++ src/main/java/com/lothrazar/cyclic/block/battery/EnumBatteryPercent.java
@@ -0,0 +1,24 @@
+package com.lothrazar.cyclic.block.battery;
+
+import net.minecraft.util.IStringSerializable;
+
+public enum EnumBatteryPercent implements IStringSerializable {
+  ZERO,TWENTY,FOURTY,SIXTY,EIGHTY,NINETY,ONEHUNDRED;
+
+  @Override
+  public String getString() {
+    if (this.name().equals(ONEHUNDRED.name()))
+      return String.valueOf(100);
+    else if (this.name().equals(NINETY.name()))
+      return String.valueOf(90);
+    else if (this.name().equals(EIGHTY.name()))
+      return String.valueOf(80);
+    else if (this.name().equals(SIXTY.name()))
+      return String.valueOf(60);
+    else if (this.name().equals(FOURTY.name()))
+      return String.valueOf(40);
+    else if (this.name().equals(TWENTY.name()))
+      return String.valueOf(20);
+    return String.valueOf(0);
+  }
+}
