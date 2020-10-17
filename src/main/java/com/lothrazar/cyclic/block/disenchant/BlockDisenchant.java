998f29c85c05be17fec053d967c884f770dc6093
lothrazar <samson.bassett@gmail.com>
2020-10-17 15:22:02 -0700

    0.8.3 disenchanter rendering

--- src/main/java/com/lothrazar/cyclic/block/disenchant/BlockDisenchant.java
+++ src/main/java/com/lothrazar/cyclic/block/disenchant/BlockDisenchant.java
@@ -1,21 +1,68 @@
 package com.lothrazar.cyclic.block.disenchant;
 
+import java.util.Random;
 import com.lothrazar.cyclic.base.BlockBase;
 import com.lothrazar.cyclic.registry.ContainerScreenRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class BlockDisenchant extends BlockBase {
 
+  protected static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D);
+
   public BlockDisenchant(Properties properties) {
     super(properties.hardnessAndResistance(1.8F).notSolid());
     this.setHasGui();
+  }
+
+  @Override
+  public boolean isTransparent(BlockState state) {
+    return true;
+  }
+
+  @Override
+  public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+    return SHAPE;
+  }
+
+  /**
+   * Called periodically clientside on blocks near the player to show effects (like furnace fire particles). Note that this method is unrelated to {@link randomTick} and {@link #needsRandomTick}, and
+   * will always be called regardless of whether the block can receive random update ticks
+   */
+  @Override
+  @OnlyIn(Dist.CLIENT)
+  public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+    super.animateTick(stateIn, worldIn, pos, rand);
+    for (int i = -2; i <= 2; ++i) {
+      for (int j = -2; j <= 2; ++j) {
+        if (i > -2 && i < 2 && j == -1) {
+          j = 2;
+        }
+        if (rand.nextInt(16) == 0) {
+          for (int k = 0; k <= 1; ++k) {
+            BlockPos blockpos = pos.add(i, k, j);
+            if (worldIn.getBlockState(blockpos).getEnchantPowerBonus(worldIn, blockpos) > 0) {
+              if (!worldIn.isAirBlock(pos.add(i / 2, 0, j / 2))) {
+                break;
+              }
+              worldIn.addParticle(ParticleTypes.ENCHANT, pos.getX() + 0.5D, pos.getY() + 2.0D, pos.getZ() + 0.5D, i + rand.nextFloat() - 0.5D, k - rand.nextFloat() - 1.0F, j + rand.nextFloat() - 0.5D);
+            }
+          }
+        }
+      }
+    }
   }
 
   @Override
