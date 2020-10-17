d35bc75c6c42b71e1bb0cec88182b382dcaf7493
trunksbomb <trunskbomb@msn.com>
2020-10-16 21:38:05 -0400

    Implement new Fireplace block. 2 behaviors with 2 variants: When it receives power, tries to a light a fire on the block in front of it if able to do so When it loses power, tries to extinguish fire in front of it
    Variant behavior: If there is an infinitely-burnable block in front of it (such as netherrack), it will attempt to light the top of that block on fire or extinguish an existing fire on top of that block.
    
    Also added infiniburn_overworld tag so the fireplace can light itself on fire when facing up, and stay lit.

--- src/main/java/com/lothrazar/cyclic/block/BlockFireplace.java
+++ src/main/java/com/lothrazar/cyclic/block/BlockFireplace.java
@@ -11,9 +11,11 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Dimension;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -24,31 +26,74 @@ public class BlockFireplace extends BlockBase {
     this.setDefaultState(this.getDefaultState().with(LIT, false));
   }
 
+  @SuppressWarnings("deprecation")
   @Override
   public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
     if (worldIn.isRemote)
       return;
     boolean isPowered = worldIn.isBlockPowered(pos);
     BlockPos posFire = pos.offset(state.get(BlockStateProperties.FACING));
-    BlockPos posBase = posFire.offset(Direction.DOWN);
     if (isPowered && !state.get(LIT)) { //set fire
-      if (worldIn.isAirBlock(posFire) && worldIn.getBlockState(posBase).isSolid()) {
-        worldIn.setBlockState(posFire, Blocks.FIRE.getDefaultState());
+      if (setFire(worldIn, posFire, false)) {
         UtilSound.playSound(ModCyclic.proxy.getClientPlayer(), pos, SoundEvents.ITEM_FLINTANDSTEEL_USE);
       }
-      else
+      else {
         UtilSound.playSound(ModCyclic.proxy.getClientPlayer(), pos, SoundEvents.BLOCK_LEVER_CLICK);
+      }
     }
     else if (!isPowered && state.get(LIT)) { //put out fire
-      if (worldIn.getBlockState(posFire).getBlock() == Blocks.FIRE) {
-        worldIn.setBlockState(posFire, Blocks.AIR.getDefaultState());
+      if (setFire(worldIn, posFire, true)) {
         UtilSound.playSound(ModCyclic.proxy.getClientPlayer(), pos, SoundEvents.BLOCK_FIRE_EXTINGUISH);
       }
-      else
+      else {
         UtilSound.playSound(ModCyclic.proxy.getClientPlayer(), pos, SoundEvents.BLOCK_LEVER_CLICK);
+      }
     }
     worldIn.setBlockState(pos, state.with(LIT, isPowered));
     super.neighborChanged(state, worldIn, pos, blockIn, fromPos, isMoving);
+  }
+
+  private boolean setFire(World worldIn, BlockPos pos, boolean extinguish) {
+    if (extinguish && canExtinguish(worldIn, pos)) {
+      worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
+      return true;
+    }
+    else if (extinguish && canExtinguishInfiniteFire(worldIn, pos)) {
+      worldIn.setBlockState(pos.offset(Direction.UP), Blocks.AIR.getDefaultState());
+      return true;
+    }
+    else if ((!extinguish && canSetFire(worldIn, pos))) {
+      worldIn.setBlockState(pos, Blocks.FIRE.getDefaultState());
+      return true;
+    }
+    else if (!extinguish && canSetInfiniteFire(worldIn, pos)) {
+      worldIn.setBlockState(pos.offset(Direction.UP), Blocks.FIRE.getDefaultState());
+      return true;
+    }
+    return false;
+  }
+
+  private boolean canSetFire(World worldIn, BlockPos pos) {
+    return worldIn.isAirBlock(pos) && worldIn.getBlockState(pos.offset(Direction.DOWN)).isSolid();
+  }
+
+  private boolean canSetInfiniteFire(World worldIn, BlockPos pos) {
+    return worldIn.isAirBlock(pos.offset(Direction.UP))
+            && hasInfiniburnTag(worldIn, pos);
+  }
+
+  private boolean canExtinguish(World worldIn, BlockPos pos) {
+    return worldIn.getBlockState(pos).getBlock() == Blocks.FIRE;
+  }
+
+  private boolean canExtinguishInfiniteFire(World worldIn, BlockPos pos) {
+    return hasInfiniburnTag(worldIn, pos) && canExtinguish(worldIn, pos.offset(Direction.UP));
+  }
+
+  private boolean hasInfiniburnTag(World worldIn, BlockPos pos) {
+    return (BlockTags.INFINIBURN_END.contains(worldIn.getBlockState(pos).getBlock()) && worldIn.getDimensionKey() == World.THE_END)
+            || (BlockTags.INFINIBURN_NETHER.contains(worldIn.getBlockState(pos).getBlock()) && worldIn.getDimensionKey() == World.THE_NETHER)
+            || (BlockTags.INFINIBURN_OVERWORLD.contains(worldIn.getBlockState(pos).getBlock()) && worldIn.getDimensionKey() == World.OVERWORLD);
   }
 
   @Override
