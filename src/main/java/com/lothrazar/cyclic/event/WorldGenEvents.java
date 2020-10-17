a1d38a7e19ed227c4f355b6f5804443f6a6399b8
lothrazar <samson.bassett@gmail.com>
2020-10-17 11:02:33 -0700

    0.8.3 flower gen: cleanup and changelog

--- src/main/java/com/lothrazar/cyclic/event/WorldGenEvents.java
+++ src/main/java/com/lothrazar/cyclic/event/WorldGenEvents.java
@@ -2,6 +2,7 @@ package com.lothrazar.cyclic.event;
 
 import com.lothrazar.cyclic.ModCyclic;
 import com.lothrazar.cyclic.registry.BlockRegistry;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.blockplacer.SimpleBlockPlacer;
 import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
@@ -16,16 +17,20 @@ public class WorldGenEvents {
 
   @SubscribeEvent
   public static void onBiomeLoadingEvent(BiomeLoadingEvent event) {
-    //  if (CYAN_FLOWER == null) {
-    CYAN_FLOWER = (new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.flower_cyan.getDefaultState()),
-        new SimpleBlockPlacer())).tries(64 * 64).xSpread(20).ySpread(128).zSpread(30).build();
-    // }
+    if (CYAN_FLOWER == null) {
+      CYAN_FLOWER = (new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.flower_cyan.getDefaultState()),
+          new SimpleBlockPlacer())).tries(64).xSpread(20).ySpread(128).zSpread(20).build();
+    }
     //
-    ModCyclic.LOGGER.info(String.format("BIOME=%s, temperature= %s, category=%s", event.getName(), event.getClimate().temperature, event.getCategory().getName()));
-    //so the getName is something like  minecraft:modified_wooded_badlands_plateau
-    //    Biome.Category.FOREST
-    event.getGeneration().withFeature(
-        GenerationStage.Decoration.VEGETAL_DECORATION,
-        Feature.FLOWER.withConfiguration(CYAN_FLOWER));
+    if (event.getCategory() == Biome.Category.FOREST
+        || event.getCategory() == Biome.Category.PLAINS
+        || event.getCategory() == Biome.Category.TAIGA
+        || event.getCategory() == Biome.Category.EXTREME_HILLS) {
+      ModCyclic.LOGGER.info(String.format("cyan_flower in BIOME=%s; category=%s", event.getName(), event.getCategory().getName()));
+      //spawn 
+      event.getGeneration().withFeature(
+          GenerationStage.Decoration.VEGETAL_DECORATION,
+          Feature.FLOWER.withConfiguration(CYAN_FLOWER));
+    }
   }
 }
