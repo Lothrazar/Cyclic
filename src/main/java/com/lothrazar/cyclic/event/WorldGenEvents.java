06ba25a1a0af6606b19de31ea278122f520752b8
lothrazar <samson.bassett@gmail.com>
2020-10-17 10:35:11 -0700

    0.8.3 flower gen: as soon as i added ySpread it started working

--- src/main/java/com/lothrazar/cyclic/event/WorldGenEvents.java
+++ src/main/java/com/lothrazar/cyclic/event/WorldGenEvents.java
@@ -16,10 +16,10 @@ public class WorldGenEvents {
 
   @SubscribeEvent
   public static void onBiomeLoadingEvent(BiomeLoadingEvent event) {
-    if (CYAN_FLOWER == null) {
-      CYAN_FLOWER = (new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.flower_cyan.getDefaultState()),
-          new SimpleBlockPlacer())).tries(64 * 64).xSpread(20).zSpread(30).build();
-    }
+    //  if (CYAN_FLOWER == null) {
+    CYAN_FLOWER = (new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.flower_cyan.getDefaultState()),
+        new SimpleBlockPlacer())).tries(64 * 64).xSpread(20).ySpread(128).zSpread(30).build();
+    // }
     //
     ModCyclic.LOGGER.info(String.format("BIOME=%s, temperature= %s, category=%s", event.getName(), event.getClimate().temperature, event.getCategory().getName()));
     //so the getName is something like  minecraft:modified_wooded_badlands_plateau
