ccb349838ceb594bb2070e62ddb61f7b0f01601b
lothrazar <samson.bassett@gmail.com>
2020-10-17 19:29:25 -0700

    0.8.3 #1535 kicked for flying fix

--- src/main/java/com/lothrazar/cyclic/net/PacketPlayerFalldamage.java
+++ src/main/java/com/lothrazar/cyclic/net/PacketPlayerFalldamage.java
@@ -6,6 +6,9 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.network.NetworkEvent;
 
+/**
+ * Used by: Fan block; Launch enchant; Air charm; Climbing Glove; Scaffolding Block
+ */
 public class PacketPlayerFalldamage extends PacketBase {
 
   public static final int TICKS_FALLDIST_SYNC = 22;//tick every so often
@@ -13,7 +16,14 @@ public class PacketPlayerFalldamage extends PacketBase {
   public static void handle(PacketPlayerFalldamage message, Supplier<NetworkEvent.Context> ctx) {
     ctx.get().enqueueWork(() -> {
       ServerPlayerEntity player = ctx.get().getSender();
+      /**
+       * if fall damage gets high, they take damage on landing
+       */
       player.fallDistance = 0.0F;
+      /**
+       * Used to keep track of how the player is floating while gamerules should prevent that. Surpassing 80 ticks means kick
+       */
+      player.connection.floatingTickCount = 0;
     });
     message.done(ctx);
   }
