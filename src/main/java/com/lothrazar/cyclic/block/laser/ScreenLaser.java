cb3badeb49371f672a6a6863a73cb27045f8c654
lothrazar <samson.bassett@gmail.com>
2020-12-07 19:50:25 -0800

    laser offset buttons

--- src/main/java/com/lothrazar/cyclic/block/laser/ScreenLaser.java
+++ src/main/java/com/lothrazar/cyclic/block/laser/ScreenLaser.java
@@ -1,9 +1,13 @@
 package com.lothrazar.cyclic.block.laser;
 
 import com.lothrazar.cyclic.base.ScreenBase;
+import com.lothrazar.cyclic.gui.ButtonMachine;
 import com.lothrazar.cyclic.gui.ButtonMachineRedstone;
 import com.lothrazar.cyclic.gui.GuiSliderInteger;
+import com.lothrazar.cyclic.net.PacketTileData;
+import com.lothrazar.cyclic.registry.PacketRegistry;
 import com.lothrazar.cyclic.registry.TextureRegistry;
+import com.lothrazar.cyclic.util.UtilChat;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.text.ITextComponent;
@@ -11,6 +15,9 @@ import net.minecraft.util.text.ITextComponent;
 public class ScreenLaser extends ScreenBase<ContainerLaser> {
 
   private ButtonMachineRedstone btnRedstone;
+  private ButtonMachine btnX;
+  private ButtonMachine btnY;
+  private ButtonMachine btnZ;
 
   public ScreenLaser(ContainerLaser screenContainer, PlayerInventory inv, ITextComponent titleIn) {
     super(screenContainer, inv, titleIn);
@@ -25,9 +32,9 @@ public class ScreenLaser extends ScreenBase<ContainerLaser> {
     y = guiTop + 8;
     btnRedstone = addButton(new ButtonMachineRedstone(x, y, TileLaser.Fields.REDSTONE.ordinal(), container.tile.getPos()));
     //
-    y += 20;
+    y += 26;
     int w = 160;
-    int h = 10;
+    int h = 14;
     int f = TileLaser.Fields.RED.ordinal();
     GuiSliderInteger red = this.addButton(new GuiSliderInteger(x, y, w, h, f, container.tile.getPos(),
         0, 255, container.tile.getField(f)));
@@ -52,6 +59,34 @@ public class ScreenLaser extends ScreenBase<ContainerLaser> {
     GuiSliderInteger thick = this.addButton(new GuiSliderInteger(x, y, w, h, f, container.tile.getPos(),
         1, 20, container.tile.getField(f)));
     thick.setTooltip("cyclic.screen.thick");
+    //
+    //
+    //
+    //
+    w = 50;
+    y += h + 2;
+    btnX = addButton(new ButtonMachine(x, y, w, 20, "X", (p) -> {
+      final int fl = TileLaser.Fields.XOFF.ordinal();
+      container.tile.setField(fl, container.tile.getField(fl) + 1);
+      PacketRegistry.INSTANCE.sendToServer(new PacketTileData(fl, container.tile.getField(fl), container.tile.getPos()));
+    }));
+    btnX.setTooltip("button.offsetx.tooltip");
+    //
+    x += w + 2;
+    btnY = addButton(new ButtonMachine(x, y, w, 20, "Y", (p) -> {
+      final int fl = TileLaser.Fields.YOFF.ordinal();
+      container.tile.setField(fl, container.tile.getField(fl) + 1);
+      PacketRegistry.INSTANCE.sendToServer(new PacketTileData(fl, container.tile.getField(fl), container.tile.getPos()));
+    }));
+    btnY.setTooltip("button.offsety.tooltip");
+    //
+    x += w + 2;
+    btnZ = addButton(new ButtonMachine(x, y, w, 20, "z", (p) -> {
+      final int fl = TileLaser.Fields.ZOFF.ordinal();
+      container.tile.setField(fl, container.tile.getField(fl) + 1);
+      PacketRegistry.INSTANCE.sendToServer(new PacketTileData(fl, container.tile.getField(fl), container.tile.getPos()));
+    }));
+    btnZ.setTooltip("button.offsetz.tooltip");
   }
 
   @Override
@@ -72,5 +107,11 @@ public class ScreenLaser extends ScreenBase<ContainerLaser> {
   protected void drawGuiContainerBackgroundLayer(MatrixStack ms, float partialTicks, int mouseX, int mouseY) {
     this.drawBackground(ms, TextureRegistry.INVENTORY_LARGE_PLAIN);
     this.drawSlot(ms, 151, 7, TextureRegistry.SLOT_GPS, 18);
+    btnX.setMessage(UtilChat.ilang("button.offsetblock.name" +
+        container.tile.getField(TileLaser.Fields.XOFF.ordinal())));
+    btnY.setMessage(UtilChat.ilang("button.offsetblock.name" +
+        container.tile.getField(TileLaser.Fields.YOFF.ordinal())));
+    btnZ.setMessage(UtilChat.ilang("button.offsetblock.name" +
+        container.tile.getField(TileLaser.Fields.ZOFF.ordinal())));
   }
 }
