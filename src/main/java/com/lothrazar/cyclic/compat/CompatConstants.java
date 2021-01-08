583eaf017460faaa45d3d79cca248af331c24b75
lothrazar <samson.bassett@gmail.com>
2021-01-07 22:31:33 -0800

    harvester run linear, so nonrandom

---
+++ src/main/java/com/lothrazar/cyclic/compat/CompatConstants.java
@@ -0,0 +1,6 @@
+package com.lothrazar.cyclic.compat;
+public class CompatConstants {
+
+  public static final String RESYNTH_MODID = "resynth";
+  public static final String RESYNTH_CROPSAGE = "growth_stage";
+}
