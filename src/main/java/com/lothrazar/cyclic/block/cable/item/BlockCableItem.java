91f9b946574b344efe6bd1dec67152b7c8badf24
lothrazar <samson.bassett@gmail.com>
2021-06-05 17:39:36 -0700

    fix cables not dropping items for 1.2.10

--- src/main/java/com/lothrazar/cyclic/block/cable/item/BlockCableItem.java
+++ src/main/java/com/lothrazar/cyclic/block/cable/item/BlockCableItem.java
@@ -77,6 +77,15 @@ public class BlockCableItem extends CableBase {
       if (tileentity != null && tileentity.filter != null) {
         InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), tileentity.filter.getStackInSlot(0));
       }
+      for (Direction dir : Direction.values()) {
+        IItemHandler items = tileentity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, dir).orElse(null);
+        if (items != null) {
+          for (int i = 0; i < items.getSlots(); ++i) {
+            InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), items.getStackInSlot(i));
+          }
+        }
+      }
+      worldIn.updateComparatorOutputLevel(pos, this);
     }
     super.onReplaced(state, worldIn, pos, newState, isMoving);
   }
