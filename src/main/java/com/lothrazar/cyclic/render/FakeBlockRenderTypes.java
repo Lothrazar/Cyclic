170f66f327f77b1e2688e718c5671d6f05c8b8ad
lothrazar <samson.bassett@gmail.com>
2021-10-09 08:52:54 -0700

    port(1.17.1): update shader

--- src/main/java/com/lothrazar/cyclic/render/FakeBlockRenderTypes.java
+++ src/main/java/com/lothrazar/cyclic/render/FakeBlockRenderTypes.java
@@ -15,9 +15,9 @@ public class FakeBlockRenderTypes extends RenderType {
   }
 
   /**
-   * laser rendering from this MIT project https://github.com/Direwolf20-MC/DireGoo2/blob/master/LICENSE.md 1.17 BOOLS are
+   * laser rendering from this MIT project https://github.com/Direwolf20-MC/DireGoo2/blob/master/LICENSE.md
    * <p>
-   * this.affectsCrumbling = p_173182_; this.sortOnUpload = p_173183_;
+   * 1.17 BOOLS are this.affectsCrumbling = p_173182_; this.sortOnUpload = p_173183_;
    */
   public static final RenderType LASER_MAIN_BEAM = create("MiningLaserMainBeam",
       DefaultVertexFormat.POSITION_COLOR_TEX, VertexFormat.Mode.QUADS, 256,
@@ -60,11 +60,13 @@ public class FakeBlockRenderTypes extends RenderType {
   public static final RenderType SOLID_COLOUR = create("solidColour",
       DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.QUADS, 256, false, false, // affectsCrumbling, sortOnUpload
       RenderType.CompositeState.builder()
-          .setShaderState(RenderStateShard.RENDERTYPE_TRANSLUCENT_MOVING_BLOCK_SHADER) //1.17 new
+          //RENDERTYPE_TRANSLUCENT_MOVING_BLOCK_SHADER 
+          //RENDERTYPE_TRANSLUCENT_SHADER
+          .setShaderState(RenderStateShard.ShaderStateShard.RENDERTYPE_LINES_SHADER) //1.17 new
           //          .layer(PROJECTION_LAYERING)
           .setTransparencyState(ADDITIVE_TRANSPARENCY)
           .setTextureState(NO_TEXTURE)
-          .setDepthTestState(LEQUAL_DEPTH_TEST)
+          .setDepthTestState(NO_DEPTH_TEST) //  .setDepthTestState(LEQUAL_DEPTH_TEST)
           .setCullState(CULL)
           .setLightmapState(NO_LIGHTMAP)
           .setWriteMaskState(COLOR_DEPTH_WRITE)
