9166948f4ec436eb908432c2eec05eb94db11a09
lothrazar <samson.bassett@gmail.com>
2021-10-18 22:01:02 -0700

    1.5.4 item node drop filter

--- src/main/java/com/lothrazar/cyclic/block/wireless/item/BlockWirelessItem.java
+++ src/main/java/com/lothrazar/cyclic/block/wireless/item/BlockWirelessItem.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.fluid.FluidState;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
@@ -15,6 +16,7 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockDisplayReader;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 public class BlockWirelessItem extends BlockBase {
 
@@ -57,5 +59,18 @@ public class BlockWirelessItem extends BlockBase {
   @Override
   public TileEntity createTileEntity(BlockState state, IBlockReader world) {
     return new TileWirelessItem();
+  }
+
+  @Override
+  public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+    if (state.getBlock() != newState.getBlock()) {
+      TileWirelessItem tileentity = (TileWirelessItem) worldIn.getTileEntity(pos);
+      if (tileentity != null) {
+        for (int i = 0; i < tileentity.gpsSlots.getSlots(); ++i) {
+          InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), tileentity.gpsSlots.getStackInSlot(i));
+        }
+      }
+      super.onReplaced(state, worldIn, pos, newState, isMoving);
+    }
   }
 }
