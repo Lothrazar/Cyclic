01f92deceb0c7f9dfeb8fcfb500daa2f2df520d7
lothrazar <samson.bassett@gmail.com>
2021-11-09 06:09:22 -0800

    1.6.1 merge all 1.16 changes and add buckets back

--- src/main/java/com/lothrazar/cyclic/fluid/FluidMagmaHolder.java
+++ src/main/java/com/lothrazar/cyclic/fluid/FluidMagmaHolder.java
@@ -3,7 +3,12 @@ package com.lothrazar.cyclic.fluid;
 import com.lothrazar.cyclic.fluid.block.MagmaFluidBlock;
 import com.lothrazar.cyclic.registry.BlockRegistry;
 import com.lothrazar.cyclic.registry.FluidRegistry;
+import com.lothrazar.cyclic.registry.ItemRegistry;
+import com.lothrazar.cyclic.registry.MaterialRegistry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.material.FlowingFluid;
@@ -21,7 +26,7 @@ public class FluidMagmaHolder {
   public static RegistryObject<LiquidBlock> BLOCK = BlockRegistry.BLOCKS.register(id + "_block", () -> new MagmaFluidBlock(STILL, Block.Properties.of(Material.WATER).strength(100.0F).lightLevel((p_235456_0_) -> {
     return 8;
   }).noDrops()));
-  //  public static RegistryObject<Item> BUCKET = ItemRegistry.ITEMS.register(id + "_bucket", () -> new BucketItem(STILL, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1).tab(MaterialRegistry.ITEM_GROUP)));
+  public static RegistryObject<Item> BUCKET = ItemRegistry.ITEMS.register(id + "_bucket", () -> new BucketItem(STILL, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1).tab(MaterialRegistry.ITEM_GROUP)));
 
   private static ForgeFlowingFluid.Properties makeProperties() {
     return new ForgeFlowingFluid.Properties(
@@ -30,7 +35,7 @@ public class FluidMagmaHolder {
         FluidAttributes.builder(
             new ResourceLocation("minecraft:block/" + id),
             new ResourceLocation("minecraft:block/" + id)))
-                //                .bucket(BUCKET)
+                .bucket(BUCKET)
                 .block(BLOCK);
   }
 }
