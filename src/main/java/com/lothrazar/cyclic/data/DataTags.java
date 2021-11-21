4024da0f6d1b242a5d8dd4ddf2d8aa90207cb4a8
lothrazar <samson.bassett@gmail.com>
2021-11-21 10:43:29 -0800

    1.6.3 recipes and tags

--- src/main/java/com/lothrazar/cyclic/data/DataTags.java
+++ src/main/java/com/lothrazar/cyclic/data/DataTags.java
@@ -16,19 +16,24 @@ public class DataTags {
   public static final Named<Fluid> MAGMA = FluidTags.bind("forge:magma");
   public static final Named<Fluid> SLIME = FluidTags.bind("forge:slime");
   public static final Named<Block> WITH_MATTOCK = BlockTags.bind("minecraft:mineable/mattock");
+  public static final Named<Block> GLASS_DARK = BlockTags.bind("forge:glass/dark");
   public static final Named<Block> PLANTS = BlockTags.bind("forge:plants");
   public static final Named<Block> MUSHROOMS = BlockTags.bind("forge:mushrooms");
   public static final Named<Block> VINES = BlockTags.bind("forge:vines");
-  public static final Named<Block> GLASS_DARK = BlockTags.bind("forge:glass/dark");
   public static final Named<Block> CACTUS = BlockTags.bind("forge:cactus");
   public static final Named<Block> CROP_BLOCKS = BlockTags.bind("forge:crop_blocks");
   public static final Named<Item> FISHING_RODS = ItemTags.bind("forge:fishing_rods");
+  public static final Named<Item> GLASS_DARKI = ItemTags.bind("forge:glass/dark");
   public static final Named<Item> BOOKS = ItemTags.bind("forge:books");
   public static final Named<Item> ANVIL_IMMUNE = ItemTags.bind("cyclic:anvil_immune");
   public static final Named<Item> DISENCHANTER_IMMUNE = ItemTags.bind("cyclic:disenchanter_immune");
   public static final Named<Item> COPPER_ORE = ItemTags.bind("forge:ores/copper");
   public static final Named<Item> COPPER_INGOTS = ItemTags.bind("forge:ingots/copper");
   public static final Named<Item> WRENCH = ItemTags.bind("forge:tools/wrench");
+  public static final Named<Item> IPLANTS = ItemTags.bind("forge:plants");
+  public static final Named<Item> IMUSHROOMS = ItemTags.bind("forge:mushrooms");
+  public static final Named<Item> IVINES = ItemTags.bind("forge:vines");
+  public static final Named<Item> ICACTUS = ItemTags.bind("forge:cactus");
 
   public static void setup() {
     // do not delete:! this makes the mod get classloaded so the wrapper tags correctly get added to the registry early, before recipe testing
