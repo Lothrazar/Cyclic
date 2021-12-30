a1f1479d7560e424830eccd4bb0383fa497c02c8
lothrazar <samson.bassett@gmail.com>
2021-12-30 14:34:36 -0800

    1.6.8 fix #2001 wireless redstone

--- src/main/java/com/lothrazar/cyclic/block/wireless/redstone/ContainerTransmit.java
+++ src/main/java/com/lothrazar/cyclic/block/wireless/redstone/ContainerTransmit.java
@@ -8,7 +8,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.items.SlotItemHandler;
 
 public class ContainerTransmit extends ContainerBase {
@@ -20,12 +20,11 @@ public class ContainerTransmit extends ContainerBase {
     tile = (TileWirelessTransmit) world.getBlockEntity(pos);
     this.playerEntity = player;
     this.playerInventory = playerInventory;
-    tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
-      this.endInv = h.getSlots();
-      for (int s = 0; s < h.getSlots(); s++) {
-        addSlot(new SlotItemHandler(h, s, 8 + 18 * s, 49));
-      }
-    });
+    ItemStackHandler h = tile.inventory;
+    this.endInv = h.getSlots();
+    for (int s = 0; s < h.getSlots(); s++) {
+      addSlot(new SlotItemHandler(h, s, 8 + 18 * s, 49));
+    }
     layoutPlayerInventorySlots(8, 84);
     this.trackAllIntFields(tile, TileWirelessTransmit.Fields.values().length);
   }
