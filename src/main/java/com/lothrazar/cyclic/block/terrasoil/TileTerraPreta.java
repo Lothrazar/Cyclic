f8f95ea6d699082e690e0bd4c799ab9685d88987
Sr_endi <info@denizdemiroglu.de>
2022-02-04 16:46:35 +0100

    fix(style violations): remove unused import

--- src/main/java/com/lothrazar/cyclic/block/terrasoil/TileTerraPreta.java
+++ src/main/java/com/lothrazar/cyclic/block/terrasoil/TileTerraPreta.java
@@ -10,7 +10,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
 
