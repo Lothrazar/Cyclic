1fc23c08f09711187edaa607ee71fe6aad931ac8
lothrazar <samson.bassett@gmail.com>
2022-07-29 09:04:55 -0700

    1.19-1.7.7 fix #2140

--- src/main/java/com/lothrazar/cyclic/registry/ClientRegistryCyclic.java
+++ src/main/java/com/lothrazar/cyclic/registry/ClientRegistryCyclic.java
@@ -30,6 +30,7 @@ import com.lothrazar.cyclic.block.soundmuff.ghost.SoundmuffRender;
 import com.lothrazar.cyclic.block.sprinkler.RenderSprinkler;
 import com.lothrazar.cyclic.block.tank.RenderTank;
 import com.lothrazar.cyclic.block.wireless.redstone.RenderTransmit;
+import com.lothrazar.cyclic.capabilities.ClientDataManager;
 import com.lothrazar.cyclic.event.ClientInputEvents;
 import com.lothrazar.cyclic.event.EventRender;
 import com.lothrazar.cyclic.item.ItemBaseCyclic;
@@ -51,8 +52,11 @@ import net.minecraft.world.item.SpawnEggItem;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.client.settings.IKeyConflictContext;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.MinecraftForge;
@@ -66,22 +70,36 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ClientRegistryCyclic {
 
   //TODO: refactor split into keyboard registry, overlay registry, other renderers below 
-  public static KeyMapping CAKE;
-  //  public static final IIngameOverlay HUD_MANA = (gui, poseStack, partialTicks, width, height) -> {
-  //    //cancel if turned off
-  //    if (!FeatureRegistry.PLAYER_RENDER_CAPS) {
-  //      return;
-  //    }
-  //    //ok go
-  //    if (Minecraft.getInstance().player.getMainHandItem().is(ItemRegistry.BATTERY_INFINITE.get())) {
-  //      final String toDisplay = "P:" + ClientDataManager.getPlayerMana() + " CH:" + ClientDataManager.getChunkMana();
-  //      int x = 10; // ManaConfig.MANA_HUD_X.get();
-  //      int y = 10; // ManaConfig.MANA_HUD_Y.get(); //TODO: client-config
-  //      if (x >= 0 && y >= 0) {
-  //        gui.getFont().draw(poseStack, toDisplay, x, y, 0xFF0000); // client config color
-  //      }
-  //    }
-  //  };
+  public static final KeyMapping CAKE = new KeyMapping("key." + ModCyclic.MODID + ".cake", new IKeyConflictContext() {
+
+    @Override
+    public boolean isActive() {
+      //client side cant know when active. stored on server player file 
+      //maybe when no gui is open 
+      return true;
+    }
+
+    @Override
+    public boolean conflicts(IKeyConflictContext other) {
+      return this == other || KeyConflictContext.IN_GAME == other;
+    }
+  }, InputConstants.Type.KEYSYM.getOrCreate(GLFW.GLFW_KEY_X), "key." + ModCyclic.MODID + ".category");
+  //IIngameOverlay
+  public static final IGuiOverlay HUD_MANA = (gui, poseStack, partialTicks, width, height) -> {
+    //cancel if turned off
+    if (!FeatureRegistry.PLAYER_RENDER_CAPS) {
+      return;
+    }
+    //ok go
+    if (Minecraft.getInstance().player.getMainHandItem().is(ItemRegistry.BATTERY_INFINITE.get())) {
+      final String toDisplay = "P:" + ClientDataManager.getPlayerMana() + " CH:" + ClientDataManager.getChunkMana();
+      int x = 10; // ManaConfig.MANA_HUD_X.get();
+      int y = 10; // ManaConfig.MANA_HUD_Y.get(); //TODO: client-config
+      if (x >= 0 && y >= 0) {
+        gui.getFont().draw(poseStack, toDisplay, x, y, 0xFF0000); // client config color
+      }
+    }
+  };
 
   public ClientRegistryCyclic() {
     //fired by mod constructor  DistExecutor.safeRunForDist
@@ -98,13 +116,7 @@ public class ClientRegistryCyclic {
     }
     initRenderLayers();
     initColours();
-    initKeybindings();
     initShields();
-    initOverlay();
-  }
-
-  private static void initOverlay() {
-    //    OverlayRegistry.registerOverlayAbove(ForgeIngameGui.HOTBAR_ELEMENT, "data", HUD_MANA);
   }
 
   @SubscribeEvent
@@ -181,26 +193,14 @@ public class ClientRegistryCyclic {
     ItemBlockRenderTypes.setRenderLayer(BlockRegistry.COPPER_SOUL_LANTERN.get(), RenderType.cutoutMipped());
   }
 
-  private static void initKeybindings() {
-    CAKE = new KeyMapping("key." + ModCyclic.MODID + ".cake", new IKeyConflictContext() {
-
-      @Override
-      public boolean isActive() {
-        //client side cant know when active. stored on server player file 
-        //maybe when no gui is open 
-        return true;
-      }
-
-      @Override
-      public boolean conflicts(IKeyConflictContext other) {
-        return this == other || KeyConflictContext.IN_GAME == other;
-      }
-    }, InputConstants.Type.KEYSYM.getOrCreate(GLFW.GLFW_KEY_X), "key." + ModCyclic.MODID + ".category");
-    //    import net.minecraftforge.client.ClientRegistry;
+  //    OverlayRegistry.registerOverlayAbove(ForgeIngameGui.HOTBAR_ELEMENT, "data", HUD_MANA);
+  @SubscribeEvent
+  public static void onRegisterGuiOverlays(RegisterGuiOverlaysEvent event) {
+    event.registerAbove(VanillaGuiOverlay.HOTBAR.id(), ModCyclic.MODID, HUD_MANA);
   }
 
   @SubscribeEvent
-  public static void onRegisterKeyMappingsEvent(RegisterKeyMappingsEvent event) {
+  public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
     //    net.minecraftforge.client.ClientRegistry.registerKeyBinding(CAKE);
     event.register(CAKE);
   }
