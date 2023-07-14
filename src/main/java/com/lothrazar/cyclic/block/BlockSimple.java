f8f99649ded32389997d2df3a993d1358c7bb143
lothrazar <samson.bassett@gmail.com>
2023-07-13 22:40:25 -0700

    0.0.7-flib base blocks and gradle-fix

--- src/main/java/com/lothrazar/cyclic/block/BlockSimple.java
+++ src/main/java/com/lothrazar/cyclic/block/BlockSimple.java
@@ -1,24 +1,10 @@
 package com.lothrazar.cyclic.block;
 
-import java.util.List;
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import com.lothrazar.library.block.BlockFlib;
 
-public class BlockSimple extends Block {
+public class BlockSimple extends BlockFlib {
 
   public BlockSimple(Properties properties) {
-    super(properties);
-  }
-
-  @Override
-  @OnlyIn(Dist.CLIENT)
-  public void appendHoverText(ItemStack stack, BlockGetter worldIn, List<Component> tooltip, TooltipFlag flagIn) {
-    tooltip.add(Component.translatable(getDescriptionId() + ".tooltip").withStyle(ChatFormatting.GRAY));
+    super(properties, new BlockFlib.Settings().tooltip());
   }
 }
