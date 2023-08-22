ee43bde93d6c47e583b67046c259311ce56a331d
lothrazar <samson.bassett@gmail.com>
2023-08-22 11:34:02 -0700

    1.20.3 shield textures texture binding to correct atlas #2297

--- src/main/java/com/lothrazar/cyclic/registry/MaterialShieldRegistry.java
+++ src/main/java/com/lothrazar/cyclic/registry/MaterialShieldRegistry.java
@@ -1,22 +1,20 @@
 package com.lothrazar.cyclic.registry;
 
 import com.lothrazar.cyclic.ModCyclic;
+import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.InventoryMenu;
 
 public class MaterialShieldRegistry {
 
-  // InventoryMenu.BLOCK_ATLAS was TextureAtlas.LOCATION_BLOCKS
-  // ClientRegistryCyclic : onStitch(TextureStitchEvent.Pre event)
-  public static final Material SHIELD_BASE_WOOD = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/wood_base"));
-  public static final Material SHIELD_BASE_WOOD_NOPATTERN = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/wood_base_nopattern"));
-  public static final Material SHIELD_BASE_LEATHER = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/leather_base"));
-  public static final Material SHIELD_BASE_LEATHER_NOPATTERN = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/leather_base_nopattern"));
-  public static final Material SHIELD_BASE_FLINT = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/flint_base"));
-  public static final Material SHIELD_BASE_FLINT_NOPATTERN = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/flint_base_nopattern"));
-  public static final Material SHIELD_BASE_BONE = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/bone_base"));
-  public static final Material SHIELD_BASE_BONE_NOPATTERN = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/bone_base_nopattern"));
-  public static final Material SHIELD_BASE_OBSIDIAN = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/obsidian_base"));
-  public static final Material SHIELD_BASE_OBSIDIAN_NOPATTERN = new Material(InventoryMenu.BLOCK_ATLAS, new ResourceLocation(ModCyclic.MODID, "entity/shield/obsidian_base_nopattern"));
+  public static final Material SHIELD_BASE_WOOD = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/wood_base"));
+  public static final Material SHIELD_BASE_WOOD_NOPATTERN = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/wood_base_nopattern"));
+  public static final Material SHIELD_BASE_LEATHER = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/leather_base"));
+  public static final Material SHIELD_BASE_LEATHER_NOPATTERN = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/leather_base_nopattern"));
+  public static final Material SHIELD_BASE_FLINT = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/flint_base"));
+  public static final Material SHIELD_BASE_FLINT_NOPATTERN = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/flint_base_nopattern"));
+  public static final Material SHIELD_BASE_BONE = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/bone_base"));
+  public static final Material SHIELD_BASE_BONE_NOPATTERN = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/bone_base_nopattern"));
+  public static final Material SHIELD_BASE_OBSIDIAN = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/obsidian_base"));
+  public static final Material SHIELD_BASE_OBSIDIAN_NOPATTERN = new Material(Sheets.SHIELD_SHEET, new ResourceLocation(ModCyclic.MODID, "entity/shield/obsidian_base_nopattern"));
 }
