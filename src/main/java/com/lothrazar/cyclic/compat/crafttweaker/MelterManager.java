cf5e1577565307b9a33fc0fc646bc003eab117ac
lothrazar <samson.bassett@gmail.com>
2023-08-23 10:00:12 -0700

    new examples folder with datapack to disable emerald items

--- src/main/java/com/lothrazar/cyclic/compat/crafttweaker/MelterManager.java
+++ src/main/java/com/lothrazar/cyclic/compat/crafttweaker/MelterManager.java
@@ -1,32 +1,29 @@
 //package com.lothrazar.cyclic.compat.crafttweaker;
 //
 //import com.blamejared.crafttweaker.api.CraftTweakerAPI;
-//import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+//import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 //import com.blamejared.crafttweaker.api.fluid.IFluidStack;
-//import com.blamejared.crafttweaker.api.item.IIngredient;
-//import com.blamejared.crafttweaker.api.managers.IRecipeManager;
-//import com.blamejared.crafttweaker.impl.actions.recipes.ActionAddRecipe;
+//import com.blamejared.crafttweaker.api.ingredient.IIngredient;
+//import com.blamejared.crafttweaker.api.recipe.manager.base.IRecipeManager;
 //import com.lothrazar.cyclic.ModCyclic;
 //import com.lothrazar.cyclic.block.melter.RecipeMelter;
 //import com.lothrazar.cyclic.compat.CompatConstants;
-//import com.lothrazar.cyclic.recipe.CyclicRecipeType;
+//import com.lothrazar.cyclic.registry.CyclicRecipeType;
 //import net.minecraft.world.item.crafting.RecipeType;
 //import net.minecraft.resources.ResourceLocation;
 //import net.minecraftforge.fluids.FluidStack;
 //import org.openzen.zencode.java.ZenCodeType;
-//
-//@SuppressWarnings("rawtypes")
 //@ZenRegister
 //@ZenCodeType.Name("mods.cyclic.melter")
 //public class MelterManager implements IRecipeManager {
 //
 //  @Override
 //  public RecipeType<?> getRecipeType() {
-//    return CyclicRecipeType.MELTER;
+//    return CyclicRecipeType.MELTER.get();
 //  }
 //
 //  @ZenCodeType.Method
-//  public void addRecipe(String name, IIngredient inputFirst, IIngredient inputSecond, IFluidStack fluidStack) {
+//  public void addRecipe(String name, IIngredient[] input, IFluidStack fluidStack) {
 //    name = fixRecipeName(name);
 //    RecipeMelter<?> m = new RecipeMelter(new ResourceLocation(CompatConstants.CRAFTTWEAKER, name),
 //        inputFirst.asVanillaIngredient(),
@@ -37,8 +34,8 @@
 //  }
 //
 //  @ZenCodeType.Method
-//  public void removeRecipe(String name) {
-//    removeByName(name);
-//    ModCyclic.LOGGER.info("CRAFT TWEAKER: Recipe removed " + name);
+//  public void removeRecipe(String... names) {
+//    removeByName(names);
+//    ModCyclic.LOGGER.info("CRAFT TWEAKER: Recipe removed " + names);
 //  }
 //}
