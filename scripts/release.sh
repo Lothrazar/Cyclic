522d9c1f1d2dfcb6bcd9447a46e60496ea4d0a45
lothrazar <samson.bassett@gmail.com>
2023-08-23 10:26:46 -0700

    move example configs and add re-generator on release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+echo 'updating example configs...'
+
+cp ./run/config/cyclic.toml ./examples/config/cyclic.toml
+cp ./run/config/cyclic-client.toml ./examples/config/cyclic-client.toml
+
+echo '... done'
+
+echo 'deploying...'
+
 ./gradlew cleanJar build signJar
+
+echo 'jar deployed to ./build/libs/'
