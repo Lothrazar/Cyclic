8ed9ca2f6b8de004994da62ea20e6e71c1415e64
PocketSizedWeeb <PocketSizedWeeb@gmail.com>
2024-04-13 19:58:24 -0700

    JEI compat changes
    - Fixes issue #2369
    - Made some JEI category changes to make it look a bit better

--- src/main/java/com/lothrazar/cyclic/compat/jei/SolidifierRecipeCategory.java
+++ src/main/java/com/lothrazar/cyclic/compat/jei/SolidifierRecipeCategory.java
@@ -3,7 +3,11 @@ package com.lothrazar.cyclic.compat.jei;
 import java.util.List;
 import com.lothrazar.cyclic.ModCyclic;
 import com.lothrazar.cyclic.block.solidifier.RecipeSolidifier;
+import com.lothrazar.cyclic.block.solidifier.TileSolidifier;
 import com.lothrazar.cyclic.registry.BlockRegistry;
+import com.lothrazar.cyclic.registry.TextureRegistry;
+import com.lothrazar.library.gui.EnergyBar;
+import com.lothrazar.library.gui.TexturedProgress;
 import com.lothrazar.library.util.ChatUtil;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.forge.ForgeTypes;
@@ -16,6 +20,7 @@ import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -29,10 +34,16 @@ public class SolidifierRecipeCategory implements IRecipeCategory<RecipeSolidifie
   static final RecipeType<RecipeSolidifier> TYPE = new RecipeType<>(ID, RecipeSolidifier.class);
   private IDrawable gui;
   private IDrawable icon;
+  private Font font;
+  private EnergyBar bar;
+  private TexturedProgress progress;
 
   public SolidifierRecipeCategory(IGuiHelper helper) {
+    font = Minecraft.getInstance().font;
     gui = helper.drawableBuilder(new ResourceLocation(ModCyclic.MODID, "textures/jei/solidifier_recipe.png"), 0, 0, 169, 69).setTextureSize(169, 69).build();
     icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(BlockRegistry.SOLIDIFIER.get()));
+    bar = new EnergyBar(font, TileSolidifier.MAX);
+    progress = new TexturedProgress(font, 63, 25, 24, 17, TextureRegistry.ARROW);
   }
 
   @Override
@@ -57,10 +68,11 @@ public class SolidifierRecipeCategory implements IRecipeCategory<RecipeSolidifie
 
   @Override
   public void draw(RecipeSolidifier recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics ms, double mouseX, double mouseY) {
-    var font = Minecraft.getInstance().font;
-    ms.drawString(font, recipe.getEnergy().getTicks() + " t", 60, 0, FONT);
-    ms.drawString(font, recipe.getEnergy().getRfPertick() + " RF/t", 60, 10, FONT);
-    ms.drawString(font, recipe.getEnergy().getEnergyTotal() + " RF", 60, 20, FONT);
+    ms.drawString(font, recipe.getEnergy().getRfPertick() + " RF/t", 55, 10, FONT);
+    bar.draw(ms, recipe.getEnergy().getEnergyTotal());
+    progress.draw(ms, 0);
+    bar.renderHoveredToolTip(ms, (int) mouseX, (int) mouseY, recipe.getEnergy().getEnergyTotal());
+    progress.renderHoveredToolTip(ms, (int) mouseX, (int) mouseY, recipe.getEnergy().getTicks());
   }
 
   @Override
@@ -70,7 +82,7 @@ public class SolidifierRecipeCategory implements IRecipeCategory<RecipeSolidifie
     builder.addSlot(RecipeIngredientRole.INPUT, 34, 43).addIngredients(recipe.at(2));
     builder.addSlot(RecipeIngredientRole.OUTPUT, 104, 25).addItemStack(recipe.result);
     List<FluidStack> matchingFluids = recipe.fluidIngredient.getMatchingFluids();
-    builder.addSlot(RecipeIngredientRole.INPUT, 4, 25).addIngredients(ForgeTypes.FLUID_STACK, matchingFluids);
+    builder.addSlot(RecipeIngredientRole.INPUT, 4, 25).addIngredients(ForgeTypes.FLUID_STACK, matchingFluids).setFluidRenderer(4000, false, 16, 16);
   }
   //  @Override
   //  public void setIngredients(RecipeSolidifier recipe, IIngredients ingredients) {
