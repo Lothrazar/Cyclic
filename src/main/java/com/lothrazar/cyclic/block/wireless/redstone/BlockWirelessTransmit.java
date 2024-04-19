a681f8088528607e6a6e9ef92eca395f6d7d2bb5
PocketSizedWeeb <PocketSizedWeeb@gmail.com>
2024-04-19 11:22:27 -0700

    Fixed #2403
    - Added the onRemove method to the Wireless Transmitter

--- src/main/java/com/lothrazar/cyclic/block/wireless/redstone/BlockWirelessTransmit.java
+++ src/main/java/com/lothrazar/cyclic/block/wireless/redstone/BlockWirelessTransmit.java
@@ -5,6 +5,7 @@ import com.lothrazar.cyclic.registry.MenuTypeRegistry;
 import com.lothrazar.cyclic.registry.TileRegistry;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -43,5 +44,19 @@ public class BlockWirelessTransmit extends BlockCyclic {
   @Override
   protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
     builder.add(POWERED);
+  }
+  
+  @Override // was onReplaced
+  public void onRemove(BlockState state, Level worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+    if (state.getBlock() != newState.getBlock()) {
+      TileWirelessTransmit tileentity = (TileWirelessTransmit) worldIn.getBlockEntity(pos);
+      if (tileentity != null) {
+        for (int i = 0; i < tileentity.inventory.getSlots(); ++i) {
+          // was  InventoryHelper.spawnItemStack
+          Containers.dropItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), tileentity.inventory.getStackInSlot(i));
+        }
+      }
+      super.onRemove(state, worldIn, pos, newState, isMoving);
+    }
   }
 }
